b785149d617decb495c5181c2f7619de771061ae
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2010-07-23 15:50:57 +0000

    improved the command line parser

--- src/platform/desktop/properties/AssemblyInfo.cs
+++ src/platform/desktop/properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.4.0.24")]
-[assembly: AssemblyFileVersion("0.4.0.24")]
+[assembly: AssemblyVersion("0.4.0.50")]
+[assembly: AssemblyFileVersion("0.4.0.50")]
