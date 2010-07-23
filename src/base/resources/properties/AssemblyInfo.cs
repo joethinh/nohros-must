5abef6cd3bb1b85dcad5d1198e73c4b9ded3c71f
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2010-07-23 14:28:31 +0000

    Fix a bug in command line parsing, when a switch value contains a switch separator the value is not correctly parsed.

--- src/base/resources/properties/AssemblyInfo.cs
+++ src/base/resources/properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.2.90")]
-[assembly: AssemblyFileVersion("0.1.2.90")]
+[assembly: AssemblyVersion("0.1.2.100")]
+[assembly: AssemblyFileVersion("0.1.2.100")]
