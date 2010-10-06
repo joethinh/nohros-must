aecbae3f3838d9ea1ea6f20a0e22b012759bc308
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2010-10-06 15:38:48 +0000

    generic logger improvements.

--- src/base/resources/properties/AssemblyInfo.cs
+++ src/base/resources/properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.2.0.682")]
-[assembly: AssemblyFileVersion("0.2.0.683")]
+[assembly: AssemblyVersion("0.2.0.685")]
+[assembly: AssemblyFileVersion("0.2.0.686")]
