c1dea91c8a7fb318896cc1d2d7f15d0c3f609ad9
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2011-05-14 14:08:17 +0000

    added classes for abstract the cache.

---
+++ src/platform/web/components/cache/HttpRuntimeCacheProvider.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Net.Caching
+{
+    /// <summary>
+    /// Implements the <see cref="iCacheProvider"/> interface using the built-in .NET HTTP runtime cache.
+    /// </summary>
+    public class HttpRuntimeCacheProvider : ICacheProvider
+    {
+    }
+}
