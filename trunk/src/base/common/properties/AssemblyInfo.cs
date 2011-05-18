a15facde5b33132b1ec6c226be72ef615ebb40aa
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2011-05-18 20:58:15 +0000

    Cretaed a class to handle the concept of principals and a class to hold a collection of distinct principals. Changed the behavior of the Subject to acomodate the new principals concept.

--- trunk/src/base/common/properties/AssemblyInfo.cs
+++ trunk/src/base/common/properties/AssemblyInfo.cs
@@ -39,6 +39,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.5.6.224")]
-[assembly: AssemblyFileVersion("0.5.6.224")]
+[assembly: AssemblyVersion("0.5.6.249")]
+[assembly: AssemblyFileVersion("0.5.6.249")]
 
