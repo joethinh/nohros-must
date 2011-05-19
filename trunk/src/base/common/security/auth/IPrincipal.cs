a691fec81315c4550fb86e6172b85ecaf0358f7d
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2011-05-19 22:16:44 +0000

    Changed the beavior of the IPrincipal interface in order to make this interface compatible with the ISecureSet interface.

--- trunk/src/base/common/security/auth/IPrincipal.cs
+++ trunk/src/base/common/security/auth/IPrincipal.cs
@@ -15,5 +15,50 @@ namespace Nohros.Security.Auth
         /// Gets the name of the principal.
         /// </summary>
         string Name { get; }
+
+        /// <summary>
+        /// Determines whether this instance of <see cref="IPrincipal"/> and a specified object, which
+        /// must also be a <see cref="IPrincipal"/> object, refers to the same permission.
+        /// </summary>
+        /// <param name="obj">An object.</param>
+        /// <returns>true if <paramref name="obj"/> is a <see cref="IPrincipal"/> and its value is
+        /// the same as this instance; otherwise, false.</returns>
+        /// <remarks>
+        /// This methods should not throw any exception, even if the specified object is null.
+        /// </remarks>
+        bool Equals(object obj);
+
+        /// <summary>
+        /// Determines whether this instance of <see cref="IPrincipal"/> and another specified
+        /// <see cref="IPrincipal"/> refers to the same permission.
+        /// </summary>
+        /// <param name="principal">A <see cref="IPrincipal"/> object.</param>
+        /// <returns>true if the value of the <paramref name="perm"/> parameter is the same as this
+        /// instance; otherwise, false.</returns>
+        /// <remarks>
+        /// This methods should not throw any exception, even if the specified permission is null.
+        /// </remarks>
+        bool Equals(IPrincipal principal);
+
+        /// <summary>
+        /// Gets the hash code value for this permission object.
+        /// </summary>
+        /// <returns>The hash code for this permission object.</returns>
+        /// <remarks>
+        /// The required hash code behavior for permission objects is the followig:
+        /// <list type="bullet">
+        /// <item>Whenever it is invoked on the same permission object more than once during an execution
+        /// of a application, the GetHashCode methos must consistently return the same integer. This
+        /// integer does not remain consistent from one execution of an application to another execution
+        /// to another execution of the same application</item>
+        /// <item>
+        /// If two permission objects are equal according to the equals method, then calling the
+        /// GetHashCode method on each of the two permission obhects must produce the same integer result.
+        /// </item>
+        /// </list>
+        /// </remarks>
+        /// <seealso cref="Object.GetHashCode()"/>
+        /// <see cref="Object.Equals(System.Object)"/>
+        int GetHashCode();
     }
 }
