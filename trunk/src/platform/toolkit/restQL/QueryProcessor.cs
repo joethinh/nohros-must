fe5930a781dfd4e12cea8fde962badca5ceffee1
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-01-05 21:12:01 +0000

    Create a class that is used to process the restQL queries.

---
+++ trunk/src/platform/toolkit/restQL/QueryProcessor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Toolkit.RestQL
+{
+  /// <summary>
+  /// A class used to process a restQL query.
+  /// </summary>
+  public class QueryProcessor
+  {
+    QueryInfo query_;
+    string query_key_;
+
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QueryProcessor"/> class
+    /// by using the specified query string.
+    /// </summary>
+    /// <param name="query_key">A string that uniquely identifies a query
+    /// within the main data store.</param>
+    /// <param name="query_string">A <see cref="IDictionary"/> object
+    /// containing the parameters that will be used by the query associated
+    /// with the given <paramref name="query_key"/>.</param>
+    public QueryProcessor(QueryInfo query) {
+    }
+    #endregion
+
+    /// <summary>
+    /// Retrieve the query information from the main datastore and execute it,
+    /// using the supplied parameters.
+    /// </summary>
+    /// <returns></returns>
+    public string Process() {
+    }
+  }
+}
