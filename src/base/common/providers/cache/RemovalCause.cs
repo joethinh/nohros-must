be3c8f3d262acca3fde63ceaf55e776e32fcb0b5
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-01-05 21:14:15 +0000

    Defined the causes of cache item removal.

---
+++ src/base/common/providers/cache/RemovalCause.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Caching
+{
+  /// <summary>
+  /// The reason why a cached entry was removed.
+  /// </summary>
+  public enum RemovalCause
+  {
+    /// <summary>
+    /// The entry was manually removed by the user. This can result from the
+    /// user invoking <see cref="Cache.Remove"/>
+    /// </summary>
+    Explicit = 0,
+
+    /// <summary>
+    /// The entry was removed automatically because its key or value was
+    /// garbage-collected. This can occur when the cache is full.
+    /// </summary>
+    Collected = 1,
+
+    /// <summary>
+    /// The entry's expiration timestamp has passed.
+    /// </summary>
+    Expired = 2
+  }
+}
