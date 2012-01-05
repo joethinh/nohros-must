3ce7d02147ca5970ad72359cba91354d296bd636
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-01-05 21:10:42 +0000

    Created a class that abstract the concepts of a query.

---
+++ trunk/src/platform/toolkit/restQL/Query.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Nohros.Data;
+
+namespace Nohros.Toolkit.RestQL
+{
+  /// <summary>
+  /// A class that contains informations about a particular query.
+  /// </summary>
+  public class Query : ParameterizedString
+  {
+    ParameterizedString query_;
+    IDictionary<string, string> parms_;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QueryProcessor"/> class
+    /// by using the specified query string.
+    /// </summary>
+    /// <param name="query_key">A string that uniquely identifies a query
+    /// within the main data store.</param>
+    /// <param name="parms">A <see cref="IDictionary&lt;TKey, TValue>"/> object
+    /// containing the parameters that should be bound to the query.</param>
+    public Query(): base() {
+      query_ = query;
+    }
+  }
+}
