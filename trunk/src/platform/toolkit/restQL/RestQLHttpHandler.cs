f339d522a7f5348df66d000828981e9d46e1718a
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-01-05 21:12:49 +0000

    Defined a HTTP Handler that is used to process HTTP restQL requests

---
+++ trunk/src/platform/toolkit/restQL/RestQLHttpHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Text;
+
+namespace Nohros.Toolkit.RestQL
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  public class RestQLHttpHandler : IHttpHandler
+  {
+    /// <summary>
+    /// Process the HTTP request.
+    /// </summary>
+    /// <param name="context">
+    /// As <see cref="HttpContext"/> object that provides references to the
+    /// intrinsic server objects used to service HTTP requests.
+    /// </param>
+    public void ProcessRequest(HttpContext context) {
+    }
+
+    /// <summary>
+    /// Gets a value indicationg if this object could be pooled.
+    /// </summary>
+    public bool IsReusable {
+      get { return true; }
+    }
+  }
+}
