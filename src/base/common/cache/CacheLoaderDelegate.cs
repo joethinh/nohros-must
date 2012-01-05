3dd264deca6c57e1cfdf796acc0517b1bc5a460c
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-01-05 21:16:57 +0000

    Defined the methods that is used to automatically load items in the cache using the cache key.

---
+++ src/base/common/cache/CacheLoaderDelegate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Caching
+{
+  /// <summary>
+  /// Defines the signature of the method that are used to populate a
+  /// <see cref="Cache&lt;V&gt;"/>.
+  /// </summary>
+  public delegate V CacheLoaderDelegate<V>(string key);
+}
