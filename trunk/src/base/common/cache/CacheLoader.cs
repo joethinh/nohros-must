48e68b15ddc76f7bdcf10ba37787c70f9bfdc014
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-01-05 21:16:57 +0000

    Defined the methods that is used to automatically load items in the cache using the cache key.

---
+++ trunk/src/base/common/cache/CacheLoader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Caching
+{
+  public abstract class CacheLoader<V>
+  {
+    #region CacheLoaderDelegateToCacheLoader
+    class CacheLoaderDelegateToCacheLoader<V>: CacheLoader<V>
+    {
+      CacheLoaderDelegate<V> loader_;
+
+      /// <summary>
+      /// Initializes a new instance of the
+      /// <see cref="CacheLoaderDelegateToCacheLoader"/> class by using the
+      /// specified <see cref="CacheLoaderDelegate&gt;K, V&lt;"/>
+      /// </summary>
+      /// <param name="loader"></param>
+      public CacheLoaderDelegateToCacheLoader(
+        CacheLoaderDelegate<V> loader) {
+        loader_ = loader;
+      }
+
+      /// <inheritdoc/>
+      public override V Load(string key) {
+        return loader_(key);
+      }
+    }
+    #endregion
+
+    /// <summary>
+    /// Returns a <see cref="CacheLoader"/> which creates values by applying
+    /// a <see cref="CacheLoaderDelegate"/> using a key.
+    /// </summary>
+    /// <param name="loader">A <see cref="CacheLoaderDelegate"/> that
+    /// is used to compute the value from the key.</param>
+    /// <returns>A <see cref="CacheLoader"/> wich creates values by applying
+    /// the <paramref name="loader"/> to the key.</returns>
+    public static CacheLoader<V> From(CacheLoaderDelegate<V> loader) {
+      return new CacheLoaderDelegateToCacheLoader<V>(loader);
+    }
+
+    /// <summary>
+    /// Computes or retrieves the value correspnoding to <paramref name="key"/>.
+    /// </summary>
+    /// <param name="key">The key whose value should be loaded; will never
+    /// be null.</param>
+    /// <returns>The value associated with <paramref name="key"/>; may not be
+    /// null.</returns>
+    public abstract V Load(string key);
+  }
+}
