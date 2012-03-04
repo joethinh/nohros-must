df7102724b0c91be8fa89a23f1a4cc7abc0be718
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-03-04 17:59:30 +0000

    Created a factory used to build the authentication classes.

---
+++ src/base/common/security/auth/LoginFactory.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+using Nohros.Data.Collections;
+using Nohros.Configuration;
+
+namespace Nohros.Security.Auth
+{
+  /// <summary>
+  /// A factory responsible to create and configure all the related login
+  /// objects.
+  /// </summary>
+  public class LoginFactory
+  {
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LoginFactory"/> class.
+    /// </summary>
+    public LoginFactory() { }
+    #endregion
+  }
+}
