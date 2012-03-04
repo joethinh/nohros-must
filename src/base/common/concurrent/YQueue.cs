548d009cd95c56227693d10bb85bc69337c4b91b
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-03-04 17:03:10 +0000

    common project update

--- src/base/common/concurrent/YQueue.cs
+++ src/base/common/concurrent/YQueue.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Threading;
 using System.Collections.Generic;
+
+using Nohros.Resources;
 
 namespace Nohros.Concurrent
 {
@@ -15,10 +18,9 @@ namespace Nohros.Concurrent
     #region Chunk
     class Chunk
     {
-      T[] values_;
-      Chunk previous_;
-      Chunk next_;
-      volatile int current_pos_;
+      public T[] values;
+      public volatile int tail_pos, head_pos;
+      public volatile Chunk next;
 
       #region .ctor
       /// <summary>
@@ -28,70 +30,37 @@ namespace Nohros.Concurrent
       /// <param name="capacity">The number of elements that the chunk can
       /// hold.</param>
       public Chunk(int capacity) {
-        values_ = new T[capacity];
-        current_pos_ = 0;
-        previous_ = null;
-        next_ = null;
+        values = new T[capacity];
+        head_pos = 0;
+        tail_pos = 0;
+        next = null;
       }
       #endregion
-
-      /// <summary>
-      /// Gets or sets the previous chunk.
-      /// </summary>
-      public Chunk Previous {
-        get { return previous_; }
-        set { previous_ = value; }
-      }
-
-      /// <summary>
-      /// Gets or sets the next chunk.
-      /// </summary>
-      public Chunk Next {
-        get { return next_; }
-        set { next_ = value; }
-      }
-
-      /// <summary>
-      /// Gets the values that this chunk contains.
-      /// </summary>
-      public T[] Values {
-        get { return values_; }
-      }
     }
     #endregion
 
     int granularity_;
-    volatile int head_current_pos_, tail_current_pos_;
-    volatile int head_end_pos, tail_end_pos_;
-    Chunk head_chunk_;
-    Chunk tail_chunk_;
-
-    static Chunk divider_;
+    volatile Chunk head_chunk_, tail_chunk_, divider_;
 
     // People are likely to produce and consume at similar rates. In this
     // scenario holding onto the most recently freed chunk saves us from
     // having to instantiate new chunks.
-    AtomicReference<Chunk> spare_chunk_;
+    volatile Chunk spare_chunk_;
 
     #region .ctor
     /// <summary>
-    /// Initializes the static variables.
-    /// </summary>
-    static YQueue() {
-      divider_ = new Chunk(0);
-    }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="YQueue"/> class by using
-    /// the specified granularity.
+    /// Initializes a new instance of the <see cref="YQueue&lt;T&gt;"/> class
+    /// by using the specified granularity.
     /// </summary>
     /// <param name="granularity">A number that defines the granularity of the
     /// queue(how many pushes have to be done till actual memory allocation
     /// is required).</param>
     public YQueue(int granularity) {
       granularity_ = granularity;
+      divider_ = new Chunk(0);
+      divider_.next = tail_chunk_;
       head_chunk_ = tail_chunk_ = divider_;
-      head_pos_ = tail_pos_ = 0;
+      spare_chunk_ = null;
     }
     #endregion
 
@@ -101,31 +70,56 @@ namespace Nohros.Concurrent
     /// <param name="element">The element to be added to the back end of the
     /// queue.</param>
     public void Enqueue(T element) {
-      int tail_current_pos = tail_current_pos_;
+      int tail_pos = tail_chunk_.tail_pos;
 
-      if (++tail_current_pos < granularity_) {
-        // Add the element to the back end of the current tail chunk.
-        tail_chunk_.Values[tail_current_pos] = element;
+      // If either the queue is not empty or the tail chunk is not full, adds
+      // the specified element to the back end of the current tail chunk.
+      if (tail_chunk_ != divider_ && ++tail_pos < granularity_) {
+        tail_chunk_.values[tail_pos] = element;
+
+        // Prevents any kind of instruction reorderring or caching.
+        Thread.MemoryBarrier();
 
         // "Commit" the newly added item and "publish" it atomically
         // to the consumer thread.
-        tail_current_pos_ = tail_current_pos;
+        tail_chunk_.tail_pos = tail_pos;
         return;
       }
 
-      // The tail chunk is full, create a new one.
-      Chunk spare_chunk = spare_chunk_.Exchange(null);
-      if (spare_chunk == null) {
-        spare_chunk = new Chunk(granularity_);
-      }
+      // Create a new chunk if a cached one does not exists yet and links it
+      // to the current last node.
+      Chunk chunk = (spare_chunk_ == null) ?
+        new Chunk(granularity_) : spare_chunk_;
+      tail_chunk_.next = chunk;
 
-      // Append the newly created chunk to the queue chain.
-      //
-      // NOTE: We need to set the previous chunk of the newly created
-      // chunk before set the next chunk of the tail to keep the
-      // queue state consistent for the consumer thread.
-      spare_chunk.Previous = tail_chunk_;
-      tail_chunk_.Next = spare_chunk;
+      // Reset the chunk and append the specified element to the first slot.
+      chunk.tail_pos = 0; // An unconsumed element is added to the first slot.
+      chunk.head_pos = 0;
+      chunk.next = null;
+      chunk.values[0] = element;
+
+      // At this point the newly created chunk(or the last cached chunk) is
+      // not yet shared, but still private to the producer; the consumer will
+      // not follow the linked chunk unless the value of |tail_chunk_| says
+      // it may follow. The line above "commit" the update and publish it
+      // atomically to the consumer thread.
+      tail_chunk_ = tail_chunk_.next;
+
+      // Performs a lazy cleanup of now-unused nodes. Because we always stop
+      // before |divider_|,this can't conflict with anything the consumer
+      // might be doing later in the list. Each time we read |divider_|, we
+      // see it either before or after any concurrent update by the consumer,
+      // both if which let the producer see the list in a consistent state.
+      while (head_chunk_.head_pos > head_chunk_.tail_pos &&
+        head_chunk_ != divider_) {
+
+        head_chunk_ = head_chunk_.next;
+
+        // |head_chunk_| has been more recently used than |spare_chunk_|, so
+        // for cache reasons we'll get rid of the spare and use |head_chunk_|
+        // as the spare.
+        spare_chunk_ = head_chunk_;
+      }
     }
 
     /// <summary>
@@ -137,20 +131,61 @@ namespace Nohros.Concurrent
     /// <exception cref="InvalidOperationException">The
     /// <see cref="YQueue&lt;T&gt;"/> is empty.</exception>
     public T Dequeue() {
-      if (divider_ != tail_chunk_) {
+      T t;
+      bool ok = Dequeue(out t);
+      if (!ok) {
+        throw new InvalidOperationException(
+          StringResources.InvalidOperation_EmptyQueue);
       }
+      return t;
+    }
 
-      if (++head_pos_ == granularity_) {
-        Chunk chunk = head_chunk_;
-        head_chunk_ = head_chunk_.Next;
-        head_chunk_.Previous = null;
-        head_pos_ = 0;
+    /// <summary>
+    /// Removes and returns the object at the beginning of the
+    /// <see cref="YQueue&lt;T&gt;"/>.
+    /// </summary>
+    /// <param name="t">When this method returns, contains the object that was
+    /// removed from the beginning of the <see cref="YQueue&lt;T&gt;"/>, if
+    /// the object was successfully removed; otherwise the default value
+    /// of the type <typeparamref name="T"/>.</param>
+    /// <returns><c>true</c> if the queue is not empty and the object at the
+    /// beginning of it was successfully removed; otherwise, false.
+    /// </returns>
+    public bool Dequeue(out T t) {
+      // checks if the queue is empty
+      while (divider_ != tail_chunk_) {
+        // The chunks that sits between the |divider_| and the |tail_chunk_|,
+        // excluding the |divider_| and including the |tail_chunk_|, are
+        // unconsumed.
+        Chunk current_chunk = divider_.next;
 
-        // 'chunk' has been more recently used than 'spare_chunk_', so for
-        // cache reasons we'll get rid of the spare and use 'chunk' as the 
-        // spare.
-        spare_chunk_.Exchange(chunk);
+        int head_pos, tail_pos;
+        head_pos = current_chunk.head_pos;
+        tail_pos = current_chunk.tail_pos;
+        
+        if (head_pos > tail_pos) {
+          // we have reached the end of the chunk, go to the next.
+          if (tail_pos == granularity_) {
+            divider_ = current_chunk.next;
+            continue;
+          } else {
+            // we already consume all the available itens.
+            t = default(T);
+            return false;
+          }
+        } else {
+          // Ensure that we are reading the freshness value.
+          Thread.MemoryBarrier();
+
+          // Here the |head_pos| is less than or equals to |tail_pos|, get
+          // the first unconsumed element and increments |head_pos| to publish
+          // that the queue item was removed.
+          t = current_chunk.values[current_chunk.head_pos++];
+          return true;
+        }
       }
+      t = default(T);
+      return false;
     }
   }
 }
