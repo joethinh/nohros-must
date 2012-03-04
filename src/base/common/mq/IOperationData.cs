550bcecf548c06a30604f124ba80c5992d862bd9
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-03-04 17:49:57 +0000

    Created an abstraction of the message queue.

---
+++ src/base/common/mq/IOperationData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.MessageQueue
+{
+  /// <summary>
+  /// Defines a class that constains the data to be used when performing a
+  /// queue opration
+  /// </summary>
+  public interface IOperationData
+  {
+  }
+}
