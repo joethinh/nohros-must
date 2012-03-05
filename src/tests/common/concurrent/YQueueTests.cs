126df471a8555be4ab44d5ee5cf6687311c14d53
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-03-05 15:36:35 +0000

    Created a unit test for the YQueue.

---
+++ src/tests/common/concurrent/YQueueTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Nohros.Concurrent
+{
+  [TestFixture]
+  public class YQueueTests
+  {
+    [Test]
+    public void ShouldEnqueueAndDequeueInOrder() {
+      YQueue<long> queue = new YQueue<long>(1);
+      queue.Enqueue(10);
+      Assert.AreEqual(10, queue.Dequeue());
+
+      queue.Enqueue(1);
+      queue.Enqueue(2);
+      queue.Enqueue(3);
+      Assert.AreEqual(1, queue.Dequeue());
+      Assert.AreEqual(2, queue.Dequeue());
+      Assert.AreEqual(3, queue.Dequeue());
+
+      queue.Enqueue(4);
+      queue.Enqueue(5);
+      Assert.AreEqual(4, queue.Dequeue());
+
+      queue.Enqueue(3);
+      Assert.AreEqual(5, queue.Dequeue());
+      Assert.AreEqual(3, queue.Dequeue());
+    }
+  }
+}
