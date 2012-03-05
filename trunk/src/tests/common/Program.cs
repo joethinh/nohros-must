39fe04ec701fcb9180d172ba07faf03ccae1e31b
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-03-05 15:20:26 +0000

    Created some tests for the concurrent classes.

---
+++ trunk/src/tests/common/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Logging.Tests
+{
+  public static class Program
+  {
+    public static void Main() {
+      Log4NetFileLogger logger = new Log4NetFileLogger("nohros.logger");
+      logger.Configure();
+
+      logger.Info("logging-message");
+    }
+  }
+}
---
+++ trunk/src/tests/common/concurrent/AtomicLongTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Nohros.Concurrent.Tests
+{
+  [TestFixture]
+  public class AtomicLongTests
+  {
+    [Test]
+    public void ShouldAtomicallyAdd() {
+      AtomicLong atl = new AtomicLong(0);
+      long lg = atl.Add(1);
+      Assert.AreEqual(1, lg);
+    }
+
+    [Test]
+    public void ShouldAtomiccallyCompareAndExchange() {
+      AtomicLong atl = new AtomicLong(69);
+      long lg = atl.CompareExchange(69, 70);
+      Assert.AreEqual(69, lg);
+
+      atl = new AtomicLong(80);
+      lg = atl.CompareExchange(85, 90);
+      Assert.AreEqual(lg, 80);
+
+    }
+
+    [Test]
+    public void ShouldAtomicallyDecrement() {
+      AtomicLong atl = new AtomicLong(15);
+      long lg = atl.Decrement();
+      Assert.AreEqual(14, lg);
+    }
+
+    [Test]
+    public void ShouldAtomicallyExchange() {
+      AtomicLong atl = new AtomicLong(10);
+      long lg = atl.Exchange(21);
+      Assert.AreEqual(10, lg);
+    }
+
+    [Test]
+    public void ShouldAtomicallyIncrement() {
+      AtomicLong atl = new AtomicLong(45);
+      long lg = atl.Increment();
+      Assert.AreEqual(46, lg);
+    }
+
+    [Test]
+    public void ShouldGetTheValue() {
+      AtomicLong atl = new AtomicLong();
+      Assert.AreEqual(0, atl.Value);
+
+      atl = new AtomicLong(15);
+      Assert.AreEqual(15, atl.Value);
+    }
+  }
+}
