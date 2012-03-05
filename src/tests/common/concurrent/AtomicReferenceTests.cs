807f7a295188fba7e9d4094657678d8aa9373e26
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-03-05 15:20:26 +0000

    Created some tests for the concurrent classes.

---
+++ src/tests/common/concurrent/AtomicReferenceTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Nohros.Concurrent.Tests
+{
+  [TestFixture]
+  public class AtomicReferenceTests
+  {
+    public class ReferenceMock {
+      public ReferenceMock(string value) {
+        Value = value;
+      }
+      public string Value;
+    }
+
+    public ReferenceMock Reference {
+      get { return new ReferenceMock("reference-mock"); }
+    }
+
+    [Test]
+    public void ShouldUpdateWhenComparisonIsTrueAndReturnOldValue() {
+      ReferenceMock reference = Reference;
+
+      AtomicReference<ReferenceMock> ar =
+        new AtomicReference<ReferenceMock>(reference);
+
+      ReferenceMock new_mock = new ReferenceMock("new-mock");
+      ReferenceMock old_mock = ar.CompareExchange(reference, new_mock);
+      Assert.AreEqual(new_mock, ar.Value);
+      Assert.AreEqual(reference, old_mock);
+    }
+
+    [Test]
+    public void ShouldNotUpdateWhenComparisonIsFalseAndReturnOldValue() {
+      ReferenceMock reference = Reference;
+
+      AtomicReference<ReferenceMock> ar =
+        new AtomicReference<ReferenceMock>(reference);
+
+      ReferenceMock new_mock = new ReferenceMock("new-mock");
+      ReferenceMock old_mock = ar.CompareExchange(new_mock, new_mock);
+      Assert.AreEqual(reference, ar.Value);
+      Assert.AreEqual(reference, old_mock);
+    }
+
+    [Test]
+    public void ShouldExchangeAndReturnOldValue() {
+      ReferenceMock reference = Reference;
+
+      AtomicReference<ReferenceMock> ar =
+        new AtomicReference<ReferenceMock>(reference);
+
+      ReferenceMock new_mock = new ReferenceMock("new_mock");
+      ReferenceMock old_mock = ar.Exchange(new_mock);
+      Assert.AreEqual(new_mock, ar.Value);
+      Assert.AreEqual(reference, old_mock);
+    }
+  }
+}
