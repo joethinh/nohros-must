b05013f6983ecba7de27b0aa122ec16f8574a64b
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-03-06 18:02:50 +0000

    Created the interface for the token mapper and token mapper factory classes.

---
+++ src/platform/toolkit/restQL/ITokenPrincipalMapperFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Toolkit.RestQL
+{
+  /// <summary>
+  /// <para>
+  /// This interface implies a constructor with no parameters.
+  /// </para>
+  /// </summary>
+  public interface ITokenPrincipalMapperFactory
+  {
+    /// <summary>
+    /// Creates a <see cref="ITokenPrincipalMapper"/> object using the
+    /// specified settings object.
+    /// </summary>
+    /// <returns>A <see cref="ITokenPrincipalMapper"/> object.</returns>
+    /// <exception cref=""
+    ITokenPrincipalMapper CreateTokenPrincipalMapper(RestQLSettings settings);
+  }
+}
