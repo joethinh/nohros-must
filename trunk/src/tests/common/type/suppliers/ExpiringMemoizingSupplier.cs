da052301e1d128e6a9321ae33593c7452c7b2b88
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-03-08 22:29:41 +0000

    Added test classes for the suppliers.

---
+++ trunk/src/tests/common/type/suppliers/ExpiringMemoizingSupplier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Nohros.Tests
+{
+  [TestFixture]
+  public class ExpiringMemoizingSupplierTests
+  {
+    long duration = 5; // seconds
+
+    [Test]
+    public void ShouldReturnSameObjectWhileItIsNotExpired() {
+      TSupplierMock supplier = new TSupplierMock("supplier");
+      ExpiringMemoizingSupplier<TMock> expiring_supplier =
+        new ExpiringMemoizingSupplier<TMock>(supplier, duration,
+                                                     TimeUnit.Seconds);
+      TMock instance = expiring_supplier.Supply();
+      Assert.AreEqual(instance, expiring_supplier.Supply());
+      Thread.Sleep(3*1000);
+      Assert.AreEqual(instance, expiring_supplier.Supply());
+
+      Thread.Sleep(3*1000);
+      Assert.AreNotEqual(instance, expiring_supplier.Supply());
+    }
+  }
+}
