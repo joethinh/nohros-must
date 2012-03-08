5b69a0ba73b8ae655a0e1acf5a09bef7bb23dca0
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-03-08 22:29:41 +0000

    Added test classes for the suppliers.

---
+++ src/tests/common/type/suppliers/MemoizingSupplierTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Nohros.Tests
+{
+  [TestFixture]
+  public class MemoizingSupplierTests
+  {
+    [Test]
+    public void ShouldReturnTheSameObjectAfterTheFirstCall() {
+      TSupplierMock supplier = new TSupplierMock("mock-supplier");
+      MemoizingSupplier<TMock> memoizing =
+        new MemoizingSupplier<TMock>(supplier);
+
+      Assert.AreEqual(memoizing.Supply(), memoizing.Supply());
+    }
+  }
+}
---
+++ src/tests/common/type/suppliers/Mocks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Tests
+{
+  internal class TMock
+  {
+    public string Name { get; set; }
+
+    public TMock(string name) {
+      Name = name;
+    }
+  }
+
+  internal class TSupplierMock: ISupplier<TMock>
+  {
+    public string Name { get; set; }
+
+    public TSupplierMock(string name) { }
+
+    public TMock Supply() {
+      return new TMock(Name);
+    }
+  }
+}
