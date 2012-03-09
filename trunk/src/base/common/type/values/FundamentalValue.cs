50a47a0290c9b896b35ad7580bd3f7984087d0fb
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-03-09 22:48:57 +0000

    Overrided the methods [fundamental type] GetAsIn(...) in the FundamentalValue class.

--- trunk/src/base/common/type/values/FundamentalValue.cs
+++ trunk/src/base/common/type/values/FundamentalValue.cs
@@ -5,133 +5,155 @@ using System.Runtime.InteropServices;
 
 namespace Nohros
 {
-    /// <summary>
-    /// Represents the simple fundamental types of values.
-    /// </summary>
-    public class FundamentalValue : Value
+  /// <summary>
+  /// Represents the simple fundamental types of values.
+  /// </summary>
+  public class FundamentalValue: Value
+  {
+    [StructLayout(LayoutKind.Explicit)]
+    struct FundamentalValue_
     {
-        [StructLayout(LayoutKind.Explicit)]
-        struct FundamentalValue_
-        {
-            [FieldOffset(0)]
-            public bool boolean_value_;
-            [FieldOffset(0)]
-            public int integer_value_;
-            [FieldOffset(0)]
-            public double real_value_;
-        }
-
-        FundamentalValue_ in_value_;
-
-        #region .ctor
-        /// <summary>
-        /// Initializes a new instance_ of the FundamentalCalss by using the specified boolean value.
-        /// </summary>
-        /// <param name="in_value">The underlying value</param>
-        public FundamentalValue(bool in_value)
-            : base(ValueType.TYPE_BOOLEAN)
-        {
-            in_value_ = new FundamentalValue_();
-            in_value_.boolean_value_ = in_value;
-        }
-
-        /// <summary>
-        /// Initializes a new instance_ of the FundamentalCalss by using the specified boolean value.
-        /// </summary>
-        /// <param name="in_value">The underlying value</param>
-        public FundamentalValue(int in_value)
-            : base(ValueType.TYPE_INTEGER)
-        {
-            in_value_ = new FundamentalValue_();
-            in_value_.integer_value_ = in_value;
-        }
-
-        /// <summary>
-        /// Initializes a new instance_ of the FundamentalCalss by using the specified boolean value.
-        /// </summary>
-        /// <param name="in_value">The underlying value</param>
-        public FundamentalValue(double in_value)
-            : base(ValueType.TYPE_REAL)
-        {
-            in_value_ = new FundamentalValue_();
-            in_value_.real_value_ = in_value;
-        }
-        #endregion
-
-        public override bool GetAsBoolean(out bool out_value)
-        {
-            if (IsType(ValueType.TYPE_BOOLEAN))
-                out_value = in_value_.boolean_value_;
-            else
-                out_value = default(bool);
-
-            return IsType(ValueType.TYPE_BOOLEAN);
-        }
-
-        public override bool GetAsInteger(out int out_value)
-        {
-            if (IsType(ValueType.TYPE_INTEGER))
-                out_value = in_value_.integer_value_;
-            else
-                out_value = default(int);
-
-            return IsType(ValueType.TYPE_INTEGER);
-        }
-
-        public override bool GetAsReal(out double out_value)
-        {
-            if (IsType(ValueType.TYPE_REAL))
-                out_value = in_value_.real_value_;
-            else
-                out_value = default(double);
-
-            return IsType(ValueType.TYPE_REAL);
-        }
-
-        public override IValue DeepCopy()
-        {
-            switch(ValueType) {
-                case ValueType.TYPE_BOOLEAN:
-                    return CreateBooleanValue(in_value_.boolean_value_);
-
-                case ValueType.TYPE_INTEGER:
-                    return CreateIntegerValue(in_value_.integer_value_);
-
-                case ValueType.TYPE_REAL:
-                    return CreateRealValue(in_value_.real_value_);
-
-                default:
-                    return null;
-            }
-        }
-
-        public override bool Equals(IValue other)
-        {
-            if (other.ValueType != ValueType)
-                return false;
-
-            switch(ValueType) {
-                case ValueType.TYPE_BOOLEAN:
-                    {
-                        bool lhs, rhs;
-                        return GetAsBoolean(out lhs) && other.GetAsBoolean(out rhs) && lhs == rhs;
-                    }
-
-                case ValueType.TYPE_INTEGER:
-                    {
-                        int lhs, rhs;
-                        return GetAsInteger(out lhs) && other.GetAsInteger(out rhs) && lhs == rhs;
-                    }
-
-                case ValueType.TYPE_REAL:
-                    {
-                        double lhs, rhs;
-                        return GetAsReal(out lhs) && other.GetAsReal(out rhs) && lhs == rhs;
-                    }
-
-                default:
-                    return false;
-            }
-        }
+      [FieldOffset(0)]
+      public bool boolean_value_;
+      [FieldOffset(0)]
+      public int integer_value_;
+      [FieldOffset(0)]
+      public double real_value_;
     }
+
+    FundamentalValue_ in_value_;
+
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance_ of the FundamentalCalss by using the specified boolean value.
+    /// </summary>
+    /// <param name="in_value">The underlying value</param>
+    public FundamentalValue(bool in_value)
+      : base(ValueType.TYPE_BOOLEAN) {
+      in_value_ = new FundamentalValue_();
+      in_value_.boolean_value_ = in_value;
+    }
+
+    /// <summary>
+    /// Initializes a new instance_ of the FundamentalCalss by using the specified boolean value.
+    /// </summary>
+    /// <param name="in_value">The underlying value</param>
+    public FundamentalValue(int in_value)
+      : base(ValueType.TYPE_INTEGER) {
+      in_value_ = new FundamentalValue_();
+      in_value_.integer_value_ = in_value;
+    }
+
+    /// <summary>
+    /// Initializes a new instance_ of the FundamentalCalss by using the specified boolean value.
+    /// </summary>
+    /// <param name="in_value">The underlying value</param>
+    public FundamentalValue(double in_value)
+      : base(ValueType.TYPE_REAL) {
+      in_value_ = new FundamentalValue_();
+      in_value_.real_value_ = in_value;
+    }
+    #endregion
+
+    /// <inheritdoc/>
+    public override bool GetAsBoolean(out bool out_value) {
+      if (IsType(ValueType.TYPE_BOOLEAN))
+        out_value = in_value_.boolean_value_;
+      else
+        out_value = default(bool);
+
+      return IsType(ValueType.TYPE_BOOLEAN);
+    }
+
+    /// <inheritdoc/>
+    public override bool GetAsInteger(out int out_value) {
+      if (IsType(ValueType.TYPE_INTEGER))
+        out_value = in_value_.integer_value_;
+      else
+        out_value = default(int);
+
+      return IsType(ValueType.TYPE_INTEGER);
+    }
+
+    /// <inheritdoc/>
+    public override bool GetAsReal(out double out_value) {
+      if (IsType(ValueType.TYPE_REAL))
+        out_value = in_value_.real_value_;
+      else
+        out_value = default(double);
+
+      return IsType(ValueType.TYPE_REAL);
+    }
+
+    /// <inheritdoc/>
+    public override bool GetAsBoolean() {
+      bool value;
+      if(!GetAsBoolean(out value)) {
+        // TODO: Add a message to the exception
+        throw new InvalidCastException();
+      }
+      return value;
+    }
+
+    /// <inheritdoc/>
+    public override int GetAsInteger() {
+      int value;
+      if (!GetAsInteger(out value)) {
+        // TODO: Add a message to the exception
+        throw new InvalidCastException();
+      }
+      return value;
+    }
+
+    /// <inheritdoc/>
+    public override double GetAsReal() {
+      double value;
+      if (!GetAsReal(out value)) {
+        // TODO: Add a message to the exception
+        throw new InvalidCastException();
+      }
+      return value;
+    }
+
+    public override IValue DeepCopy() {
+      switch (ValueType) {
+        case ValueType.TYPE_BOOLEAN:
+          return CreateBooleanValue(in_value_.boolean_value_);
+
+        case ValueType.TYPE_INTEGER:
+          return CreateIntegerValue(in_value_.integer_value_);
+
+        case ValueType.TYPE_REAL:
+          return CreateRealValue(in_value_.real_value_);
+
+        default:
+          return null;
+      }
+    }
+
+    public override bool Equals(IValue other) {
+      if (other.ValueType != ValueType)
+        return false;
+
+      switch (ValueType) {
+        case ValueType.TYPE_BOOLEAN: {
+            bool lhs, rhs;
+            return GetAsBoolean(out lhs) && other.GetAsBoolean(out rhs) && lhs == rhs;
+          }
+
+        case ValueType.TYPE_INTEGER: {
+            int lhs, rhs;
+            return GetAsInteger(out lhs) && other.GetAsInteger(out rhs) && lhs == rhs;
+          }
+
+        case ValueType.TYPE_REAL: {
+            double lhs, rhs;
+            return GetAsReal(out lhs) && other.GetAsReal(out rhs) && lhs == rhs;
+          }
+
+        default:
+          return false;
+      }
+    }
+  }
 }
