64f32f9b86de0c6d0b026cebd172356f5e8057df
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-03-10 22:55:22 +0000

    Moved the defintion of the properties Options and AssemblyLocation from the ProviderNode class to the IProviderNode interface.

--- src/base/common/configuration/common/provider/IProviderNode.cs
+++ src/base/common/configuration/common/provider/IProviderNode.cs
@@ -16,10 +16,69 @@ namespace Nohros.Configuration
     string Name { get; }
 
     /// <summary>
-    /// Gets the assembly-qualified name of the provider type, which includes the name of the assembly
-    /// from which the provider type was loaded.
+    /// Gets the assembly-qualified name of the provider type, which includes
+    /// the name of the assembly from which the provider type was loaded.
     /// </summary>
     /// <seealso cref="System.Type.AssemblyQualifiedName"/>
     string Type { get; }
+
+    /// <summary>
+    /// Gets a string representing the fully qualified path to the directory
+    /// where the assembly associated with the provider is located.
+    /// </summary>
+    /// <value>
+    /// The fully qualified path to the folder where the provider assembly is
+    /// stored.
+    /// </value>
+    /// <remarks>
+    /// The assembly location must be an absolute path or a path relative to
+    /// the configuration file.
+    /// <para>
+    /// The default location returned by this property is the application base
+    /// directory.
+    /// </para>
+    /// </remarks>
+    string AssemblyLocation { get; }
+
+    /// <summary>
+    /// Gets a collection of key/value pairs containing the options configured
+    /// for a provider.
+    /// </summary>
+    /// <value>
+    /// A collection of key/value pairs representing the options configured for
+    /// the provider.
+    /// </value>
+    /// <remarks>The <see cref="Options"/> property represents the options
+    /// configured for a provider by a user in the configuration repository.
+    /// The options are defined by the provider itself and control the behavior
+    /// within it. For example a provider may define options to support
+    /// debugging/testing capabilities. Options are defined using a key-value
+    /// syntax such as <c>debug=true</c>. The options should be defined using
+    /// attributes of a Xml node that has the provider node as a parent and is
+    /// named öptions". The provider stores the options as a
+    /// <see cref="IDictionary&lt;TKey, TValue&gt;"/> so that the values may
+    /// be retrieved using the key.
+    /// <para>
+    /// NOTE: There is no limit to the number of options a provider chooses to
+    /// define.
+    /// </para>
+    /// <para>
+    /// <example>
+    /// <code>
+    ///   ...
+    ///   <provider name="SomeProvider">
+    ///     <options
+    ///      debug="true"
+    ///      http-port="8080"
+    ///      server-address="192.168.0.1">
+    ///   </provider>
+    ///   ..
+    /// </code>
+    /// The debug, http-port and server-address are the options configured for
+    /// the provider "SomeProvider".
+    /// </example>
+    /// </para>
+    /// </remarks>
+    IDictionary<string, string> Options { get; }
   }
 }
