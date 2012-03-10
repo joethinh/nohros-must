16ad02bbaf95949c46ab0c7efdbd77f5034f517c
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-03-10 20:20:40 +0000

    A ISQLDataProvider is a interface that defines the methods and properties that the most relational data providers have in common.

---
+++ src/base/common/providers/data/ISQLDataProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace Nohros.Data.Providers
+{
+  /// <summary>
+  /// Defines method and properties that is used to query relational data
+  /// providers.
+  /// </summary>
+  public interface ISQLDataProvider : IDataProvider
+  {
+    /// <summary>
+    /// Gets the name of the owner of the database.
+    /// </summary>
+    string DatabaseOwner { get; }
+  }
+}
