96f6fa5624836cd0a15fcfb7e5e152920a2f9350
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-03-10 20:18:59 +0000

    Ported the concept of suppliers from the guava library. Suppliers are objects that supplies other objects, it could cache the supplied object, cache with expiration or just supply a new instance every time.

---
+++ trunk/src/base/common/type/suppliers/ISupplier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros
+{
+  /// <summary>
+  /// A class that can supply objects of a single type. Semantically, this
+  /// could be a factory, generator, builder, or something else entirely. No
+  /// guarantess are implied by this interface.
+  /// </summary>
+  public interface ISupplier<T>
+  {
+    /// <summary>
+    /// Gets an instance of the appropriate type. The returned object may or
+    /// not be a new instance, depending on the implementation.
+    /// </summary>
+    T Supply();
+  }
+}
---
+++ trunk/src/base/common/type/suppliers/SupplierOfInstance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros
+{
+  /// <summary>
+  /// A <see cref="ISupplier{T}"/> that always returns the same instance.
+  /// </summary>
+  /// <remarks><see cref="SupplierOfInstance{T}"/> is similar to
+  /// <see cref="MemoizingSupplier{T}"/>, but instead to create a instance it
+  /// uses the instance that is passed to it on the constructor.
+  /// </remarks>
+  internal class SupplierOfInstance<T> : ISupplier<T>
+  {
+    readonly T instance_;
+
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SupplierOfInstance{T}"/>
+    /// class by using the specified <paramref name="instance"/> object.
+    /// </summary>
+    /// <param name="instance">The object that will be supplied on every
+    /// call to <see cref="Supply"/></param>
+    public SupplierOfInstance(T instance) {
+      instance_ = instance;
+    }
+    #endregion
+
+    /// <inheritdoc/>
+    public T Supply() {
+      return instance_;
+    }
+  }
+}
