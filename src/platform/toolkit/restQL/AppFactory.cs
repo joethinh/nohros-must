0e0c395b301fd0ad880712184a6201133c97bc3f
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-03-10 22:38:15 +0000

    A factory used to create the main app object graph

---
+++ src/platform/toolkit/restQL/AppFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Nohros.Providers;
+using Nohros.Configuration;
+
+namespace Nohros.Toolkit.RestQL
+{
+  /// <summary>
+  /// A factory used the created the main application object graph.
+  /// </summary>
+  internal class AppFactory
+  {
+    ITokenPrincipalMapperFactory settings_;
+
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AppFactory"/>.
+    /// </summary>
+    public AppFactory() { }
+    #endregion
+
+    /// <summary>
+    /// Creates an instance of the <see cref="ITokenPrincipalMapper"/> object
+    /// using the information that is defined on the application settings.
+    /// </summary>
+    /// <param name="node"></param>
+    /// <returns>An instance of a class that implements or derives from a
+    /// class that implements the <see cref="ITokenPrincipalMapper"/>
+    /// interface.
+    /// </returns>
+    /// <remarks></remarks>
+    public ITokenPrincipalMapper CreateTokenPrincipalMapper(
+      SimpleProviderNode node) {
+      ITokenPrincipalMapperFactory factory =
+        ProviderFactory<ITokenPrincipalMapperFactory>.CreateProviderFactory(
+        node);
+      return factory.CreateTokenPrincipalMapper(node.Options);
+    }
+  }
+}
