2326ab396e1213f2ac557efb6582973f3be978ab
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-03-12 13:07:14 +0000

    Created a interface for the cache provider factories.

---
+++ trunk/src/base/common/providers/cache/ICacheProviderFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nohros.Configuration;
+
+namespace Nohros.Caching.Providers
+{
+  /// <summary>
+  /// Defines the methods and properties that is used to create instances of
+  /// the <see cref="ICacheProvider"/> class.
+  /// </summary>
+  /// <remarks>
+  /// This interface implie a constructor with no parameters.
+  /// </remarks>
+  public interface ICacheProviderFactory
+  {
+    /// <summary>
+    /// Creates an instance of the <see cref="ICacheProvider"/> using the
+    /// specified provider options.
+    /// </summary>
+    /// <param name="options">A <see cref="IDictionary{TKey,TValue}"/>
+    /// containing a collection options configured for the provider.</param>
+    /// <returns></returns>
+    ICacheProvider CreateCacheProvider(IDictionary<string, string> options);
+  }
+}
