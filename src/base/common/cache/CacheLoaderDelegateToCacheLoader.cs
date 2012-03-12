6f2dbafd9f0ea8682c3f5666e12f8ddffff0f459
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-03-12 13:04:47 +0000

    Moved the code of the CacheLoaderDelegateToCacheLoader class to a exclusive file.

---
+++ src/base/common/cache/CacheLoaderDelegateToCacheLoader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Nohros.Caching
+{
+  public abstract partial class CacheLoader<T>
+  {
+    class CacheLoaderDelegateToCacheLoader<T> : CacheLoader<T>
+    {
+      readonly CacheLoaderDelegate<T> loader_;
+
+      /// <summary>
+      /// Initializes a new instance of the
+      /// <see cref="CacheLoader{T}.CacheLoaderDelegateToCacheLoader{T}"/>
+      /// class by using the specified <see cref="CacheLoaderDelegate{T}"/>
+      /// </summary>
+      /// <param name="loader">The loader that is used to load instances of
+      /// type <typeparamref name="T"/></param>
+      public CacheLoaderDelegateToCacheLoader(
+        CacheLoaderDelegate<T> loader) {
+        loader_ = loader;
+      }
+
+      /// <inheritdoc/>
+      public override T Load(string key) {
+        return loader_(key);
+      }
+    }
+  }
+}
