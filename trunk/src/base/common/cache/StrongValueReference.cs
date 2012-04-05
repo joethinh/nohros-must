3d9812a8d11bf5ebec4dc471ccab7ab2a3f109e3
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-04-05 21:15:13 +0000

    Finished the skeletal implementation of the ICache interface.

---
+++ trunk/src/base/common/cache/StrongValueReference.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Caching
+{
+  /// <summary>
+  /// References a string value.
+  /// </summary>
+  /// <typeparam name="T">
+  /// The type of the value that is referenced.
+  /// </typeparam>
+  internal class StrongValueReference<T> : IValueReference<T>
+  {
+    readonly T referent_;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StrongValueReference"/>
+    /// class by using the specified referent value.
+    /// </summary>
+    /// <param name="referent"></param>
+    public StrongValueReference(T referent) {
+      referent_ = referent;
+    }
+
+    /// <inheritdoc/>
+    public T Value {
+      get { return referent_; }
+    }
+
+    /// <inheritdoc/>
+    public bool IsLoading {
+      get { return false; }
+    }
+
+    /// <inheritdoc/>
+    public bool IsActive {
+      get { return true; }
+    }
+
+    /// <inheritdoc/>
+    public T WaitForValue() {
+      return Value;
+    }
+  }
+}
