813398378bb25f25edf414ca3cf1cf175a91d10e
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-04-05 21:15:13 +0000

    Finished the skeletal implementation of the ICache interface.

---
+++ src/base/common/cache/Strength.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Caching
+{
+  internal sealed class Strength
+  {
+    Strength() { }
+
+    /// <summary>
+    /// Creates a <see cref="IValueReference{T}"/> for the specified
+    /// value according to the given strength type.
+    /// </summary>
+    /// <param name="strength_type">The type of strength</param>
+    public static IValueReference<T> ReferenceValue<T>(
+      AbstractCache<T>.CacheEntry<T> entry, T value,
+      StrengthType strength_type) {
+
+      switch(strength_type) {
+        case StrengthType.Strong:
+          return new StrongValueReference<T>(value);
+
+        case StrengthType.Soft:
+        case StrengthType.Weak:
+        default:
+          throw new NotImplementedException("Soft and weak values are not implemented.");
+      }
+    }
+  }
+}
---
+++ src/base/common/cache/StrengthType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Caching
+{
+  internal enum StrengthType
+  {
+    /// <summary>
+    /// Strong references.
+    /// </summary>
+    Strong = 0,
+
+    /// <summary>
+    /// Soft references.
+    /// </summary>
+    Soft = 1,
+
+    /// <summary>
+    /// Weak references.
+    /// </summary>
+    Weak = 2
+  }
+}
