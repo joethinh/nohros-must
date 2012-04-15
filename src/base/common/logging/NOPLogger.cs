f199677bde5a9a47fb66f36ce51ecd75f4cb0c58
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-04-15 18:01:24 +0000

    Code cleanup.

--- src/base/common/logging/NOPLogger.cs
+++ src/base/common/logging/NOPLogger.cs
@@ -4,84 +4,84 @@ using System.Text;
 
 namespace Nohros.Logging
 {
+  /// <summary>
+  /// No-operation implementation og <see cref="ILogger"/> interface.
+  /// </summary>
+  /// <remarks>
+  /// Since this logger does not do anything the Is[...] methods always
+  /// returns null and the level of the logger will be always Off, even if
+  /// the user set it to a diferrent value.
+  /// </remarks>
+  public class NOPLogger: ILogger
+  {
+    #region .ctor
+
     /// <summary>
-    /// No-operation implementation og <see cref="ILogger"/> interface.
+    /// Initializes a new instance of the <see cref="NOPLogger"/> class.
     /// </summary>
-    /// <remarks>
-    /// Since this logger does not do anything the Is[...] methods always
-    /// returns null and the level of the logger will be always Off, even if
-    /// the user set it to a diferrent value.
-    /// </remarks>
-    public class NOPLogger : ILogger
-    {
-        #region .ctor
+    public NOPLogger() { }
+    #endregion
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Log4NetLogger"/>.
-        /// </summary>
-        public NOPLogger() { }
-        #endregion
+    #region IsEnabled
 
-        #region IsEnabled
+    /// <inherit />
+    public bool IsDebugEnabled { get { return false; } }
 
-        /// <inherit />
-        public bool IsDebugEnabled { get { return false; } }
+    /// <inherit />
+    public bool IsErrorEnabled { get { return false; } }
 
-        /// <inherit />
-        public bool IsErrorEnabled { get { return false; } }
+    /// <inherit />
+    public bool IsFatalEnabled { get { return false; } }
 
-        /// <inherit />
-        public bool IsFatalEnabled { get { return false; } }
+    /// <inherit />
+    public bool IsInfoEnabled { get { return false; } }
 
-        /// <inherit />
-        public bool IsInfoEnabled { get { return false; } }
+    /// <inherit />
+    public bool IsWarnEnabled { get { return false; } }
 
-        /// <inherit />
-        public bool IsWarnEnabled { get { return false; } }
+    /// <inherit />
+    public bool IsTraceEnabled { get { return false; } }
 
-        /// <inherit />
-        public bool IsTraceEnabled { get { return false; } }
+    #endregion
 
-        #endregion
+    /// <inherit />
+    public void Debug(string message) { }
 
-        /// <inherit />
-        public void Debug(string message) { }
+    /// <inherit />
+    public void Debug(string message, Exception exception) { }
 
-        /// <inherit />
-        public void Debug(string message, Exception exception) { }
+    /// <inherit />
+    public void Error(string message) { }
 
-        /// <inherit />
-        public void Error(string message) { }
+    /// <inherit />
+    public void Error(string message, Exception exception) { }
 
-        /// <inherit />
-        public void Error(string message, Exception exception) { }
+    /// <inherit />
+    public void Fatal(string message) { }
 
-        /// <inherit />
-        public void Fatal(string message) { }
+    /// <inherit />
+    public void Fatal(string message, Exception exception) { }
 
-        /// <inherit />
-        public void Fatal(string message, Exception exception) { }
+    /// <inherit />
+    public void Info(string message) { }
 
-        /// <inherit />
-        public void Info(string message) { }
+    /// <inherit />
+    public void Info(string message, Exception exception) { }
 
-        /// <inherit />
-        public void Info(string message, Exception exception) { }
+    /// <inherit />
+    public void Warn(string message) { }
 
-        /// <inherit />
-        public void Warn(string message) { }
+    /// <inherit />
+    public void Warn(string message, Exception exception) { }
 
-        /// <inherit />
-        public void Warn(string message, Exception exception) { }
-
-        /// <summary>
-        /// Gets or sets the threshold level of the logger repository.
-        /// </summary>
-        /// <remarks>Always return null. Even if a user set it to a
-        /// diferent value.</remarks>
-        internal LogLevel LogLevel {
-            get { return LogLevel.Off; }
-            set { }
-        }
+    /// <summary>
+    /// Gets or sets the threshold level of the logger repository.
+    /// </summary>
+    /// <remarks>Always return null. Even if a user set it to a
+    /// diferent value.</remarks>
+    internal LogLevel LogLevel {
+      get { return LogLevel.Off; }
+      set { }
     }
+  }
 }
