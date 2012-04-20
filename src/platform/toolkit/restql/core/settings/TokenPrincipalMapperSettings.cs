f99caa4a401d2bd9102bd23dc2bd8dad68ad7b35
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-04-20 21:22:01 +0000

    Created a interface and default implementation for the settings related with the token mapper.

---
+++ src/platform/toolkit/restql/core/settings/TokenPrincipalMapperSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Toolkit.RestQL
+{
+  public class TokenPrincipalMapperSettings : Settings, ITokenPrincipalMapperSettings
+  {
+    string anonymous_token_;
+
+    /// <summary>
+    /// Initializes a new instance of the
+    /// <see cref="TokenPrincipalMapperSettings"/> class.
+    /// </summary>
+    public TokenPrincipalMapperSettings() {
+      anonymous_token_ = "anonymous";
+    }
+
+    /// <inheritdoc/>
+    public string AnonymousToken {
+      get { return anonymous_token_; }
+      protected set { anonymous_token_ = value; }
+    }
+  }
+}
