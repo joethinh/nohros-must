768fa7d321194a78e6ab3dd233c52ee69f8cbafc
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-04-20 21:21:13 +0000

    Created a interface that describe the settings common to the whole application.

---
+++ trunk/src/platform/toolkit/restql/core/settings/Settings.cs
@@ -0,0 +1,22 @@
+﻿using Nohros.Configuration;
+
+namespace Nohros.Toolkit.RestQL
+{
+  /// <summary>
+  /// The application settings.
+  /// </summary>
+  public class Settings : NohrosConfiguration, ISettings
+  {
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Settings"/> class.
+    /// </summary>
+    public Settings() {
+    }
+    #endregion
+
+    protected override void OnLoadComplete(System.EventArgs e) {
+      base.OnLoadComplete(e);
+    }
+  }
+}
