7e58dca4395a55954bdc3fa4bf757698ce37716e
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-04-20 21:19:13 +0000

    Created a interface that defines the configuration properties used by the classes related with the rest queries.

---
+++ trunk/src/platform/toolkit/restql/core/IQuerySettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Toolkit.RestQL
+{
+  /// <summary>
+  /// Contains the configuration data related with query definition.
+  /// </summary>
+  public interface IQuerySettings : ISettings
+  {
+    long QueryCacheDuration { get; }
+  }
+}
