43896449791ee87d47eb13e94bdc73acec44d079
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-04-20 21:21:13 +0000

    Created a interface that describe the settings common to the whole application.

---
+++ src/platform/toolkit/restql/core/settings/ISettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Toolkit.RestQL
+{
+  /// <summary>
+  /// Defines the common application settings.
+  /// </summary>
+  public interface ISettings
+  {
+  }
+}
