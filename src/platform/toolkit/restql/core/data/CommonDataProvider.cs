d8ab0e0bbdd8390c5e7af47d62044f3ee76cac24
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-04-20 21:24:37 +0000

    Created an description about data provider that is used to access the common restql data store.

---
+++ src/platform/toolkit/restql/core/data/CommonDataProvider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using Nohros.Data;
+using Nohros.Data.Providers;
+
+namespace Nohros.Toolkit.RestQL
+{
+  public abstract class CommonDataProvider : ICommonDataProvider
+  {
+    protected readonly string[] get_query_fields = {
+      "queryname", "querytype" ,"query" };
+
+    /// <summary>
+    /// Create an <see cref="Query"/> object by reading the data from the
+    /// goven <see cref="IDataReader"/>.
+    /// </summary>
+    /// <param name="data_reader">
+    /// A <see cref="IDataReader"/> object where the data to create the
+    /// <see cref="Query"/> object could be read from.
+    /// </param>
+    /// <returns>
+    /// A new <see cref="Query"/> object if the specified
+    /// <see cref="IDataReader"/> is readable; otherwise the value of
+    /// <see cref="Query.EmptyQuery"/> property.
+    /// </returns>
+    protected Query CreatedQueryFromDataReader(IDataReader data_reader) {
+      if (data_reader.Read()) {
+        int[] ordinals = DataReaders.GetOrdinals(data_reader, get_query_fields);
+
+        const int kQueryName = 0;
+        const int kQueryProcessor = 2;
+        const int kQuery = 3;
+
+        string name = data_reader.GetString(ordinals[kQueryName]);
+        string type = data_reader.GetString(ordinals[kQueryProcessor]);
+        string query = data_reader.GetString(ordinals[kQuery]);
+
+        return new Query(name, type, query);
+      }
+      return Query.EmptyQuery;
+    }
+
+    /// <inheritdoc/>
+    public abstract Query GetQuery(string name);
+  }
+}
---
+++ src/platform/toolkit/restql/core/data/ICommonDataProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Toolkit.RestQL
+{
+  public interface ICommonDataProvider
+  {
+    /// <summary>
+    /// Gets a <see cref="Query"/> object whose name if <paramref name="name"/>.
+    /// </summary>
+    /// <param name="name">
+    /// A string that uniquely identifies the query to get.
+    /// </param>
+    /// <returns>
+    /// A <see cref="Query"/> object whose name is <paramref name="name"/>.
+    /// </returns>
+    /// <remarks>
+    /// This method should not throw exceptions. If a <see cref="Query"/> with
+    /// name does not exists in the database this method should return an
+    /// instance of the class <see cref="Query.EmptyQuery"/>.
+    /// </remarks>
+    Query GetQuery(string name);
+  }
+}
