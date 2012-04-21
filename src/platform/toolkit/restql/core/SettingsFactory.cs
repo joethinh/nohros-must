5a30df152d10894627c0ab8cfd978b5fbfd97e4a
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-04-21 19:49:51 +0000

    Created a factory to construct the settings object graph

---
+++ src/platform/toolkit/restql/core/SettingsFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Nohros.Collections;
+using Nohros.Configuration;
+
+namespace Nohros.Toolkit.RestQL
+{
+  public partial class Settings
+  {
+    const string kRestQLSettingsFileName = "restql.config";
+    const string kRestQLRootNodeName = "restql";
+    const string kQueryProcessorProvidersNode = "query-processor";
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public ISettings CreateSettings() {
+      string settings_file_path = Path.Combine(
+        Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+        kRestQLSettingsFileName);
+
+      FileInfo settings_file_info = new FileInfo(settings_file_path);
+
+      Settings settings = new Settings();
+      settings.LoadAndWatch(settings_file_info, kRestQLRootNodeName);
+
+      query_settings_ = CreateQuerySettings(settings);
+
+      return settings;
+    }
+
+    IQuerySettings CreateQuerySettings(Settings settings) {
+      DictionaryValue<SimpleProviderNode> providers =
+        settings.SimpleProviderNodes[kQueryProcessorProvidersNode];
+
+      QuerySettings query_settings = new QuerySettings(providers.ToArray());
+    }
+  }
+}
