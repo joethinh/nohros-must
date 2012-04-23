bb6017002cc44a76dde96aa0c7a6df9b537d389f
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-04-23 21:45:42 +0000

    Improved documentation.

--- src/base/common/cache/CacheLoaderDelegate.cs
+++ src/base/common/cache/CacheLoaderDelegate.cs
@@ -4,7 +4,7 @@ namespace Nohros.Caching
 {
   /// <summary>
   /// Defines the signature of the method that are used to populate a
-  /// <see cref="Cache&lt;V&gt;"/>.
+  /// <see cref="ICache{T}"/>.
   /// </summary>
   public delegate T CacheLoaderDelegate<T>(string key);
 }
