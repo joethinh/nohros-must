86b9c6fb8aa327b40f58209e12717bf1682c8270
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-04-23 18:26:24 +0000

    Created the unit tests for the CacheLoader and CacheBuilder classes.

---
+++ trunk/src/tests/common/cache/CacheBuilderTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Nohros.Caching;
+using Nohros.Caching.Providers;
+
+namespace Nohros.Common.Caching
+{
+  [TestFixture]
+  public class CacheBuilderTests
+  {
+    [Test]
+    public void ShouldSetExpirationAndRefreshToGivenValues() {
+      CacheBuilder<string> builder = new CacheBuilder<string>();
+
+      builder.ExpireAfterWrite(1, TimeUnit.Seconds);
+      Assert.AreEqual(builder.ExpiryAfterWriteNanos,
+        TimeUnitHelper.ToNanos(1, TimeUnit.Seconds));
+
+      builder.ExpireAfterAccess(2, TimeUnit.Seconds);
+      Assert.AreEqual(builder.ExpiryAfterAccessNanos,
+        TimeUnitHelper.ToNanos(2, TimeUnit.Seconds));
+    }
+
+    [Test]
+    public void ShouldNotAcceptNegativeExpirarionOrRefreshValues() {
+      CacheBuilder<string> builder = new CacheBuilder<string>();
+
+      Assert.Throws<ArgumentOutOfRangeException>(
+        delegate()
+        {
+          builder.ExpireAfterAccess(-1, TimeUnit.Seconds);
+        });
+
+      Assert.Throws<ArgumentOutOfRangeException>(
+        delegate()
+        {
+          builder.ExpireAfterWrite(-1, TimeUnit.Seconds);
+        });
+
+      Assert.Throws<ArgumentOutOfRangeException>(
+        delegate()
+        {
+          builder.RefreshAfterWrite(-1, TimeUnit.Seconds);
+        });
+    }
+  }
+}
---
+++ trunk/src/tests/common/cache/CacheLoaders.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nohros.Caching.Providers;
+
+namespace Nohros.Caching
+{
+  class ThrowableLongCacheLoader : CacheLoader<long>
+  {
+    public override long Load(string key)
+    {
+      throw new ArgumentException();
+    }
+  }
+
+  class RandonLongCacheLoader : CacheLoader<long>
+  {
+    public override long Load(string key)
+    {
+      return new Random().Next() * 1000;
+    }
+  }
+
+  class StringCacheLoader : CacheLoader<string>
+  {
+    public override string Load(string key)
+    {
+      return "loaded-string";
+    }
+  }
+
+  class LongCacheLoader : CacheLoader<long>
+  {
+    public override long Load(string key)
+    {
+      return 30;
+    }
+  }
+
+  class LoadingCacheMock<T> : LoadingCache<T>
+  {
+    public LoadingCacheMock(CacheBuilder<T> builder)
+      : base(new CacheProviderMock(), builder) {
+    }
+  }
+}
