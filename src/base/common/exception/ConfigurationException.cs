91eaaffb18a9452886d9287cacd8497f8e0b3cee
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-04-23 21:49:11 +0000

    Moved the classes related with exceptions to exception folder.

---
+++ src/base/common/exception/ConfigurationException.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Nohros.Configuration
+{
+  /// <summary>
+  /// The exception that is thrown when a configuration error has occured.
+  /// </summary>
+  public class ConfigurationException : Exception
+  {
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConfigurationException"/>
+    /// class.
+    /// </summary>
+    public ConfigurationException() {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConfigurationException"/>
+    /// class with a specified error message.
+    /// </summary>
+    /// <param name="message">
+    /// The message that describes the error.
+    /// </param>
+    public ConfigurationException(string message) : base(message) {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConfigurationException"/>
+    /// class with a specified error message and a reference to the inner
+    /// exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">
+    /// The error message that explains the reason for the exception.
+    /// </param>
+    /// <param name="inner_exception">
+    /// The exception that is the cause of the current exception, or a null
+    /// reference if no inner exception is specified.
+    /// </param>
+    public ConfigurationException(string message, Exception inner_exception)
+      : base(message, inner_exception) {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConfigurationException"/>
+    /// class with serialized data.
+    /// </summary>
+    /// <param name="context">
+    /// The <see cref="StreamingContext"/> that contains contextual information
+    /// about the source or destination.
+    /// </param>
+    /// <param name="info">The <see cref="SerializationInfo"/> that holds the
+    /// serialized object data about the exception being thrown.
+    /// </param>
+    /// <exception cref="SerializationException">The class name is null or
+    /// <see cref="Exception.HResult"/> is zero (0).
+    /// </exception>
+    /// <exception cref="ArgumentNullException">
+    /// The info parameter is null.
+    /// </exception>
+    protected ConfigurationException(SerializationInfo info,
+      StreamingContext context) : base(info, context) {
+    }
+    #endregion
+  }
+}
