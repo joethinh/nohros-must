53036e5fce7d1a7e02387afe512921e9291db6d5
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-04-23 20:05:49 +0000

    Unit tests for AbstractConfiguration class.

---
+++ trunk/src/tests/common/configuration/AbstractConfigurationTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using NUnit.Framework;
+
+namespace Nohros.Configuration
+{
+  class AbstractConfigurationMock : AbstractConfiguration {
+    public AbstractConfigurationMock() { }
+  }
+
+  public class AbstractConfigurationTests
+  {
+    string config_file_path;
+
+    [SetUp]
+    public void Setup() {
+      config_file_path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+        "config-tests.xml");
+    }
+
+    [Test]
+    [ExpectedException(typeof(ConfigurationException))]
+    public void ShouldThrowConfigurationExceptionWhenAppConfigKeyIsMissing() {
+      AbstractConfigurationMock mock = new AbstractConfigurationMock();
+      mock.Load();
+    }
+
+    [Test]
+    public void ShouldThrowArgumentExceptionWhenArgIsNull() {
+      AbstractConfigurationMock mock = new AbstractConfigurationMock();
+
+      Assert.Throws<ArgumentNullException>(delegate()
+      {
+        mock.Load((XmlElement) null);
+      });
+
+      Assert.Throws<ArgumentNullException>(delegate() {
+        mock.Load((string)null, "missing-root-node");
+      });
+
+      Assert.Throws<ArgumentNullException>(delegate()
+      {
+        mock.LoadAndWatch((string)null, "missing-root-node");
+      });
+
+      Assert.Throws<ArgumentNullException>(delegate()
+      {
+        mock.LoadAndWatch((FileInfo)null, "missing-root-node");
+      });
+    }
+
+    [Test]
+    public void ShouldNotThrowExceptionWhenRootNodeIsMissing() {
+      AbstractConfigurationMock mock = new AbstractConfigurationMock();
+
+      Assert.DoesNotThrow(delegate()
+      {
+        mock.Load(config_file_path, null);
+      });
+
+      Assert.DoesNotThrow(delegate()
+      {
+        FileInfo fi = new FileInfo(config_file_path);
+        mock.Load(fi, null);
+      });
+
+      Assert.DoesNotThrow(delegate()
+      {
+        FileInfo fi = new FileInfo(config_file_path);
+        mock.LoadAndWatch(fi, null);
+      });
+
+      Assert.DoesNotThrow(delegate()
+      {
+        mock.LoadAndWatch(config_file_path, null);
+      });
+    }
+
+    [Test]
+    public void ShouldReturnBaseDirectoryWhenLocationIsNotSpecified() {
+      AbstractConfigurationMock mock = new AbstractConfigurationMock();
+
+      Assert.AreEqual(mock.Location, AppDomain.CurrentDomain.BaseDirectory);
+    }
+
+    [Test]
+    public void ShouldGetTheFirstNodeThatMatchesXPath() {
+      XmlDocument doc = new XmlDocument();
+      doc.LoadXml("<root><first><second></second></first></root>");
+
+      XmlNode node = doc.FirstChild;
+
+      XmlNode selected = AbstractConfiguration.SelectNode(node, "first/second");
+      Assert.IsNotNull(selected);
+      Assert.AreEqual(selected.Name, "second");
+
+      selected = AbstractConfiguration.SelectNode(node, "/first/second");
+      Assert.IsNotNull(selected);
+      Assert.AreEqual(selected.Name, "second");
+
+      selected = AbstractConfiguration.SelectNode(node, "//first/second");
+      Assert.IsNotNull(selected);
+      Assert.AreEqual(selected.Name, "second");
+    }
+  }
+}
