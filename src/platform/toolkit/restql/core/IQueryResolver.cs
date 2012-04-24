266db89cccd1badc99b4d13e3d01774ff1ce7f71
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-04-24 20:10:19 +0000

    Created a class used to resolve query names to Query objects.

---
+++ src/platform/toolkit/restql/core/IQueryResolver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Toolkit.RestQL
+{
+  /// <summary>
+  /// A <see cref="IQueryResolver"/> is used to perform query resolution.
+  /// </summary>
+  /// <remarks>
+  /// A <see cref="IQueryResolver"/> process a given string and produces a
+  /// <see cref="Query"/> object that can be sent to a
+  /// <see cref="IQueryExecutor"/> to be executed.
+  /// </remarks>
+  public interface IQueryResolver
+  {
+    /// <summary>
+    /// Resolves a query using its unique name.
+    /// </summary>
+    /// <param name="name">
+    /// The name of the query to resolve.
+    /// </param>
+    /// <returns>
+    /// The resolved query or <see cref="Query.EmptyQuery"/> if the query could
+    /// not be resolved.
+    /// </returns>
+    Query GetQuery(string name);
+
+    /// <summary>
+    /// Resolves a query using its unique name and parameters.
+    /// </summary>
+    /// <param name="name">
+    /// The query's unique name.
+    /// </param>
+    /// <param name="parameters">
+    /// The query's parameters.
+    /// </param>
+    /// <returns>
+    /// The resolved query or <see cref="Query.EmptyQuery"/> if the query could
+    /// not be resolved.
+    /// </returns>
+    /// <remarks>
+    /// The given parameters will be bound to the query if it can be resolved
+    /// using its name.
+    /// </remarks>
+    Query GetQuery(string name, IDictionary<string, string> parameters);
+  }
+}
