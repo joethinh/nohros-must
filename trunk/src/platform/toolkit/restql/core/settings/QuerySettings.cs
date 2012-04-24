9c62d90b54b82824fbf8173aa7c3c970f352e090
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-04-24 20:12:53 +0000

    Created the default implementation of the IQuerySettings interface.

---
+++ trunk/src/platform/toolkit/restql/core/settings/QuerySettings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Xml;
+using Nohros.Configuration;
+
+namespace Nohros.Toolkit.RestQL
+{
+  public class QuerySettings : NohrosConfiguration, IQuerySettings
+  {
+    IProviderNode[] processors_;
+    long query_cache_duration_;
+
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NohrosConfiguration"/>
+    /// class.
+    /// </summary>
+    public QuerySettings(IProviderNode[] processors) {
+      processors_ = processors;
+    }
+    #endregion
+
+    #region IQuerySettings Members
+    /// <inheritdoc/>
+    public IProviderNode[] Processors {
+      get { return processors_; }
+    }
+
+    /// <inheritdoc/>
+    public long QueryCacheDuration {
+      get { return query_cache_duration_; }
+
+      // set by base class.
+      private set { query_cache_duration_ = value; }
+    }
+    #endregion
+  }
+}
