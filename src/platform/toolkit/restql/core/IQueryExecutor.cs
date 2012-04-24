47b7a80aaccaeb6e2c15265a92bf9d1e2ab83d7c
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-04-24 20:21:23 +0000

    Renamed the interface IQueryProcessor to IQueryExecutor to better reflects its usage.

---
+++ src/platform/toolkit/restql/core/IQueryExecutor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Toolkit.RestQL
+{
+  /// <summary>
+  /// A <see cref="IQueryExecutor"/> is used to process the submitted
+  /// rest queries.
+  /// </summary>
+  public interface IQueryExecutor
+  {
+    /// <summary>
+    /// Computes the specified query and returns the computation results as a
+    /// string.
+    /// </summary>
+    /// <param name="query">
+    /// The query to be processed.
+    /// </param>
+    /// <remarks>
+    /// The format of the returned string is processor dependant. Callers
+    /// should check the documentation of each processor to understand the
+    /// meaning of the returned string.
+    /// </remarks>
+    string Execute(Query query);
+  }
+}
