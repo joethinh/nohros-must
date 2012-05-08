d81760b58ffd6a3ba08c6c35c7a890b59769ed92
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-05-08 21:58:49 +0000

    Created a default implementation of the IXmlElementsNode interface.

---
+++ src/base/common/configuration/common/XmlElementsNodeParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Xml;
+
+namespace Nohros.Configuration
+{
+  public partial class XmlElementsNode
+  {
+    /// <summary>
+    /// Parses the specified <see cref="XmlElement"/> element into a
+    /// <see cref="XmlElementsNode"/> object.
+    /// </summary>
+    /// <param name="element">
+    /// A Xml element that contains the xml elements configuration data.
+    /// </param>
+    /// <returns>
+    /// A <see cref="XmlElementsNode"/> containing the configured xml elements.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="element"/> is a <c>null</c> reference.
+    /// </exception>
+    /// <exception cref="ConfigurationException">
+    /// The <paramref name="element"/> contains invalid configuration data.
+    /// </exception>
+    public static XmlElementsNode Parse(XmlElement element) {
+      if (element == null) {
+        throw new ArgumentNullException("element");
+      }
+
+      XmlElementsNode xml_elements_node = new XmlElementsNode();
+      foreach (XmlNode node in element.ChildNodes) {
+        if (node.NodeType == XmlNodeType.Element) {
+          xml_elements_node.AddChildNode(new XmlElementNode(element));
+        }
+      }
+      return xml_elements_node;
+    }
+  }
+}
