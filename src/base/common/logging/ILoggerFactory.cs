50d61564eff77733983527de23a3f0ad8ba2a05b
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-05-08 23:10:56 +0000

    Created a interface to allow logger to be created using a factory.

---
+++ src/base/common/logging/ILoggerFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Nohros.Configuration;
+
+namespace Nohros.Logging
+{
+  /// <summary>
+  /// A factory used to create instances of the <see cref="ILogger"/> class.
+  /// </summary>
+  public interface ILoggerFactory
+  {
+    /// <summary>
+    /// Creates an instance of the <see cref="ILogger"/> class using the
+    /// specified provider node.
+    /// </summary>
+    /// <param name="options">
+    /// A <see cref="IDictionary{TKey,TValue}"/> object that contains the
+    /// options for the logger to be created.
+    /// </param>
+    /// <param name="configuration">
+    /// A <see cref="IMustConfiguration"/> object taht can be used to get
+    /// configuration inforamtions related with the logger to be created - such
+    /// as the configuration of a related logger.
+    /// </param>
+    /// <returns>
+    /// The newly created <see cref="ILogger"/> object.
+    /// </returns>
+    ILogger CreateLogger(IDictionary<string, string> options,
+      IMustConfiguration configuration);
+  }
+}
