8ecf9ec0f5bb5aa77d5f854697f373af275e8397
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-05-14 18:41:28 +0000

    Added a new method to allows callers to retrieve all the providers that is associated with a particular group.

--- trunk/src/base/common/configuration/common/provider/IProvidersNode.cs
+++ trunk/src/base/common/configuration/common/provider/IProvidersNode.cs
@@ -11,99 +11,116 @@ namespace Nohros.Configuration
   {
     /// <summary>
     /// Gets a <see cref="IProviderNode"/> node whose name is
-    /// <paramref name="provider_name"/> and is associated with the
+    /// <paramref name="name"/> and is associated with the
     /// default group.
     /// </summary>
-    /// <param name="provider_name">
+    /// <param name="name">
     /// The name of the provider.
     /// </param>
     /// <returns>
     /// A <see cref="IProviderNode"/> object whose name is
-    /// <paramref name="provider_name"/>.
+    /// <paramref name="name"/>.
     /// </returns>
     /// <exception cref="KeyNotFoundException">
-    /// A simple provider whose name is <param name="provider_name"> was
+    /// A simple provider whose name is <param name="name"> was
     /// not found.
     /// </param>
     /// </exception>
-    IProviderNode GetProviderNode(string provider_name);
+    IProviderNode GetProviderNode(string name);
 
     /// <summary>
     /// Gets a <see cref="IProviderNode"/> node whose name is
-    /// <paramref name="provider_name"/> and is associated with the
-    /// group <paramref name="provider_group"/>.
+    /// <paramref name="name"/> and is associated with the
+    /// group <paramref name="group"/>.
     /// </summary>
-    /// <param name="provider_name">
+    /// <param name="name">
     /// The name of the provider.
     /// </param>
-    /// <param name="provider_group">
+    /// <param name="group">
     /// The name of the group associated with the provider.
     /// </param>
     /// <returns>
     /// A <see cref="IProviderNode"/> object whose name is
-    /// <paramref name="provider_name"/>.
+    /// <paramref name="name"/>.
     /// </returns>
     /// <exception cref="KeyNotFoundException">
-    /// A provider whose name is <param name="provider_name"> and is
-    /// associated with the group <paramref name="provider_group"/> was not
+    /// A provider whose name is <param name="name"> and is
+    /// associated with the group <paramref name="group"/> was not
     /// found.
     /// </param>
     /// </exception>
-    IProviderNode GetProviderNode(string provider_name, string provider_group);
+    IProviderNode GetProviderNode(string name, string group);
 
     /// <summary>
     /// Gets a <see cref="IProviderNode"/> whose name is
-    /// <paramref name="provider_name"/>.
+    /// <paramref name="name"/>.
     /// </summary>
-    /// <param name="provider_name">
+    /// <param name="name">
     /// The name of the provider.
     /// </param>
     /// <param name="simple_provider">
     /// When this method returns contains a <see cref="IProviderNode"/>
-    /// object whose name is <paramref name="provider_name"/>.
+    /// object whose name is <paramref name="name"/>.
     /// </param>
     /// <returns>
     /// <c>true</c> when a provider whose name is
-    /// <paramref name="provider_name"/> is found; otherwise, <c>false</c>.
+    /// <paramref name="name"/> is found; otherwise, <c>false</c>.
     /// </returns>
-    bool GetProviderNode(string provider_name, out IProviderNode simple_provider);
+    bool GetProviderNode(string name, out IProviderNode simple_provider);
 
     /// <summary>
     /// Gets a <see cref="IProvidersNode"/> node whose name is
-    /// <paramref name="provider_name"/> and is associated with the
-    /// group <paramref name="provider_group"/>.
+    /// <paramref name="name"/> and is associated with the
+    /// group <paramref name="group"/>.
     /// </summary>
-    /// <param name="provider_name">
+    /// <param name="name">
     /// The name of the provider.
     /// </param>
-    /// <param name="provider_group">
+    /// <param name="group">
     /// The name of the group associated with the provider.
     /// </param>
     /// <param name="simple_provider">
     /// When this method returns contains a <see cref="IProviderNode"/>
-    /// object whose name is <paramref name="provider_name"/>.
+    /// object whose name is <paramref name="name"/>.
     /// </param>
     /// <returns>
     /// <c>true</c> when a provider whose name is
-    /// <paramref name="provider_name"/> is found; otherwise, <c>false</c>.
+    /// <paramref name="name"/> is found; otherwise, <c>false</c>.
     /// </returns>
-    bool GetProviderNode(string provider_name,
-      string provider_group, out IProviderNode simple_provider);
+    bool GetProviderNode(string name,
+      string group, out IProviderNode simple_provider);
+
+    /// <summary>
+    /// Gets an array of <see cref="IProviderNode"/> containing all the
+    /// providers that belongs to the grou <paramref name="group"/>
+    /// </summary>
+    /// <param name="group">
+    /// The name of the group associated with the providers.
+    /// </param>
+    /// <returns>
+    /// An array of <see cref="IProviderNode"/> containing all the providers
+    /// associated with the group <paramref name="group"/>.
+    /// </returns>
+    /// <remarks>
+    /// If there are no providers associated with the group
+    /// <paramref name="group"/> this method will returns an empty array.
+    /// </remarks>
+    IProviderNode[] GetProvidersNode(string group);
 
     /// <summary>
     /// Gets a <see cref="IProviderNode"/> node whose name is
-    /// <paramref name="provider_name"/> and is associated with the
+    /// <paramref name="name"/> and is associated with the
     /// default group.
     /// </summary>
-    /// <param name="provider_name">
+    /// <param name="name">
     /// The name of the provider.
     /// </param>
     /// <returns>
     /// A <see cref="IProviderNode"/> object whose name is
-    /// <paramref name="provider_name"/>.
+    /// <paramref name="name"/>.
     /// </returns>
     /// <exception cref="KeyNotFoundException">
-    /// A simple provider whose name is <param name="provider_name"> was
+    /// A simple provider whose name is <param name="name"> was
     /// not found.
     /// </param>
     /// </exception>
@@ -111,26 +128,26 @@ namespace Nohros.Configuration
     /// This method is a shortcut for the <see cref="GetProviderNode(string)"/>
     /// method.
     /// </remarks>
-    IProviderNode this[string provider_name] { get; }
+    IProviderNode this[string name] { get; }
 
     /// <summary>
     /// Gets a <see cref="IProviderNode"/> node whose name is
-    /// <paramref name="provider_name"/> and is associated with the
-    /// group <paramref name="provider_group"/>.
+    /// <paramref name="name"/> and is associated with the
+    /// group <paramref name="group"/>.
     /// </summary>
-    /// <param name="provider_name">
+    /// <param name="name">
     /// The name of the provider.
     /// </param>
-    /// <param name="provider_group">
+    /// <param name="group">
     /// The name of the group associated with the provider.
     /// </param>
     /// <returns>
     /// A <see cref="IProviderNode"/> object whose name is
-    /// <paramref name="provider_name"/>.
+    /// <paramref name="name"/>.
     /// </returns>
     /// <exception cref="KeyNotFoundException">
-    /// A provider whose name is <param name="provider_name"> and is
-    /// associated with the group <paramref name="provider_group"/> was not
+    /// A provider whose name is <param name="name"> and is
+    /// associated with the group <paramref name="group"/> was not
     /// found.
     /// </param>
     /// </exception>
@@ -138,6 +155,6 @@ namespace Nohros.Configuration
     /// This method is a shortcut for the
     /// <see cref="GetProviderNode(string, string)"/> method.
     /// </remarks>
-    IProviderNode this[string provider_name, string provider_group] { get; }
+    IProviderNode this[string name, string group] { get; }
   }
 }
