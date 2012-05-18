b66cda03907ef8a3dd3657a6dabcb80c4a893eb0
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-05-18 21:37:55 +0000

    Removed the IDataField interface and added the IJsonDataField interface from the list of interfaces that is implemented by the fundamental json tokens(boolean, decimal, double, float, integer, long, short, string)

--- trunk/src/base/common/data/json/tokens/JsonDecimal.cs
+++ trunk/src/base/common/data/json/tokens/JsonDecimal.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace Nohros.Data.Json
 {
@@ -6,7 +7,7 @@ namespace Nohros.Data.Json
   /// An implementation of the <see cref="IJsonToken{T}"/> that maps a
   /// <see cref="Decimal"/> type to a json number token.
   /// </summary>
-  public class JsonDecimal: JsonNumber<decimal>
+  public class JsonDecimal : JsonNumber<decimal>, IJsonDataField
   {
     #region .ctor
     /// <summary>
@@ -34,6 +35,28 @@ namespace Nohros.Data.Json
     /// </param>
     public JsonDecimal(decimal value, string format)
       : base(value, format) {
+    }
+    #endregion
+
+    #region IJsonDataField Members
+    /// <summary>
+    /// Gets a <see cref="JsonDecimal"/> object that contains the decimal value
+    /// readed from the <see cref="IDataReader"/> at
+    /// <paramref name="position"/>.
+    /// </summary>
+    /// <param name="reader">
+    /// A <see cref="IDataReader"/> that can be used to extract a decimal
+    /// value at <paramref name="position"/>.
+    /// </param>
+    /// <param name="position">
+    /// A integer that identifies the position to read the decimal value that
+    /// will be associated with the json decimal token.
+    /// </param>
+    /// <returns>
+    /// The newly created <see cref="JsonDecimal"/> object.
+    /// </returns>
+    IJsonToken IJsonDataField.GetJsonToken(IDataReader reader, int position) {
+      return new JsonDecimal(reader.GetDecimal(position));
     }
     #endregion
 
