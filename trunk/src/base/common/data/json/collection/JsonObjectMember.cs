2f4986b7ede908e3ce1fc38c043923cce26dccae
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-05-21 22:50:21 +0000

    Created a class to represent a json object token.

---
+++ trunk/src/base/common/data/json/collection/JsonObjectMember.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nohros.Data.Json
+{
+  public partial class JsonObject
+  {
+    #region JsonMember
+    /// <summary>
+    /// An implementation of the <see cref="IJsonToken{T}"/> that represents a
+    /// json object member token.
+    /// </summary>
+    public class JsonMember: IJsonToken<KeyValuePair<string, IJsonToken>>
+    {
+      readonly string name_;
+      readonly IJsonToken value_;
+
+      #region .ctor
+      /// <summary>
+      /// Initializes a new instance of the <see cref="JsonMember"/> class
+      /// by using the member name and value.
+      /// </summary>
+      /// <param name="name">
+      /// The member's name.
+      /// </param>
+      /// <param name="value">
+      /// The member's value.
+      /// </param>
+      public JsonMember(string name, IJsonToken value) {
+        name_ = name;
+        value_ = value;
+      }
+      #endregion
+
+      /// <inheritdoc/>
+      public string AsJson() {
+        return "\"" + name_ + "\":" + value_.AsJson();
+      }
+
+      /// <inheritdoc/>
+      public KeyValuePair<string, IJsonToken> Value {
+        get { return new KeyValuePair<string, IJsonToken>(name_, value_); }
+      }
+    }
+    #endregion
+  }
+}
