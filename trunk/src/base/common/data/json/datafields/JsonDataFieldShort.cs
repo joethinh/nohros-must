142964f42d05dc48c4da560d721b21601aec494b
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-05-21 18:41:41 +0000

    Created a interface (as a as its specific implementations) that maps a data field (IDataField) to a json token(IJsonToken).

---
+++ trunk/src/base/common/data/json/datafields/JsonDataFieldShort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+
+namespace Nohros.Data.Json
+{
+  public class JsonDataFieldShort: DataFieldShort, IJsonDataField<short>
+  {
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="JsonDataFieldShort"/>
+    /// class using the specified field <paramref name="name"/> and
+    /// ordianl <paramref name="position"/>.
+    /// </summary>
+    /// <param name="name">
+    /// The name of the field.
+    /// </param>
+    /// <param name="position">
+    /// The zero-based positon of the field within a <see cref="IDataReader"/>.
+    /// </param>
+    public JsonDataFieldShort(string name, int position)
+      : base(name, position) {
+    }
+    #endregion
+
+    #region IJsonDataField<short> Members
+    /// <summary>
+    /// Gets the a <see cref="IJsonToken{T}"/> object that represents the
+    /// the field's value.
+    /// </summary>
+    /// <param name="reader">
+    /// A <see cref="IDataReader"/> that can be used to extract a
+    /// <see cref="Int16"/> value at the field position.
+    /// </param>
+    public IJsonToken<short> GetValue(IDataReader reader) {
+      return new JsonShort(base.GetValue(reader));
+    }
+    #endregion
+  }
+}
---
+++ trunk/src/base/common/data/json/datafields/JsonDataFieldString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+
+namespace Nohros.Data.Json
+{
+  public class JsonDataFieldString: DataFieldString, IJsonDataField<string>
+  {
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="JsonDataFieldString"/>
+    /// class using the specified field <paramref name="name"/> and
+    /// ordianl <paramref name="position"/>.
+    /// </summary>
+    /// <param name="name">
+    /// The name of the field.
+    /// </param>
+    /// <param name="position">
+    /// The zero-based positon of the field within a <see cref="IDataReader"/>.
+    /// </param>
+    public JsonDataFieldString(string name, int position)
+      : base(name, position) {
+    }
+    #endregion
+
+    #region IJsonDataField<string> Members
+    /// <summary>
+    /// Gets the a <see cref="IJsonToken{T}"/> object that represents the
+    /// the field's value.
+    /// </summary>
+    /// <param name="reader">
+    /// A <see cref="IDataReader"/> that can be used to extract a
+    /// <see cref="String"/> at the field position.
+    /// </param>
+    public IJsonToken<string> GetValue(IDataReader reader) {
+      return new JsonString(base.GetValue(reader));
+    }
+    #endregion
+  }
+}
