527ca119dbf3265a0d93fb25cd7889a7e41a50c0
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-05-21 22:58:33 +0000

    Created a executor that runs queries against a SQL database.

---
+++ trunk/src/platform/toolkit/restql/core/executors/SqlQueryExecutorFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nohros.Toolkit.RestQL
+{
+  public partial class SqlQueryExecutor : IQueryExecutorFactory
+  {
+    /// <summary>
+    /// Creates a instance of the <see cref="IQueryExecutor"/> class by using
+    /// the specified application settings.
+    /// </summary>
+    /// <param name="options">
+    /// A <see cref="IDictionary{TKey,TValue}"/> containing the specific
+    /// options configured for the query processor.
+    /// </param>
+    /// <param name="settings">
+    /// A <see cref="IQuerySettings"/> containing configuration data for the
+    /// query processor.
+    /// </param>
+    /// <returns>
+    /// An instance of the <see cref="IQueryExecutor"/> class.
+    /// </returns>
+    public IQueryExecutor CreateQueryExecutor(IDictionary<string, string> options, IQuerySettings settings) {
+    }
+  }
+}
