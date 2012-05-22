d7f29e81051b89d28d310de520aae42f730c219c
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-05-22 01:51:59 +0000

    Created the program entry point.

---
+++ trunk/src/platform/toolkit/restql/core/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Nohros.Caching.Providers;
+
+namespace Nohros.Toolkit.RestQL
+{
+  /// <summary>
+  /// Represents the RestQL application, that is the library entry-point.
+  /// </summary>
+  public sealed class Program
+  {
+    public static void Main(string[] args) {
+      Settings settings = Settings.CreateSettings();
+
+      AppFactory factory = new AppFactory(settings);
+      ICacheProvider cache_provider = factory.CreateCacheProvider();
+      ICommonDataProvider common_data_provider =
+        factory.CreateCommonDataProvider();
+      IQueryResolver resolver = QueryResolver.CreateQueryResolver(
+        settings.QuerySettings, common_data_provider, cache_provider);
+      IQueryProcessor processor = factory.CreateQueryProcessor(resolver);
+    }
+  }
+}
