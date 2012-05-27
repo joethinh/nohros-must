2a6f9e685fc5eff1246266f95050b7c647459d91
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-05-27 03:35:32 +0000

    Changed the implementation of the Mailbox class to guarantee that the receive method does not run in more than one thread at the same time.

---
+++ trunk/src/base/common/concurrent/MailboxReceiveCallback.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Nohros.Concurrent
+{
+  /// <summary>
+  /// Represents a method to be called when a <see cref="Mailbox{T}"/> receives
+  /// a message.
+  /// </summary>
+  /// <typeparam name="T">
+  /// The type of the message.
+  /// </typeparam>
+  /// <param name="message">
+  /// The message that was sent to the <see cref="Mailbox{T}"/>.
+  /// </param>
+  /// <remarks>
+  /// <see cref="MailboxReceiveCallback{T}"/> represents a callback that you
+  /// want to execute when a message is received by a
+  /// <see cref="Mailbox{T}"/>. The delegate is executed for each received
+  /// message respecting the arrival order.
+  /// <para>
+  /// The callback runs in a thread from the thread pool.
+  /// </para>
+  /// </remarks>
+  public delegate void MailboxReceiveCallback<T>(T message);
+}
