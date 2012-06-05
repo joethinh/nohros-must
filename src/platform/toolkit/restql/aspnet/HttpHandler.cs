f96fed8881e8839742f6c373ed334dba122a9509
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-06-05 02:06:44 +0000

    Finish aspnet binding implementation.

--- src/platform/toolkit/restql/aspnet/HttpHandler.cs
+++ src/platform/toolkit/restql/aspnet/HttpHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Web;
 using System.Web.Caching;
 
@@ -9,10 +10,6 @@ namespace Nohros.Toolkit.RestQL
   /// </summary>
   public class HttpHandler : IHttpHandler
   {
-    const string kAppSettingsCacheKey = "app-settings-cache-key";
-    const string kCommonSimpleProvidersConfigKey = "common";
-    const string kTokenPrincipalMapperConfigKey = "token-principal-mapper";
-
     string kTokenQueryString = "token";
 
     /// <summary>
@@ -30,7 +27,7 @@ namespace Nohros.Toolkit.RestQL
       // throw an exception; a error message shoud be sent as response when
       // a unexpected event occur.
       try {
-        // map the token to a principal and resolve the query using the
+        // Map the token to a principal and resolve the query using the
         // specified principal.
         QueryServer server = Global.QueryServer;
         ITokenPrincipalMapper token_principal_mapper =
@@ -38,7 +35,20 @@ namespace Nohros.Toolkit.RestQL
         token = (token_principal_mapper != null)
           ? token_principal_mapper.MapTokenToPrincipal(token)
           : server.TokenPrincipalMapperSettings.AnonymousToken;
+
+        // Attempt to process the query using the supplied parameters.
+        IQueryProcessor processor = server.QueryProcessor;
+        string result;
+        HttpStatusCode status_code =
+          processor.Process(context.Request.QueryString, out result);
+
+        // Set up the response and send it to the caller.
+        HttpResponse response = context.Response;
+        response.ContentType = "application/json";
+        response.StatusCode = (int)status_code;
+        response.Write(response);
       } catch {
+        // TODO(neylor.silva) Find the right HttpStatusCode to return.
       }
     }
 
--- src/platform/toolkit/restql/core/IQueryProcessor.cs
+++ src/platform/toolkit/restql/core/IQueryProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Net;
 using System.Text;
 
@@ -16,7 +17,43 @@ namespace Nohros.Toolkit.RestQL
     /// <param name="query">
     /// A query string to be processed.
     /// </param>
-    /// <returns></returns>
+    /// <param name="result">
+    /// The result of the query processing.
+    /// </param>
+    /// <returns>
+    /// A <see cref="HttpStatusCode"/> repsenting the status of the query
+    /// processing.
+    /// </returns>
     HttpStatusCode Process(string query, out string result);
+
+    /// <summary>
+    /// Process the given query.
+    /// </summary>
+    /// <param name="query">
+    /// A collection of name value pairs that represents the query.
+    /// </param>
+    /// <param name="result">
+    /// The result of the query processing.
+    /// </param>
+    /// <returns>
+    /// A <see cref="HttpStatusCode"/> repsenting the status of the query
+    /// processing.
+    /// </returns>
+    HttpStatusCode Process(NameValueCollection query, out string result);
+
+    /// <summary>
+    /// Process the given query.
+    /// </summary>
+    /// <param name="query">
+    /// A collection of name value pairs that represents the query.
+    /// </param>
+    /// <param name="result">
+    /// The result of the query processing.
+    /// </param>
+    /// <returns>
+    /// A <see cref="HttpStatusCode"/> repsenting the status of the query
+    /// processing.
+    /// </returns>
+    HttpStatusCode Process(IDictionary<string, string> query, out string result);
   }
 }
