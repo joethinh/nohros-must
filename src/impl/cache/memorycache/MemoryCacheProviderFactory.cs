7448fe2b922fb10e51b060bff79d2cafa676da03
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-06-06 13:59:14 +0000

    Created a implementation of the ICacheProvider that uses the built-in .NET 4.0 MemoryCache.

---
+++ src/impl/cache/memorycache/MemoryCacheProviderFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Caching;
+using System.Collections.Generic;
+using Nohros.Caching.Providers;
+
+namespace Nohros.Caching
+{
+  public class MemoryCacheProviderFactory : ICacheProviderFactory
+  {
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the
+    /// <see cref="MemoryCacheProviderFactory"/> class.
+    /// </summary>
+    public MemoryCacheProviderFactory() {
+    }
+    #endregion
+
+    #region ICacheProviderFactory Members
+    public ICacheProvider CreateCacheProvider(
+      IDictionary<string, string> options) {
+      return new MemoryCacheProvider(System.Runtime.Caching.MemoryCache.Default);
+    }
+    #endregion
+  }
+}
