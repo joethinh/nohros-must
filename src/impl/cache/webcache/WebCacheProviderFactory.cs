b4b6329fd9f3531ae1be5af62112796033ab438a
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-06-27 17:46:57 +0000

    Introduced WebCache a implementation of the ICacheProvider interface that uses the .NET 2.0 web cache.

---
+++ src/impl/cache/webcache/WebCacheProviderFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using Nohros.Caching.Providers;
+
+namespace Nohros.Caching
+{
+  /// <summary>
+  /// A factory used to create instances of the <see cref="WebCacheProvider"/>
+  /// class.
+  /// </summary>
+  public class WebCacheProviderFactory : ICacheProviderFactory
+  {
+    #region ICacheProviderFactory Members
+    /// <inheritdoc/>
+    public ICacheProvider CreateCacheProvider(
+      IDictionary<string, string> options) {
+      return new WebCacheProvider(HttpRuntime.Cache);
+    }
+    #endregion
+  }
+}
