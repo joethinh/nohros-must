9581cb493f81be36682056bd25834dc51a2af0e6
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-08-07 19:40:34 +0000

    Fix a bug that cuases the JsonStringBuilder class to produce a invalid JSON string

---
+++ src/tests/common/data/json/JsonStringBuilderTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NUnit.Framework;
+using Nohros.Data.Json;
+
+namespace Nohros.Common.data.json
+{
+  [TestFixture]
+  public class JsonStringBuilderTest
+  {
+    [Test]
+    public void ShouldEscapeLastWrittenToken() {
+      string json = new JsonStringBuilder()
+        .WriteBeginArray()
+        .Escape()
+        .ToString();
+      Assert.AreEqual("[", json);
+
+      json = new JsonStringBuilder()
+        .WriteBeginObject()
+        .WriteMember("key", "value")
+        .Escape()
+        .WriteEndObject()
+        .ToString();
+      Assert.AreEqual("{\"key\":\"value\"}", json);
+
+      json = new JsonStringBuilder()
+        .WriteBeginObject()
+        .WriteMember("key", "value-with-CRLF\r\n")
+        .Escape()
+        .WriteEndObject()
+        .ToString();
+      Assert.AreEqual("{\"key\":\"value-with-CRLF\\r\\n\"}", json);
+    }
+
+    [Test]
+    public void ShouldWriteJsonMember() {
+      string json = new JsonStringBuilder()
+        .WriteMember("name", "key")
+        .ToString();
+      Assert.AreEqual(json, "\"name\":\"key\"");
+
+      json = new JsonStringBuilder()
+        .WriteMember("name", (double) 20.1)
+        .ToString();
+      Assert.AreEqual("\"name\":20.1", json);
+
+      json = new JsonStringBuilder()
+        .WriteMember("name", (int)20)
+        .ToString();
+      Assert.AreEqual("\"name\":20", json);
+
+      json = new JsonStringBuilder()
+        .WriteMember("name", (long) 64000)
+        .ToString();
+      Assert.AreEqual("\"name\":64000", json);
+    }
+  }
+}
