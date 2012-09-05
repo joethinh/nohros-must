ae0a2cff31be238da15838b9209468c7389a58d6
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-09-05 20:25:42 +0000

    Create a project that contains a collection of extension classes.

---
+++ trunk/src/base/extensions/Strings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Nohros.Extensions
+{
+  public static class StringExtensions
+  {
+    public static bool CompareOrdinal(this String str, string comparand) {
+      return string.Compare(str, comparand, StringComparison.Ordinal) == 0;
+    }
+
+    public static bool CompareOrdinalIgnoreCase(this String str,
+      string comparand) {
+      return
+        string.Compare(str, comparand, StringComparison.OrdinalIgnoreCase) == 0;
+    }
+
+    public static bool CompareCurrentCultureIgnoreCase(this String str,
+      string comparand) {
+      return string.
+        Compare(str, comparand, StringComparison.CurrentCultureIgnoreCase) == 0;
+    }
+  }
+}
