dd5785b81da8e9ed03b323271cdfe40f128da0a9
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-09-15 19:46:30 +0000

    Created a collection of methods that extends the IDataCommand interface and add the capability to create parameters directly from the command object.

---
+++ src/base/extensions/DataParameters.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data;
+
+namespace Nohros.Extensions.Data
+{
+  /// <summary>
+  /// Provides facilities methods for create data parameters.
+  /// </summary>
+  public static class DbCommands
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IDbDataParameter"/> class
+    /// that uses the parameter name and the data type.
+    /// </summary>
+    /// <param name="command">
+    /// The <see cref="IDbCommand"/> object to which the created parameter
+    /// should be associated.
+    /// </param>
+    /// <param name="parameter_name">
+    /// The name of the parameter to map.
+    /// </param>
+    /// <param name="db_type">
+    /// One of the <see cref="DbType"/> values that best represents the value
+    /// of the parameter.
+    /// </param>
+    /// <returns>
+    /// A instance of the <see cref="IDbDataParameter"/> class associated with
+    /// the given <see cref="IDbCommand"/> object.
+    /// </returns>
+    public static IDbDataParameter CreateParameter(this IDbCommand command,
+      string parameter_name, DbType db_type) {
+      IDbDataParameter parameter = command.CreateParameter();
+      parameter.ParameterName = parameter_name;
+      parameter.DbType = db_type;
+      command.Parameters.Add(parameter);
+      return parameter;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IDbDataParameter"/> class
+    /// that uses the parameter name and the data type.
+    /// </summary>
+    /// <param name="command">
+    /// The <see cref="IDbCommand"/> object to which the created parameter
+    /// should be associated.
+    /// </param>
+    /// <param name="parameter_name">
+    /// The name of the parameter to map.
+    /// </param>
+    /// <param name="db_type">
+    /// One of the <see cref="DbType"/> values that best represents the value
+    /// of the parameter.
+    /// </param>
+    /// <param name="size">
+    /// The length of the parameter.
+    /// </param>
+    /// <returns>
+    /// A instance of the <see cref="IDbDataParameter"/> class associated with
+    /// the given <see cref="IDbCommand"/> object.
+    /// </returns>
+    public static IDbDataParameter CreateParameter(this IDbCommand command,
+      string parameter_name, DbType db_type, int size) {
+      IDbDataParameter parameter = CreateParameter(command, parameter_name,
+        db_type);
+      parameter.Size = size;
+      return parameter;
+    }
+  }
+}
