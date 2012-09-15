eb1f62eb9fbfc5858a13bda554f096828b662a1a
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-09-15 19:49:17 +0000

    Added a interface that abstracts the Thread creation operation and a classes that implements that interface and create threads that runs in background

---
+++ trunk/src/base/common/concurrent/BackgroundThreadFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Nohros.Concurrent
+{
+  /// <summary>
+  /// A implementation of the <see cref="IThreadFactory"/> class that create
+  /// <see cref="Thread"/>s that runs in background(
+  /// <see cref="Thread.IsBackground"/> equals to <c>true</c>).
+  /// </summary>
+  public class BackgroundThreadFactory : IThreadFactory
+  {
+    /// <summary>
+    /// Creates a <see cref="Thread"/> that runs in bacjgroun and do not
+    /// prevent a process from terminating.
+    /// </summary>
+    /// <param name="runnable"></param>
+    /// <returns>
+    /// A <see cref="Thread"/> that runs in background.
+    /// </returns>
+    public Thread CreateThread(ThreadStart runnable) {
+      Thread thread = new Thread(runnable);
+      thread.IsBackground = true;
+      return thread;
+    }
+  }
+}
