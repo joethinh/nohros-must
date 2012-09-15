08074d2661d4cb48d6ddc822a6f6f09aa41bc4b3
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-09-15 19:49:17 +0000

    Added a interface that abstracts the Thread creation operation and a classes that implements that interface and create threads that runs in background

---
+++ src/base/common/concurrent/IThreadFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace Nohros.Concurrent
+{
+  /// <summary>
+  /// A factory that create threads on demand.
+  /// </summary>
+  /// <remarks>
+  /// This removes hardwriting of calls to <c>new Thread</c>, enabling
+  /// applications to use special thread subclasses.
+  /// <para>
+  /// <example>
+  /// The simplest implementation of this interface is just
+  /// <code>
+  /// class SimpleThreadFactory : IThreadFactory {
+  /// }
+  /// </code>
+  /// </example>
+  /// </para>
+  /// </remarks>
+  public interface IThreadFactory
+  {
+    /// <summary>
+    /// Creates a new <see cref="Thread"/>.
+    /// </summary>
+    /// <param name="runnable">
+    /// A <see cref="ThreadStart"/> to be executed by the new thread.
+    /// </param>
+    /// <returns>
+    /// The newly created <see cref="Thread"/> object.
+    /// </returns>
+    Thread CreateThread(ThreadStart runnable);
+
+    /// <summary>
+    /// Creates a new <see cref="Thread"/> that allows an object to be passed
+    /// to it when the thread is started.
+    /// </summary>
+    /// <param name="runnable">
+    /// A <see cref="ThreadStart"/> to be executed by the new thread.
+    /// </param>
+    /// <returns>
+    /// The newly created <see cref="Thread"/> object.
+    /// </returns>
+    //Thread CreateThread(ParameterizedThreadStart runnable);
+  }
+}
