92344b2f6f4ae4352d1779a5a60e1b43fc80e155
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-10-06 15:31:28 +0000

    Partitioned the Histogram class in an interface and two specific classes, UniformHistogram and BiasedHistogram. This allows more control over the update procedure of the sample associated with this classes.

---
+++ src/platform/toolkit/metrics/core/Histograms.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Nohros.Concurrent;
+
+namespace Nohros.Toolkit.Metrics
+{
+  public sealed class Histograms
+  {
+    public static IHistogram Uniform() {
+      return new UniformHistogram(Samples.kDefaultSampleSize);
+    }
+
+    public static IHistogram Uniform(IExecutor executor) {
+      return new UniformHistogram(Samples.kDefaultSampleSize, executor);
+    }
+
+    public static IHistogram Biased() {
+      return new BiasedHistogram(Samples.kDefaultSampleSize,
+        Samples.kDefaultAlpha);
+    }
+
+    public static IHistogram Biased(IExecutor executor) {
+      return new BiasedHistogram(Samples.kDefaultSampleSize,
+        Samples.kDefaultAlpha, executor);
+    }
+  }
+}
