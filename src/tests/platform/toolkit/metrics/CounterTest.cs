e09b2c153119481ebffbe46442616daaacd9a675
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-10-06 15:33:41 +0000

    Metrics unit tests.

--- src/tests/platform/toolkit/metrics/CounterTest.cs
+++ src/tests/platform/toolkit/metrics/CounterTest.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using NUnit.Framework;
+using Nohros.Concurrent;
 
 namespace Nohros.Toolkit.Metrics
 {
   [TestFixture]
   public class CounterTest
   {
-    MetricsRegistry registry;
-    Counter coutner;
+    [Test]
+    public void ShouldStartsAtZero() {
+      Counter counter = new Counter(Executors.SameThreadExecutor());
+      Assert.That(counter.Count, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void ShouldIncrementByOne() {
+      Counter counter = new Counter(Executors.SameThreadExecutor());
+      counter.Increment();
+      Assert.That(counter.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void ShouldIncrementBySpecifiedDelta() {
+      Counter counter = new Counter(Executors.SameThreadExecutor());
+      counter.Increment(12);
+      Assert.That(counter.Count, Is.EqualTo(12));
+    }
+
+    [Test]
+    public void ShouldDecrementByOne()
+    {
+      Counter counter = new Counter(Executors.SameThreadExecutor());
+      counter.Decrement();
+      Assert.That(counter.Count, Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void ShouldDecrementBySpecifiedDelta()
+    {
+      Counter counter = new Counter(Executors.SameThreadExecutor());
+      counter.Decrement(12);
+      Assert.That(counter.Count, Is.EqualTo(-12));
+    }
   }
 }
