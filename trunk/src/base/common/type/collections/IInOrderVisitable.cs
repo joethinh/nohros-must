d47961bc96ad05f970111bd94cd23b103135df66
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-10-06 15:35:02 +0000

    Implemented two visitors. Fix some bugs in the AndersonTree class.

--- trunk/src/base/common/type/collections/IInOrderVisitable.cs
+++ trunk/src/base/common/type/collections/IInOrderVisitable.cs
@@ -1,46 +1,66 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Nohros.Collections
 {
+  /// <summary>
+  /// Defines methods and properties that allows a
+  /// <see cref="IVisitor{T}"/> to visit, in order, every element contained
+  /// within the structure. This interface is similar to the
+  /// <see cref="IVisitable{T}"/> interface, but the <see cref="Accept"/>
+  /// expects a <see cref="InOrderVisitor{T}"/> insted of a
+  /// <see cref="IVisitor{T}"/> method. It is useful for classes that could be
+  /// traversed in different orders(PreOrder, PostOrder, InOrder, ...)
+  /// </summary>
+  public interface IInOrderVisitable<T>
+  {
     /// <summary>
-    /// Defines methods and properties that allows a <see cref="IVisitor&gt;T&lt;"/> to visit, in order, every
-    /// element contained within the structure. This interface is similar to the <see cref="IVisitable&gt;T&lt;"/>
-    /// interface, but the <see cref="Accept"/> expects a <see cref="InOrderVisitor&gt;T&lt;"/> insted of a
-    /// <see cref="IVisitor&gt;T&lt;"/> method  It is useful for classes that could be traversed in different
-    /// orders(PreOrder, PostOrder, InOrder, ...)</remarks>
+    /// Accepts the specified visitor.
     /// </summary>
-    public interface IInOrderVisitable<T>
-    {
-        /// <summary>
-        /// Accepts the specified visitor.
-        /// </summary>
-        /// <param name="visitor">The visitor to accepts</param>
-        /// <param name="reverse_order">A value indicating if the elements will be visit in the reverse order or not.</param>
-        /// <param name="state">A user-defined object that qualifies or contains information about the visitor's
-        /// current state.</param>
-        /// <exception cref="ArgumentNullException"><paramref name="visitor"/> is a null reference</exception>
-        void Accept(InOrderVisitor<T> visitor, object state, bool reverse_order);
-    }
+    /// <param name="visitor">
+    /// The visitor to accepts.
+    /// </param>
+    /// <param name="reverse_order">
+    /// A value indicating if the elements will be visit in the reverse order
+    /// or not.
+    /// </param>
+    /// <param name="state">
+    /// A user-defined object that qualifies or contains information about the
+    /// visitor's current state.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="visitor"/> is a <c>null</c> reference.
+    /// </exception>
+    void Accept(InOrderVisitor<T> visitor, object state, bool reverse_order);
+  }
 
+  /// <summary>
+  /// Defines methods and properties that allows a
+  /// <see cref="IVisitor{T1,T2}"/> to visit, in order, every element contained
+  /// within the structure. This interface is similar to the
+  /// <see cref="IVisitable{T1,T2}"/> interface, but the <see cref="Accept"/>
+  /// expects a <see cref="InOrderVisitor{T1, T2}"/> insted of a
+  /// <see cref="IVisitor{T1, T2}"/> method  It is useful for classes that
+  /// could be traversed in different orders(PreOrder, PostOrder, InOrder, ...)
+  /// </summary>
+  public interface IInOrderVisitable<T1, T2>
+  {
     /// <summary>
-    /// Defines methods and properties that allows a <see cref="IVisitor&gt;T1, T2&lt;"/> to visit, in order, every
-    /// element contained within the structure. This interface is similar to the <see cref="IVisitable&gt;T1, T2&lt;"/>
-    /// interface, but the <see cref="Accept"/> expects a <see cref="InOrderVisitor&gt;T&lt;"/> insted of a
-    /// <see cref="IVisitor&gt;T&lt;"/> method  It is useful for classes that could be traversed in different
-    /// orders(PreOrder, PostOrder, InOrder, ...)</remarks>
+    /// Accepts the specified visitor.
     /// </summary>
-    public interface IInOrderVisitable<T1, T2>
-    {
-        /// <summary>
-        /// Accepts the specified visitor.
-        /// </summary>
-        /// <param name="visitor">The visitor to accepts</param>
-        /// <param name="reverse_order">A value indicating if the elements will be visit in the reverse order or not.</param>
-        /// <param name="state">A user-defined object that qualifies or contains information about the visitor's
-        /// current state.</param>
-        /// <exception cref="ArgumentNullException"><paramref name="visitor"/> is a null reference</exception>
-        void Accept(InOrderVisitor<T1, T2> visitor, object state, bool reverse_order);
-    }
+    /// <param name="visitor">
+    /// The visitor to accepts.
+    /// </param>
+    /// <param name="reverse_order">
+    /// A value indicating if the elements will be visit in the reverse order
+    /// or not.
+    /// </param>
+    /// <param name="state">
+    /// A user-defined object that qualifies or contains information about the
+    /// visitor's current state.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="visitor"/> is a <c>null</c> reference.
+    /// </exception>
+    void Accept(InOrderVisitor<T1, T2> visitor, object state, bool reverse_order);
+  }
 }
--- trunk/src/base/common/type/collections/InOrderVisitor.cs
+++ trunk/src/base/common/type/collections/InOrderVisitor.cs
@@ -8,12 +8,12 @@ namespace Nohros.Collections
   /// A visitor that visits single objects in order.
   /// </summary>
   /// <typeparam name="T">The type of objects to be visited.</typeparam>
-  /// <remarks>This class only wraps the methods of the <see cref="Visitor"/>.
+  /// <remarks>This class only wraps the methods of the <see cref="IVisitor{T}"/>.
   /// It is useful for classes that could be traversed in different
   /// orders(PreOrder, PostOrder, InOrder, ...)</remarks>
   public class InOrderVisitor<T>: IVisitor<T>
   {
-    IVisitor<T> visitor_;
+    readonly IVisitor<T> visitor_;
 
     #region .ctor
     /// <summary>
@@ -64,7 +64,7 @@ namespace Nohros.Collections
   /// <summary>
   /// A visitor that visits compound objects in order. Compound objects are
   /// objects that are identified by more than one object.<example>A node
-  /// within a <see cref="IDictionary&gt;T&lt;"/></example>
+  /// within a <see cref="IDictionary{TKey,TValue}"/></example>
   /// </summary>
   /// <typeparam name="T1">The type of objects to be visited.</typeparam>
   /// <remarks>This class only wraps the methods of the
@@ -77,16 +77,17 @@ namespace Nohros.Collections
 
     #region .ctor
     /// <summary>
-    /// Initializes a new instance of the InOrderVisitor class by using the
-    /// specified visitor.
+    /// Initializes a new instance of the <see cref="InOrderVisitor{T}"/> class
+    /// by using the specified visitor.
     /// </summary>
     /// <param name="visitor">The visitor to use when visiting the object.
     /// </param>
     /// <exception cref="ArgumentNullException"><paramref name="visitor"/> is
     /// a null reference.</exception>
     public InOrderVisitor(IVisitor<T1, T2> visitor) {
-      if (visitor == null)
+      if (visitor == null) {
         throw new ArgumentNullException("visitor");
+      }
       visitor_ = visitor;
     }
     #endregion
