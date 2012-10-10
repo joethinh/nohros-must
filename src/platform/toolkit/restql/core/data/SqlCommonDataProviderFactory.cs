6d198d9d0c1b8b6a572910db3f9a80dcf30497c8
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-10-10 20:03:26 +0000

    Adjusted the SqlCommonDataProviderFactory to the new version of the must.common project.

--- src/platform/toolkit/restql/core/data/SqlCommonDataProviderFactory.cs
+++ src/platform/toolkit/restql/core/data/SqlCommonDataProviderFactory.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using Nohros.Configuration;
 using Nohros.Data.Providers;
 using Nohros.Providers;
 
@@ -8,22 +7,12 @@ namespace Nohros.Toolkit.RestQL
 {
   public partial class SqlQueryDataProvider : IQueryDataProviderFactory
   {
-    readonly IQuerySettings settings_;
-
-    #region .ctor
-    public SqlQueryDataProvider(IQuerySettings settings) {
-      settings_ = settings;
-    }
-    #endregion
-
     public IQueryDataProvider CreateCommonDataProvider(
       IDictionary<string, string> options) {
-      var provider = new SqlConnectionProvider()
-      IConnectionProvider connection_provider =
-        ProviderFactory<IConnectionProviderFactory>
-          .CreateProviderFactory(provider)
-          .CreateProvider(provider.Options);
-      return new SqlQueryDataProvider(connection_provider);
+      var factory = new SqlConnectionProviderFactory();
+      var sql_connection_provider = factory
+        .CreateProvider(options) as SqlConnectionProvider;
+      return new SqlQueryDataProvider(sql_connection_provider);
     }
   }
 }
