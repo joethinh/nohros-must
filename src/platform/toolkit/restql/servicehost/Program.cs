b1c9405b14466dfef5c62cfc15f10ee44b4304c0
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-10-16 15:05:10 +0000

    Created a project to self host the restql ruby service

---
+++ src/platform/toolkit/restql/servicehost/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nohros.Ruby;
+
+namespace Nohros.Toolkit.RestQL
+{
+  public class Program
+  {
+    public static void Main(string[] args) {
+      var factory = new ServiceFactory();
+      IRubyService service = factory.CreateService(string.Empty);
+      var logger = new LoggerRubyLogger(RestQLLogger.ForCurrentProcess.Logger);
+      var ruby_service_host = new LoggerRubyServiceHost(logger);
+      service.Start(ruby_service_host);
+    }
+  }
+}
