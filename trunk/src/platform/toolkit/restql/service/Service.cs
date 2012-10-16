ad5228cb338c61326908d8f33a541def00e6a539
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-10-16 15:10:37 +0000

    Refactoring.

--- trunk/src/platform/toolkit/restql/service/Service.cs
+++ trunk/src/platform/toolkit/restql/service/Service.cs
@@ -1,26 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
+using System.Linq;
 using Nohros.Ruby;
 using Nohros.Ruby.Protocol;
+using R = Nohros.Resources.StringResources;
 
 namespace Nohros.Toolkit.RestQL
 {
   public class Service : AbstractRubyService
   {
+    const string kClassName = "Nohros.Toolkit.RestQL.Service";
+    readonly RestQLLogger logger_;
+
     readonly IQueryServer server_;
     readonly ManualResetEvent start_stop_service_event_;
+    IRubyServiceHost service_host_;
     Thread start_thread_;
 
     #region .ctor
     public Service(IQueryServer server) {
       server_ = server;
       start_stop_service_event_ = new ManualResetEvent(false);
+      logger_ = RestQLLogger.ForCurrentProcess;
     }
     #endregion
 
     public override void Start(IRubyServiceHost service_host) {
+      logger_.Logger = service_host.Logger;
       start_thread_ = Thread.CurrentThread;
       start_stop_service_event_.WaitOne();
+      service_host_ = service_host;
     }
 
     public override void Stop(IRubyMessage message) {
@@ -30,7 +40,42 @@ namespace Nohros.Toolkit.RestQL
       }
     }
 
-    public override void OnMessage(IRubyMessage message) {
+    public override void OnMessage(IRubyMessage request) {
+      try {
+        switch (request.Type) {
+          case (int) MessageType.kQueryRequestMessage:
+            QueryRequestMessage query =
+              QueryRequestMessage.ParseFrom(request.Message);
+            ProcessQuery(request, query);
+            break;
+        }
+      } catch (Exception e) {
+        // TODO: Add specific exception handling.
+        logger_.Error(string.Format(R.Log_MethodThrowsException, kClassName), e);
+        service_host_
+          .SendError(0, (int) StatusCode.kServerError, e);
+      }
+    }
+
+    void ProcessQuery(IRubyMessage request, QueryRequestMessage query) {
+      if (query.HasName) {
+        string result;
+        IDictionary<string, string> parameters = query.OptionsList
+          .ToDictionary(proto => proto.Name, proto => proto.Value);
+        bool processed = server_
+          .QueryProcessor
+          .Process(query.Name, parameters, out result);
+        if (processed) {
+          QueryResponseMessage response = new QueryResponseMessage.Builder()
+            .SetStatusCode(QueryRequestStatus.kOk)
+            .SetName(query.Name)
+            .SetResponse(result)
+            .BuildPartial();
+          service_host_
+            .Send(request.Id, (int) MessageType.kQueryResponseMessage,
+              response.ToByteArray());
+        }
+      }
     }
   }
 }
