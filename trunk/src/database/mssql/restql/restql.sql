b1637f2caf8a2ed27aa4dcce017c0bf08f2af28d
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-11-09 22:27:23 +0000

    Create a new table to group query options.

--- trunk/src/database/mssql/restql/restql.sql
+++ trunk/src/database/mssql/restql/restql.sql
@@ -109,9 +109,25 @@ on rql_query (
   query, query_method_id, query_type_id
 )
 
+create table rql_query_option_group (
+  query_option_group_id int identity(1,1) not null,
+  query_option_group_name varchar(120) not null
+)
+
+alter table rql_query_option_group
+add constraint PK_rql_query_option_group
+primary key (
+  query_option_group_id
+)
+
+create unique nonclustered index IX_rql_query_option_group
+on rql_query_option_group (
+  query_option_group_name
+)
+
 create table rql_query_option (
   query_option_id int identity(1,1) not null,
-  query_id int not null,
+  query_option_group_id int not null,
   option_name varchar(80) not null,
   option_value varchar(80) not null
 )
@@ -123,16 +139,42 @@ primary key (
 )
 
 alter table rql_query_option
-add constraint FK_rql_query_option_query
+add constraint FK_rql_query_option_group
+foreign key (
+  query_option_group_id
+) references rql_query_option_group (
+  query_option_group_id
+)
+
+create unique nonclustered index IX_rql_query_option_natural
+on rql_query_option (
+  query_option_group_id, option_name
+) include (
+  option_value
+)
+
+create table rql_query_option_crossref (
+  query_id int not null,
+  query_option_group_id int not null
+)
+
+alter table rql_query_option_crossref
+add constraint FK_rql_query_option_crossref_query
 foreign key (
   query_id
 ) references rql_query (
   query_id
 )
 
-create unique nonclustered index IX_rql_query_option_natural
-on rql_query_option (
-  query_id, option_name
-) include (
-  option_value
+alter table rql_query_option_crossref
+add constraint FK_rql_query_option_crossref_group
+foreign key (
+  query_option_group_id
+) references rql_query_option_group (
+  query_option_group_id
+)
+
+create unique nonclustered index IX_rql_query_option_crossref
+on rql_query_option_crossref (
+  query_id, query_option_group_id
 )
