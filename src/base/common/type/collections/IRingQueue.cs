d191fabd3e5cfe355208fa5ca06c7b9813cfc07c
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-11-12 18:53:03 +0000

    Created an implementation of a Circular Buffer.

---
+++ src/base/common/type/collections/IRingQueue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Nohros.Collections
+{
+  /// <summary>
+  /// Represents a first-in, first-out cyclic collection of objects.
+  /// </summary>
+  public interface IRingQueue<T> : ICollection, IEnumerable<T>, IEnumerable
+  {
+    /// <summary>
+    /// Adds an object to the end of the queue.
+    /// </summary>
+    /// <param name="item">
+    /// The object to add to the <see cref="RingQueue{T}"/>. The value can
+    /// be <c>null</c> for reference types.
+    /// </param>
+    /// <remarks>
+    /// <para>
+    /// When a queue is full and <see cref="Enqueue"/> method is called, an
+    /// exception could be throwed or not, depending on the implementation.
+    /// </para>
+    /// This method is an O(1) operation.
+    /// </remarks>
+    void Enqueue(T item);
+
+    /// <summary>
+    /// Removes and returns the object at the beginning of the
+    /// <see cref="RingQueue{T}"/>.
+    /// </summary>
+    /// <returns>
+    /// The object that is removed from the beginning of the
+    /// <see cref="RingQueue{T}"/>.
+    /// </returns>
+    /// <exception cref="InvalidOperationException">
+    /// The <see cref="RingQueue{T}"/> is empty.
+    /// </exception>
+    /// <remarks>
+    /// This method is similar to the <see cref="Peek"/> method, but
+    /// <see cref="Peek"/> does not modify the <see cref="RingQueue{T}"/>.
+    /// <para>
+    /// If type <typeparamref name="T"/> is a reference type, <c>null</c> can
+    /// be added to th <see cref="RingQueue{T}"/> as a value.
+    /// </para>
+    /// <para>
+    /// This method is an O(1) operation.
+    /// </para>
+    /// </remarks>
+    T Dequeue();
+
+    /// <summary>
+    /// Returns the object at the beginning of the <see cref="RingQueue{T}"/>
+    /// without removing it.
+    /// </summary>
+    /// <returns>
+    /// Th object at the beginning of the <see cref="RingQueue{T}"/>.
+    /// </returns>
+    /// <exception cref="InvalidOperationException">
+    /// The <see cref="RingQueue{T}"/> is empty.
+    /// </exception>
+    /// <remarks>
+    /// This method is similar to the <see cref="Dequeue"/> method, but
+    /// <see cref="Dequeue"/> does not modify the <see cref="RingQueue{T}"/>.
+    /// <para>
+    /// If type <typeparamref name="T"/> is a reference type, <c>null</c> can
+    /// be added to th <see cref="RingQueue{T}"/> as a value.
+    /// </para>
+    /// <para>
+    /// This method is an O(1) operation.
+    /// </para>
+    /// </remarks>
+    T Peek();
+  }
+}
---
+++ src/base/common/type/collections/RingQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using Nohros.Resources;
+
+namespace Nohros.Collections
+{
+  /// <summary>
+  /// Represents a first-in, first-out cyclic collection of objects.
+  /// </summary>
+  /// <remarks>
+  /// This class is not thread safe.
+  /// </remarks>
+  public class RingQueue<T> : AbstractRingQueue<T>
+  {
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RingQueue{T}"/> class that
+    /// is empty and has the specified size.
+    /// </summary>
+    public RingQueue(int size) : base(size) {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RingQueue{T}"/> class that
+    /// contains elements copied from the specified collection and has the same
+    /// size as the number of elements copied.
+    /// </summary>
+    /// <param name="elements"></param>
+    public RingQueue(IEnumerable<T> elements) : base(elements) {
+    }
+    #endregion
+
+    public override void Enqueue(T item) {
+      if (IsQueueFull) {
+        throw new InvalidOperationException(
+          StringResources.InvalidOperation_FullQueue);
+      }
+      base.Enqueue(item);
+    }
+  }
+}
