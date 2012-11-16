d1259708e7bc8a09962cb2e18fdaa754f695ad1f
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-11-16 13:33:29 +0000

    Created a new constructor for the MailBox class to allow callers to specify the initial mailbox capacity.

--- trunk/src/base/common/properties/AssemblyInfo.cs
+++ trunk/src/base/common/properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.6.7.24")]
-[assembly: AssemblyFileVersion("1.6.7.24")]
+[assembly: AssemblyVersion("1.6.7.25")]
+[assembly: AssemblyFileVersion("1.6.7.25")]
 
