7ceba803357d09493c1544938b213cc469614236
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-11-17 23:28:21 +0000

    Created a clock that uses the user clock to measure time.

---
+++ src/base/common/type/Clocks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Nohros.type
+{
+  /// <summary>
+  /// A factory for <see cref="Clock"/>.
+  /// </summary>
+  public class Clocks
+  {
+    /// <summary>
+    /// Creates a new instance of the <see cref="UserTimeClock"/>.
+    /// </summary>
+    /// <returns></returns>
+    public static Clock UserTimeClock() {
+      return new UserTimeClock();
+    }
+  }
+}
---
+++ src/base/common/type/UserTimeClock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nohros
+{
+  public class UserTimeClock : Clock
+  {
+    /// <inheritdoc/>
+    public override long Tick {
+      get { return NanoTime; }
+    }
+  }
+}
