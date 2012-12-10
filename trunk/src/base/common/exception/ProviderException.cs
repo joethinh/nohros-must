aecca37401c4115394cfe44978e9c5eb1cb198eb
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-12-10 12:28:33 +0000

    Created a exception to be thrown by repositories and some exceptions that derives from that.

--- trunk/src/base/common/exception/ProviderException.cs
+++ trunk/src/base/common/exception/ProviderException.cs
@@ -18,7 +18,7 @@ namespace Nohros
     /// <summary>
     /// Creates a new instance of the <see cref="ProviderException"/> class.
     /// </summary>
-    /// <param source="message">A message describing why this
+    /// <param name="message">A message describing why this
     /// <see cref="ProviderException"/>was throw</param>
     public ProviderException(string message) : base(message) {
     }
@@ -26,7 +26,7 @@ namespace Nohros
     /// <summary>
     /// Creates a new instance of the <see cref="ProviderException"/> class.
     /// </summary>
-    /// <param source="inner_exception">
+    /// <param name="inner_exception">
     /// The exception that caused this <see cref="ProviderException"/> to be
     /// throw.
     /// </param>
@@ -37,9 +37,9 @@ namespace Nohros
     /// <summary>
     /// Creates a new instance if the <see cref="ProviderException"/> class.
     /// </summary>
-    /// <param source="info">The object that holds the information to
+    /// <param name="info">The object that holds the information to
     /// deserialize.</param>
-    /// <param source="context">Contextual information about the source or
+    /// <param name="context">Contextual information about the source or
     /// destination.</param>
     protected ProviderException(SerializationInfo info,
       StreamingContext context) : base(info, context) {
@@ -48,9 +48,9 @@ namespace Nohros
     /// <summary>
     /// Creates a new instance of the <see cref="ProviderException"/> class.
     /// </summary>
-    /// <param source="message">A message describing why this
+    /// <param name="message">A message describing why this
     /// <see cref="ProviderException"/>was throw.</param>
-    /// <param source="inner_exception">The exception that caused this
+    /// <param name="inner_exception">The exception that caused this
     /// <see cref="ProviderException"/>to be throw.</param>
     public ProviderException(string message, System.Exception inner_exception)
       : base(message, inner_exception) {
