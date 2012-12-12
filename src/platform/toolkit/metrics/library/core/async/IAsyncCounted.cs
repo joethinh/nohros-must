2f3237971f53ce0894d5de72950c5473346b2b2b
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2012-12-12 20:47:14 +0000

    Added a definition of a async counted metric.

---
+++ src/platform/toolkit/metrics/library/core/async/IAsyncCounted.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Nohros.Metrics
+{
+  public interface IAsyncCounted
+  {
+    /// <summary>
+    /// Get the number of values recorded.
+    /// </summary>
+    /// <returns>The number of values recorded.</returns>
+    void GetCount(LongMetricCallback callback);
+  }
+}
