4c41cba688723bd55bec1c4bac501a69727433e5
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-01-03 17:58:36 +0000

    Created a factory for TransactionScope class.

---
+++ src/base/common/data/TransactionScopes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Transactions;
+
+namespace Nohros.Data
+{
+  /// <summary>
+  /// A collection of factory methods for the <see cref="TransactionScopes"/>
+  /// class.
+  /// </summary>
+  public sealed class TransactionScopes
+  {
+    /// <summary>
+    /// Creates a instance of <see cref="TransactionScope"/> class that uses
+    /// <see cref="IsolationLevel.ReadCommitted"/> as isolation level.
+    /// </summary>
+    /// <returns></returns>
+    public static TransactionScope ReadCommited() {
+      return new TransactionScope(TransactionScopeOption.Required,
+        new TransactionOptions {
+          IsolationLevel = IsolationLevel.ReadCommitted,
+          Timeout = TransactionManager.MaximumTimeout
+        });
+    }
+
+    /// <summary>
+    /// Creates a instance of <see cref="TransactionScope"/> class that uses
+    /// <see cref="IsolationLevel.ReadCommitted"/> as isolation level.
+    /// </summary>
+    /// <returns></returns>
+    public static TransactionScope ReadUncommitted() {
+      return new TransactionScope(TransactionScopeOption.Required,
+        new TransactionOptions {
+          IsolationLevel = IsolationLevel.ReadUncommitted,
+          Timeout = TransactionManager.MaximumTimeout
+        });
+    }
+  }
+}
