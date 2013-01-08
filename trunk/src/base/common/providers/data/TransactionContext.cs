0bfc257fba787441c243bdf044d199c649d808bf
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-01-08 11:33:26 +0000

    Added support for transactions on the Data Providers namespace.

---
+++ trunk/src/base/common/providers/data/TransactionContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Nohros.Data.Providers
+{
+  /// <summary>
+  /// The default implementation of the <see cref="ITransactionContext"/>.
+  /// </summary>
+  public sealed class TransactionContext : ITransactionContext
+  {
+    bool complete_;
+
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TransactionContext"/>
+    /// class by using the specified <see cref="IConnectionProvider"/> object.
+    /// </summary>
+    public TransactionContext(IConnectionProvider connection_provider) {
+      Transaction.Current = new Transaction(connection_provider);
+      complete_ = false;
+    }
+    #endregion
+
+    public void Dispose() {
+      Transaction transaction = Transaction.Current;
+      if (transaction != null && !complete_) {
+        transaction.Rollback();
+        transaction.Dispose();
+      }
+    }
+
+
+    /// <inheritdoc/>
+    public void Complete() {
+      Transaction transaction = Transaction.Current;
+      if (transaction == null) {
+        throw new InvalidOperationException(
+          Resources.Resources.DataProvider_InvalidOperation_NoTransaction);
+      }
+      if (complete_) {
+        throw new InvalidOperationException(
+          Resources.Resources.InvalidOperation_CalledMoreThanOnce);
+      }
+      complete_ = true;
+      transaction.Commit();
+    }
+  }
+}
