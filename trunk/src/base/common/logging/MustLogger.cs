a45c1fed2db423bb246f637213ec3b6963b73a78
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-01-17 18:58:04 +0000

    Changed the implementation of the MustLogger to use ForwardingLogger instead of implementing the ILogger interface.

--- trunk/src/base/common/logging/MustLogger.cs
+++ trunk/src/base/common/logging/MustLogger.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace Nohros.Logging
 {
@@ -17,149 +17,30 @@ namespace Nohros.Logging
   /// the correct logger on the app initialization.
   /// </para>
   /// </remarks>
-  public class MustLogger: ILogger
+  public class MustLogger : ForwardingLogger
   {
-    readonly static ILogger current_process_logger_;
+    static readonly MustLogger current_process_logger_;
 
-    readonly ILogger internal_logger_;
+    #region .ctor
+    static MustLogger() {
+      current_process_logger_ = new MustLogger(new NOPLogger());
+    }
+    #endregion
 
     #region .ctor
     /// <summary>
-    /// Initializes the singleton process's logger instance.
-    /// </summary>
-    static MustLogger() {
-      // set the logger to be used, configure it and
-      // do some initialization stuffs
-      //
-      // ex.
-      //   FileLogger internal_logger =
-      //     FileLogger.ForCurrentProcess();
-
-      // creates a instance of the NOPLogger to ensure that
-      // the ForCurrentProcess always returns a valid logger.
-      // Note that the client is responsible to set the value
-      // of the singleton logger object.
-      NOPLogger internal_logger = new NOPLogger();
-
-      // initialize a new instance of the MustLogger using the
-      // previously instantiated logger.
-      MustLogger logger = new MustLogger(internal_logger);
-
-      current_process_logger_ = logger as ILogger;
-    }
-    #endregion
-
-    /// <summary>
     /// Initializes a new instance of the <see cref="MustLogger"/>
-    /// class by using the specified <see cref="ILogger"/> interface.
+    /// class by using the specified <see cref="ILogger"/> object.
     /// </summary>
-    public MustLogger(ILogger logger) {
-      internal_logger_ = logger;
+    public MustLogger(ILogger logger) : base(logger) {
     }
-
-    /// <summary>
-    /// Gets the current process logger.
-    /// </summary>
-    public static ILogger ForCurrentProcess {
-      get {
-        return current_process_logger_;
-      }
-    }
-
-    #region IsEnabled
-
-    /// <inherit />
-    public bool IsDebugEnabled {
-      get {
-        return internal_logger_.IsDebugEnabled;
-      }
-    }
-
-    /// <inherit />
-    public bool IsErrorEnabled {
-      get {
-        return internal_logger_.IsErrorEnabled;
-      }
-    }
-
-    /// <inherit />
-    public bool IsFatalEnabled {
-      get {
-        return internal_logger_.IsFatalEnabled;
-      }
-    }
-
-    /// <inherit />
-    public bool IsInfoEnabled {
-      get {
-        return internal_logger_.IsInfoEnabled;
-      }
-    }
-
-    /// <inherit />
-    public bool IsWarnEnabled {
-      get {
-        return internal_logger_.IsWarnEnabled;
-      }
-    }
-
-    /// <inherit />
-    public bool IsTraceEnabled {
-      get {
-        return internal_logger_.IsTraceEnabled;
-      }
-    }
-
     #endregion
 
-    /// <inherit />
-    public void Debug(string message) {
-      internal_logger_.Debug(message);
-    }
-
-    /// <inherit />
-    public void Debug(string message, Exception exception) {
-      internal_logger_.Debug(message, exception);
-    }
-
-    /// <inherit />
-    public void Error(string message) {
-      internal_logger_.Error(message);
-    }
-
-    /// <inherit />
-    public void Error(string message, Exception exception) {
-      internal_logger_.Error(message, exception);
-    }
-
-    /// <inherit />
-    public void Fatal(string message) {
-      internal_logger_.Fatal(message);
-    }
-
-    /// <inherit />
-    public void Fatal(string message, Exception exception) {
-      internal_logger_.Fatal(message, exception);
-    }
-
-    /// <inherit />
-    public void Info(string message) {
-      internal_logger_.Info(message);
-    }
-
-    /// <inherit />
-    public void Info(string message, Exception exception) {
-      internal_logger_.Info(message, exception);
-    }
-
-    /// <inherit />
-    public void Warn(string message) {
-      internal_logger_.Warn(message);
-    }
-
-    /// <inherit />
-    public void Warn(string message, Exception exception) {
-      internal_logger_.Warn(message, exception);
+    /// <summary>
+    /// Gets the current configured application logger.
+    /// </summary>
+    public static MustLogger ForCurrentProcess {
+      get { return current_process_logger_; }
     }
   }
 }
