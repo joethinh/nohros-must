de45fbc9a5fca04042c6723631cbb39bbb0f6f05
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-01-25 14:30:40 +0000

    Created two interfaces to abstract the query concept.

---
+++ src/base/common/data/IQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Nohros.Data
+{
+  /// <summary>
+  /// Provides functionality to execute queries that retunrs no value against
+  /// a specific data provider.
+  /// </summary>
+  public interface IQuery
+  {
+    /// <summary>
+    /// Executes the query represented by the <see cref="IQuery{T}"/> object.
+    /// </summary>
+    /// <returns>
+    /// The result of the query.
+    /// </returns>
+    void Execute();
+  }
+}
---
+++ src/base/common/data/IQueryT.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nohros.Data
+{
+  /// <summary>
+  /// Provides functionality to execute queries that retunrs a value against a
+  /// specific data provider.
+  /// </summary>
+  /// <typeparam name="T">
+  /// The type of data in the data provider.
+  /// </typeparam>
+  public interface IQuery<out T>
+  {
+    /// <summary>
+    /// Executes the query represented by the <see cref="IQuery{T}"/> object.
+    /// </summary>
+    /// <returns>
+    /// The result of the query.
+    /// </returns>
+    T Execute();
+  }
+}
