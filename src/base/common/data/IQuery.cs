bcf50ef773b4adeed5f5a314e0d65171f5e5d518
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-01-26 13:15:10 +0000

    Merged the code for the IQuery classes in a single file.

--- src/base/common/data/IQuery.cs
+++ src/base/common/data/IQuery.cs
@@ -16,4 +16,63 @@ namespace Nohros.Data
     /// </returns>
     void Execute();
   }
+
+  /// <summary>
+  /// Provides functionality to execute queries that retunrs no value against
+  /// a specific data provider.
+  /// </summary>
+  public interface IQueryWithCriteria<in T>
+  {
+    /// <summary>
+    /// Executes the query represented by the
+    /// <see cref="IQueryWithCriteria{T}"/> object.
+    /// </summary>
+    /// <returns>
+    /// The result of the query.
+    /// </returns>
+    void Execute(T criteria);
+  }
+
+  /// <summary>
+  /// Provides functionality to execute queries that retunrs a value against a
+  /// specific data provider.
+  /// </summary>
+  /// <typeparam name="TResult">
+  /// The type of data in the data provider.
+  /// </typeparam>
+  /// <typeparam name="TCriteria">
+  /// The type of the object that contains the query criteria.
+  /// </typeparam>
+  public interface IQuery<out TResult, in TCriteria>
+  {
+    /// <summary>
+    /// Executes the query represented by the
+    /// <see cref="IQuery{TResult, TCriteria}"/> object.
+    /// </summary>
+    /// <param name="criteria">
+    /// A object that contains the query criteria(i.e. query filters)
+    /// </param>
+    /// <returns>
+    /// The result of the query.
+    /// </returns>
+    TResult Execute(TCriteria criteria);
+  }
+
+  /// <summary>
+  /// Provides functionality to execute queries that retunrs a value against a
+  /// specific data provider.
+  /// </summary>
+  /// <typeparam name="T">
+  /// The type of data in the data provider.
+  /// </typeparam>
+  public interface IQuery<out T>
+  {
+    /// <summary>
+    /// Executes the query represented by the <see cref="IQuery{T}"/> object.
+    /// </summary>
+    /// <returns>
+    /// The result of the query.
+    /// </returns>
+    T Execute();
+  }
 }
