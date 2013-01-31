efb61926c51e3b32fe09815950159407bf405c4c
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-01-31 12:34:18 +0000

    Visual Studio project update.

--- trunk/src/base/common/properties/AssemblyInfo.cs
+++ trunk/src/base/common/properties/AssemblyInfo.cs
@@ -42,6 +42,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.6.8.9")]
-[assembly: AssemblyFileVersion("1.6.8.9")]
+[assembly: AssemblyVersion("1.6.8.11")]
+[assembly: AssemblyFileVersion("1.6.8.11")]
 
