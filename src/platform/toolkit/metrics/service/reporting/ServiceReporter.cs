ed502d978b6f14fc41c17840a1f63addc8524def
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-03-09 15:28:20 +0000

    Created the project that should be used in the client side to communicate with the metrics reporting service.

---
+++ src/platform/toolkit/metrics/service/reporting/ServiceReporter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nohros.Metrics.Reporting
+{
+  /// <summary>
+  /// A simple reporter which sends out application metrics to a metrics
+  /// service periodically.
+  /// </summary>
+  public class ServiceReporter : AbstractPollingReporter
+  {
+    #region .ctor
+    public ServiceReporter(IMetricsRegistry registry) : base(registry) {
+    }
+    #endregion
+
+    public override void Run() {
+      throw new NotImplementedException();
+    }
+  }
+}
