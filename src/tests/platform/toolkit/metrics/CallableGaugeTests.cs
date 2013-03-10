2028e39f420bea7619b3f1277c26d78c0078ca58
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-03-10 21:02:11 +0000

    Unit tests

---
+++ src/tests/platform/toolkit/metrics/CallableGaugeTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Nohros.Metrics
+{
+  public class CallableGaugeTests
+  {
+    [Test]
+    public void ShouldReturnTheValueOfCallable() {
+      var gauge = new CallableGauge<int>(() => 10);
+      Assert.AreEqual(10, gauge.Value);
+    }
+  }
+}
---
+++ src/tests/platform/toolkit/metrics/MeterTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Nohros.Metrics
+{
+  public class MeterTests
+  {
+    [Test]
+    public void ShouldCreateBlankMeter() {
+      var meter = new Meter("thing", TimeUnit.Seconds);
+      Assert.That(meter.Count, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void ShouldCreateMeterWithThreeeEvents() {
+      var meter = new Meter("thing", TimeUnit.Seconds);
+      meter.Mark(3);
+      Assert.That(meter.Count, Is.EqualTo(3));
+    }
+  }
+}
