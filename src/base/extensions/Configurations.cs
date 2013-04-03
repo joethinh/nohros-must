5873ba993653b371343883940859db0fa00a14b9
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-04-03 16:48:02 +0000

    Created a extension for the IConfiguration interface that can be used to create instances of a specific class using the information contained in a given provider.

---
+++ src/base/extensions/Configurations.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Nohros.Configuration;
+
+namespace Nohros.Extensions
+{
+  /// <summary>
+  /// Extension methods for <see cref="IConfiguration"/> classes.
+  /// </summary>
+  public static class Configurations
+  {
+    /// <summary>
+    /// Creates an instance of the <see cref="TResult"/> class using the
+    /// information contained in the provider node named
+    /// <see cref="node_name"/>.
+    /// </summary>
+    /// <typeparam name="TFactory">
+    /// The type of the factory class that is used to create instances of the
+    /// <see cref="TResult"/> class.
+    /// </typeparam>
+    /// <typeparam name="TResult">
+    /// The type of the class that should be created.
+    /// </typeparam>
+    /// <param name="settings">
+    /// A <see cref="IConfiguration"/> class contained the list of configured
+    /// providers.
+    /// </param>
+    /// <param name="node_name">
+    /// The name of the provider node that contains the information about the
+    /// provider to be created.
+    /// </param>
+    /// <param name="instantiator">
+    /// A <see cref="Func{T1, T2, TResult}"/> delegate that can be
+    /// used to create an instance of the <see cref="TResult"/> class.
+    /// </param>
+    /// <returns>
+    /// The <see cref="TFactory"/> class is created by either, using a
+    /// constructor that accepts a <see cref="IConfiguration"/> object or a
+    /// constructor that receives no parameters.
+    /// </returns>
+    public static TResult CreateProvider<TFactory, TResult>(
+      this IConfiguration settings, string node_name,
+      Func<TFactory, IDictionary<string, string>, TResult> instantiator)
+      where TFactory : class {
+      var node = settings
+        .Providers
+        .GetProviderNode(node_name);
+      TFactory factory = RuntimeTypeFactory<TFactory>
+        .CreateInstanceFallback(node, settings);
+      return instantiator(factory, node.Options.ToDictionary());
+    }
+  }
+}
