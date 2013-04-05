85ba4a6464595bde8f37c4b1ceaf624e81f3fab1
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-04-05 00:07:47 +0000

    * Added IQuery interfaces that accepts parameters. * Created a ICommand interface.

--- src/base/common/data/ICommand.cs
+++ src/base/common/data/ICommand.cs
@@ -3,45 +3,43 @@
 namespace Nohros.Data
 {
   /// <summary>
-  /// Provides functionality to execute queries that returns a value against a
-  /// specific data provider.
+  /// Provides functionality to execute queries that retunrs no value against
+  /// a specific data provider.
   /// </summary>
-  public interface IQuery<out TResult>
+  public interface ICommand
   {
     /// <summary>
-    /// Executes the query represented by the <see cref="IQuery{T}"/> object.
+    /// Executes the command represented by the
+    /// <see cref="ICommand{T}"/> object.
     /// </summary>
-    /// <returns>
-    /// The result of the query.
-    /// </returns>
-    TResult Execute();
+    void Execute();
   }
 
   /// <summary>
   /// Provides functionality to execute queries that retunrs no value and has
   /// one parameter against a specific data provider.
   /// </summary>
-  public interface IQuery<out TResult, in T1>
+  public interface ICommand<in T>
   {
     /// <summary>
     /// Executes the command represented by the
-    /// <see cref="IQuery{T}"/> object.
+    /// <see cref="ICommand{T}"/> object.
     /// </summary>
     /// <param name="arg1">
     /// The type of the first parameter of the query.
     /// </param>
-    TResult Execute(T1 arg1);
+    void Execute(T arg1);
   }
 
   /// <summary>
   /// Provides functionality to execute queries that retunrs no value and has
   /// two parameter against a specific data provider.
   /// </summary>
-  public interface IQuery<out TResult, in T, in T2>
+  public interface ICommand<in T, in T2>
   {
     /// <summary>
     /// Executes the command represented by the
-    /// <see cref="IQuery{T}"/> object.
+    /// <see cref="ICommand{T}"/> object.
     /// </summary>
     /// <param name="arg1">
     /// The type of the first parameter of the query.
@@ -49,18 +47,18 @@ namespace Nohros.Data
     /// <param name="arg2">
     /// The type of the second parameter of the query.
     /// </param>
-    TResult Execute(T arg1, T2 arg2);
+    void Execute(T arg1, T2 arg2);
   }
 
   /// <summary>
   /// Provides functionality to execute queries that retunrs no value and has
   /// three parameter against a specific data provider.
   /// </summary>
-  public interface IQuery<out TResult, in T, in T2, in T3>
+  public interface ICommand<in T, in T2, in T3>
   {
     /// <summary>
     /// Executes the command represented by the
-    /// <see cref="IQuery{T}"/> object.
+    /// <see cref="ICommand{T}"/> object.
     /// </summary>
     /// <param name="arg1">
     /// The type of the first parameter of the query.
@@ -71,18 +69,18 @@ namespace Nohros.Data
     /// <param name="arg3">
     /// The type of the third parameter of the query.
     /// </param>
-    TResult Execute(T arg1, T2 arg2, T3 arg3);
+    void Execute(T arg1, T2 arg2, T3 arg3);
   }
 
   /// <summary>
   /// Provides functionality to execute queries that retunrs no value and has
   /// four parameter against a specific data provider.
   /// </summary>
-  public interface IQuery<out TResult, in T, in T2, in T3, in T4>
+  public interface ICommand<in T, in T2, in T3, in T4>
   {
     /// <summary>
     /// Executes the command represented by the
-    /// <see cref="IQuery{T}"/> object.
+    /// <see cref="ICommand{T}"/> object.
     /// </summary>
     /// <param name="arg1">
     /// The type of the first parameter of the query.
@@ -96,18 +94,18 @@ namespace Nohros.Data
     /// <param name="arg4">
     /// The type of the fourth parameter of the query.
     /// </param>
-    TResult Execute(T arg1, T2 arg2, T3 arg3, T4 arg4);
+    void Execute(T arg1, T2 arg2, T3 arg3, T4 arg4);
   }
 
   /// <summary>
   /// Provides functionality to execute queries that retunrs no value and has
   /// five parameter against a specific data provider.
   /// </summary>
-  public interface IQuery<out TResult, in T, in T2, in T3, in T4, in T5>
+  public interface ICommand<in T, in T2, in T3, in T4, in T5>
   {
     /// <summary>
     /// Executes the command represented by the
-    /// <see cref="IQuery{T}"/> object.
+    /// <see cref="ICommand{T}"/> object.
     /// </summary>
     /// <param name="arg1">
     /// The type of the first parameter of the query.
@@ -124,18 +122,18 @@ namespace Nohros.Data
     /// <param name="arg5">
     /// The type of the fifth parameter of the query.
     /// </param>
-    TResult Execute(T arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
+    void Execute(T arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
   }
 
   /// <summary>
   /// Provides functionality to execute queries that retunrs no value and has
   /// six parameter against a specific data provider.
   /// </summary>
-  public interface IQuery<out TResult, in T, in T2, in T3, in T4, in T5, in T6>
+  public interface ICommand<in T, in T2, in T3, in T4, in T5, in T6>
   {
     /// <summary>
     /// Executes the command represented by the
-    /// <see cref="IQuery{T}"/> object.
+    /// <see cref="ICommand{T}"/> object.
     /// </summary>
     /// <param name="arg1">
     /// The type of the first parameter of the query.
@@ -155,19 +153,18 @@ namespace Nohros.Data
     /// <param name="arg6">
     /// The type of the sixth parameter of the query.
     /// </param>
-    TResult Execute(T arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6);
+    void Execute(T arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6);
   }
 
   /// <summary>
   /// Provides functionality to execute queries that retunrs no value and has
   /// seven parameter against a specific data provider.
   /// </summary>
-  public interface IQuery<out TResult, in T, in T2, in T3, in T4, in T5, in T6,
-                          in T7>
+  public interface ICommand<in T, in T2, in T3, in T4, in T5, in T6, in T7>
   {
     /// <summary>
     /// Executes the command represented by the
-    /// <see cref="IQuery{T}"/> object.
+    /// <see cref="ICommand{T}"/> object.
     /// </summary>
     /// <param name="arg1">
     /// The type of the first parameter of the query.
@@ -190,19 +187,19 @@ namespace Nohros.Data
     /// <param name="arg7">
     /// The type of the seventh parameter of the query.
     /// </param>
-    TResult Execute(T arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7);
+    void Execute(T arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7);
   }
 
   /// <summary>
   /// Provides functionality to execute queries that retunrs no value and has
   /// eight parameter against a specific data provider.
   /// </summary>
-  public interface IQuery<out TResult, in T, in T2, in T3, in T4, in T5, in T6,
-                          in T7, in T8>
+  public interface ICommand<in T, in T2, in T3, in T4, in T5, in T6, in T7,
+                            in T8>
   {
     /// <summary>
     /// Executes the command represented by the
-    /// <see cref="IQuery{T}"/> object.
+    /// <see cref="ICommand{T}"/> object.
     /// </summary>
     /// <param name="arg1">
     /// The type of the first parameter of the query.
@@ -228,7 +225,7 @@ namespace Nohros.Data
     /// <param name="arg8">
     /// The type of the eighth parameter of the query.
     /// </param>
-    TResult Execute(T arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7,
+    void Execute(T arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7,
       T8 arg8);
   }
 
@@ -236,12 +233,12 @@ namespace Nohros.Data
   /// Provides functionality to execute queries that retunrs no value and has
   /// nine parameter against a specific data provider.
   /// </summary>
-  public interface IQuery<out TResult, in T, in T2, in T3, in T4, in T5, in T6,
-                          in T7, in T8, in T9>
+  public interface ICommand<in T, in T2, in T3, in T4, in T5, in T6, in T7,
+                            in T8, in T9>
   {
     /// <summary>
     /// Executes the command represented by the
-    /// <see cref="IQuery{T}"/> object.
+    /// <see cref="ICommand{T}"/> object.
     /// </summary>
     /// <param name="arg1">
     /// The type of the first parameter of the query.
@@ -270,7 +267,7 @@ namespace Nohros.Data
     /// <param name="arg9">
     /// The type of the ninth parameter of the query.
     /// </param>
-    TResult Execute(T arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7,
+    void Execute(T arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7,
       T8 arg8, T9 arg9);
   }
 
@@ -278,12 +275,12 @@ namespace Nohros.Data
   /// Provides functionality to execute queries that retunrs no value and has
   /// ten parameter against a specific data provider.
   /// </summary>
-  public interface IQuery<out TResult, in T, in T2, in T3, in T4, in T5, in T6,
-                          in T7, in T8, in T9, in T10>
+  public interface ICommand<in T, in T2, in T3, in T4, in T5, in T6, in T7,
+                            in T8, in T9, in T10>
   {
     /// <summary>
     /// Executes the command represented by the
-    /// <see cref="IQuery{T}"/> object.
+    /// <see cref="ICommand{T}"/> object.
     /// </summary>
     /// <param name="arg1">
     /// The type of the first parameter of the query.
@@ -315,7 +312,7 @@ namespace Nohros.Data
     /// <param name="arg10">
     /// The type of the tenth parameter of the query.
     /// </param>
-    TResult Execute(T arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7,
+    void Execute(T arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7,
       T8 arg8, T9 arg9, T10 arg10);
   }
 }
