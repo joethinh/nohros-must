eeb9faa870b2b912eed7df5f17783a347cb64739
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-05-13 19:39:57 +0000

    Added a new Map method to the DataReaderMapper class, that allow map to occur using the object property through lambda expressions.

--- src/base/extensions/Strings.cs
+++ src/base/extensions/Strings.cs
@@ -22,20 +22,20 @@ namespace Nohros.Extensions
         Compare(str, comparand, StringComparison.CurrentCultureIgnoreCase) == 0;
     }
 
-    public static string Format(this string str, object arg0) {
+    public static string Fmt(this string str, object arg0) {
       return string.Format(str, arg0);
     }
 
-    public static string Format(this string str, object arg0, object arg1) {
+    public static string Fmt(this string str, object arg0, object arg1) {
       return string.Format(str, arg0, arg1);
     }
 
-    public static string Format(this string str, object arg0, object arg1,
+    public static string Fmt(this string str, object arg0, object arg1,
       object arg2) {
       return string.Format(str, arg0, arg1, arg2);
     }
 
-    public static string Format(this string str, params object[] args) {
+    public static string Fmt(this string str, params object[] args) {
       return string.Format(str, args);
     }
 
@@ -45,7 +45,7 @@ namespace Nohros.Extensions
       for (int i = 0, j = normalized_string.Length; i < j; i++) {
         UnicodeCategory category =
           CharUnicodeInfo.GetUnicodeCategory(normalized_string[i]);
-        if(category != UnicodeCategory.NonSpacingMark) {
+        if (category != UnicodeCategory.NonSpacingMark) {
           builder.Append(normalized_string[i]);
         }
       }
