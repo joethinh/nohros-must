ca56e740ac9509c5ca41ded47db9006615b78f1f
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-07-09 18:49:49 +0000

    Added some convenient method to the StateByNameQuery class. This methods is used to convert the string state to int or Guid.

---
+++ src/base/common/data/repositories/IStateRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Nohros.Data
+{
+  /// <summary>
+  /// Represents a state repository which is a repository that contains a
+  /// collection of key/value pairs representing states of something.
+  /// </summary>
+  public interface IStateRepository
+  {
+    /// <summary>
+    /// Creates a query that returns the state that is associated with a
+    /// given name.
+    /// </summary>
+    /// <param name="query">
+    /// The newly create <see cref="IStateByNameQuery"/> query.
+    /// </param>
+    /// <returns>
+    /// The newly create <see cref="IStateByNameQuery"/> query.
+    /// </returns>
+    IStateByNameQuery Query(out IStateByNameQuery query);
+  }
+}
