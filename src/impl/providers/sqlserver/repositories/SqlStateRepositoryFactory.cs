552fc73df3f8f51d43551f2688bb7560d86f9512
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-07-09 18:46:29 +0000

    Implemented the IStateRepository interface for the Sql Server engine.

---
+++ src/impl/providers/sqlserver/repositories/SqlStateRepositoryFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nohros.Data.SqlServer
+{
+  public class SqlStateRepositoryFactory : IStateRepositoryFactory
+  {
+    public IStateRepository CreateStateRepository(
+      IDictionary<string, string> options) {
+      var factory = new SqlConnectionProviderFactory();
+      var sql_connection_provider = factory
+        .CreateProvider(options) as SqlConnectionProvider;
+      return new SqlStateRepository(sql_connection_provider);
+    }
+  }
+}
