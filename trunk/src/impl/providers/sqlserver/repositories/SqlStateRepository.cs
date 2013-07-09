bc0e3d41bd0176f32bb8004e18951f316db50547
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-07-09 18:46:29 +0000

    Implemented the IStateRepository interface for the Sql Server engine.

---
+++ trunk/src/impl/providers/sqlserver/repositories/SqlStateRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Nohros.Data.SqlServer
+{
+  /// <summary>
+  /// An implementation of the <see cref="IStateRepository"/> class for the
+  /// Microsfot SQL Server Engine.
+  /// </summary>
+  public class SqlStateRepository : IStateRepository
+  {
+    readonly SqlConnectionProvider sql_connection_provider_;
+
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IStateRepository"/>
+    /// class using the specified <see cref="SqlConnectionProvider"/>
+    /// class.
+    /// </summary>
+    /// <param name="sql_connection_provider">
+    /// A <see cref="SqlConnectionProvider"/> object that can be used to
+    /// get a connection to a SQL server.
+    /// </param>
+    public SqlStateRepository(SqlConnectionProvider sql_connection_provider) {
+      sql_connection_provider_ = sql_connection_provider;
+    }
+    #endregion
+
+    /// <inheritdoc/>
+    public IStateByNameQuery Query(out IStateByNameQuery query) {
+      return query = new StateByNameQuery(sql_connection_provider_);
+    }
+  }
+}
---
+++ trunk/src/impl/providers/sqlserver/repositories/StateByNameQuery.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Nohros.Logging;
+using R = Nohros.Resources.StringResources;
+
+namespace Nohros.Data.SqlServer
+{
+  public class StateByNameQuery : AbstractStateByNameQuery
+  {
+    const string kClassName = "Nohros.Data.SqlServer.StateByNameQuery";
+
+    /// <summary>
+    /// The name of the stored procesdure that is used to query for the state.
+    /// </summary>
+    public const string kExecute = ".nohros_state_get";
+
+    /// <summary>
+    /// The name of the parameter that is used to specify the name of the
+    /// state to query.
+    /// </summary>
+    public const string kStateNameParameter = "@name";
+
+    readonly MustLogger logger_ = MustLogger.ForCurrentProcess;
+    readonly SqlConnectionProvider sql_connection_provider_;
+
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StateByNameQuery"/>
+    /// using the given <param ref="sql_connection_provider" />
+    /// </summary>
+    /// <param name="sql_connection_provider">
+    /// A <see cref="SqlConnectionProvider"/> object that can be used to
+    /// create connections to the data provider.
+    /// </param>
+    public StateByNameQuery(SqlConnectionProvider sql_connection_provider) {
+      sql_connection_provider_ = sql_connection_provider;
+      logger_ = MustLogger.ForCurrentProcess;
+    }
+    #endregion
+
+    /// <exception cref="ProviderException">
+    /// An exception has occured while executing the query.
+    /// </exception>
+    public override bool Execute(string name, out string state) {
+      using (SqlConnection conn = sql_connection_provider_.CreateConnection())
+      using (var builder = new CommandBuilder(conn)) {
+        IDbCommand cmd = builder
+          .SetText(sql_connection_provider_.Schema + kExecute)
+          .SetType(CommandType.StoredProcedure)
+          .AddParameter(kStateNameParameter, name)
+          .Build();
+        try {
+          conn.Open();
+          object obj = cmd.ExecuteScalar();
+          if (obj != null) {
+            state = (string) obj;
+            return true;
+          }
+        } catch (SqlException e) {
+          logger_.Error(string.Format(
+            R.Log_MethodThrowsException, "Execute", kClassName), e);
+          throw new ProviderException(e);
+        }
+        state = null;
+        return false;
+      }
+    }
+  }
+}
