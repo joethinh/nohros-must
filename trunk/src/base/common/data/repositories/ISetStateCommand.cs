c48a080978ff7f3df9295b3ee3907dc1f2fd08a7
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-07-09 21:14:57 +0000

    Created a command to associates a state with a key.

---
+++ trunk/src/base/common/data/repositories/ISetStateCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Nohros.Data
+{
+  /// <summary>
+  /// Associates a string representing a state with a given key.
+  /// </summary>
+  public interface ISetStateCommand
+  {
+    /// <summary>
+    /// Associates the state represented by <paramref name="state"/> with the
+    /// given <paramref name="name"/>.
+    /// </summary>
+    /// <param name="name">
+    /// A string that can be used to identify the state within a state
+    /// repository.
+    /// </param>
+    /// <param name="state">
+    /// The state to be persisted.
+    /// </param>
+    void Execute(string name, string state);
+  }
+}
