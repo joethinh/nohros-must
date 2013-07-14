ec536dc35bab2e0e7a3bfde6d16ce38bbc9d7c2c
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-07-14 21:35:29 +0000

    Created a factory for the LoginContext class.

---
+++ src/platform/toolkit/auth/LoginContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Nohros.Security.Auth;
+
+namespace Nohros.Security
+{
+  /// <summary>
+  /// A factory for the <see cref="LoginContext"/> class.
+  /// </summary>
+  public class LoginContextFactory
+  {
+    readonly IEnumerable<ILoginModuleFactory> login_module_factories_;
+
+    #region .ctor
+    public LoginContextFactory(
+      IEnumerable<ILoginModuleFactory> login_module_factories) {
+      login_module_factories_ = login_module_factories;
+    }
+    #endregion
+
+    public LoginContext CreateLoginContext(Subject subject) {
+    }
+
+    public LoginContext CreateLoginContext(Subject subject,
+      IAuthCallbackHandler callback) {
+    }
+
+    public LoginContext CreateLoginContext(IAuthCallbackHandler callback) {
+    }
+  }
+}
