8473a445cb18a523f5b2fc063ba8652fb5d73b3c
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-07-17 19:25:05 +0000

    Version update.

--- src/platform/toolkit/auth/Resources.Designer.cs
+++ src/platform/toolkit/auth/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Nohros.Security {
+namespace Nohros.Security.Auth {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Nohros.Security {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Nohros.Security.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Nohros.Security.Auth.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
