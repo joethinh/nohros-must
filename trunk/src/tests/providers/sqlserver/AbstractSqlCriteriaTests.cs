1a9fb28e7e20b8df1efc44b74f0ff5e662a2a9d7
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-07-23 23:29:51 +0000

    Created the concept of Criteria, that can be used to define the criteria of a query for a repository.

---
+++ trunk/src/tests/providers/sqlserver/AbstractSqlCriteriaTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using Nohros.Data;
+using Nohros.Data.SqlServer;
+
+namespace sqlserver
+{
+  public class AbstractSqlCriteriaTests
+  {
+    public class TestCriteria : AbstractCriteria<TestPoco>
+    {
+      #region .ctor
+      public TestCriteria() {
+        Map(x => x.Name, "t.name_on_database");
+        Map(x => x.ID, "t.id_on_database");
+      }
+      #endregion
+
+      public TestCriteria Select<TProperty>(
+        Expression<Func<TestPoco, TProperty>> expression) {
+        BaseSelect(expression);
+        return this;
+      }
+
+      public TestCriteria Where<TProperty>(
+        Expression<Func<TestPoco, TProperty>> expression, object value) {
+        BaseWhere(expression, value);
+        return this;
+      }
+    }
+
+    public class TestPoco
+    {
+      public string Name { get; set; }
+      public int ID { get; set; }
+    }
+
+    [Test]
+    public void ShoulBuildValidQuery() {
+      var criteria = new TestCriteria()
+        .Select(x => x.Name)
+        .Select(x => x.ID);
+
+      var cmd = new CommandBuilder(new SqlCommand());
+      SqlQueryResolver.Resolve(cmd, "from table", criteria);
+      IDbCommand command = cmd.Build();
+      Assert.That(command.CommandText,
+        Is.EqualTo("select t.name_on_database,t.id_on_database from table"));
+    }
+  }
+}
