286d60539d3c3cffa8a254a68a310b095de8f71c
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-08-30 02:54:50 +0000

    Merged the code of the eventsourcing and eventstore projects.

--- trunk/src/platform/toolkit/cqrs/eventstore/properties/AssemblyInfo.cs
+++ trunk/src/platform/toolkit/cqrs/eventstore/properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("nohros.eventstore.protobuf")]
-[assembly: AssemblyDescription("nohros.eventstore.protobuf")]
+[assembly: AssemblyTitle("nohros.eventstore")]
+[assembly: AssemblyDescription("nohros.eventstore")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nohros Inc")]
 [assembly: AssemblyProduct("Nohros Inc")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.3.0")]
+[assembly: AssemblyFileVersion("0.0.3.0")]
