08b858ff006ecc18fe5f946e17042bbc20438525
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-08-31 13:19:57 +0000

    Created a implementation of the IRepository interface that uses the EventStore as storage mechanism

---
+++ src/platform/toolkit/cqrs/eventstore/EventSerializers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using EventStore.ClientAPI;
+using Nohros.CQRS.Messaging;
+
+namespace Nohros.CQRS.EventStore
+{
+  public static class EventSerializers
+  {
+    /// <summary>
+    /// Creates a <see cref="IEventSerializer"/> that uses the
+    /// <paramref name="serializer"/> delegate to serialize a event and the
+    /// <paramref name="deserializer"/> delegate to deserialize a event.
+    /// </summary>
+    /// <param name="serializer">
+    /// A <see cref="Func{T, TResult}"/> that is used to serialize a event.
+    /// </param>
+    /// <param name="deserializer">
+    /// A <see cref="Func{T, TResult}"/> that is used to deserialize a event.
+    /// </param>
+    /// <returns>
+    /// A <see cref="IEventSerializer"/> that uses the
+    /// <paramref name="serializer"/> delegate to serialize a event and the
+    /// <paramref name="deserializer"/> delegate to deserialize a event.
+    /// </returns>
+    public static IEventSerializer FuncToEventSerializer(
+      Func<Event, EventData> serializer, Func<EventData, Event> deserializer) {
+      return new FuncEventSerializer(serializer, deserializer);
+    }
+  }
+}
---
+++ src/platform/toolkit/cqrs/eventstore/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nohros.CQRS.Domain;
+using Nohros.CQRS.Messaging;
+
+namespace Nohros.CQRS.EventStore
+{
+  public class Repository<T> : IRepository<T> where T : AggregateRoot
+  {
+    readonly Func<Guid, T> aggregate_factory_;
+    readonly IEventSerializer serialzer_;
+    readonly EventStorage storage_;
+
+    #region .ctor
+    public Repository(EventStorage storage, IEventSerializer serializer,
+      Func<Guid, T> aggregate_factory) {
+      storage_ = storage;
+      serialzer_ = serializer;
+      aggregate_factory_ = aggregate_factory;
+    }
+    #endregion
+
+    public void Save(AggregateRoot aggregate, int expected_version) {
+      var events = aggregate.GetUncommittedChanges().ToArray();
+      storage_.SaveEvents(aggregate.ID, events, expected_version, serialzer_);
+    }
+
+    public T GetByID(Guid id) {
+      T aggregate = aggregate_factory_(id);
+      ICollection<Event> events = storage_.GetEventsForAggregate(id, serialzer_);
+      aggregate.LoadFromHistory(events);
+      return aggregate;
+    }
+  }
+}
