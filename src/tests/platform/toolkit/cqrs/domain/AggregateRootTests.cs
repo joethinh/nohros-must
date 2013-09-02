a518c9bbe106be12bad49a827431b525c9ab1d2c
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-09-02 21:12:53 +0000

    Unit tests for the cqrs projects.

---
+++ src/tests/platform/toolkit/cqrs/domain/AggregateRootTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+using Nohros.CQRS.Messaging;
+
+namespace Nohros.CQRS.Domain
+{
+  public class AggregateRootTests
+  {
+    class Aggregate : AggregateRoot
+    {
+      Guid id_;
+
+      #region .ctor
+      public Aggregate(Guid id) {
+        id_ = id;
+        Subscribe<Created>(Apply);
+      }
+      #endregion
+
+      void Apply(Created e) {
+        id_ = e.ID;
+      }
+
+      public override Guid ID {
+        get { return id_; }
+      }
+    }
+
+    class Created : Event
+    {
+      public readonly Guid ID;
+
+      #region .ctor
+      public Created(Guid id) {
+        ID = id;
+      }
+      #endregion
+    }
+
+    [Test]
+    public void should_replay_events() {
+      var aggregate = new Aggregate(Guid.Empty);
+      var guid = Guid.NewGuid();
+      aggregate.LoadFromHistory(new Event[] {Deserialize(guid)});
+      Assert.That(aggregate.ID, Is.EqualTo(guid));
+    }
+
+    Event Deserialize(Guid guid) {
+      return new Created(guid);
+    }
+  }
+}
---
+++ src/tests/platform/toolkit/cqrs/messaging/InMemoryBusTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Nohros.CQRS.Messaging
+{
+  public class InMemoryBusTests
+  {
+    class MyMessage : IMessage
+    {
+      public string Message { get; set; }
+    }
+
+    [Test]
+    public void should_publish_message_to_subscribers() {
+      var bus = new InMemoryBus();
+      bus.Subscribe(
+        Handlers.Runnable<MyMessage>(
+          msg => Assert.That(msg.Message, Is.EqualTo("MyMessage"))));
+      bus.Publish(new MyMessage {Message = "MyMessage"});
+    }
+  }
+}
