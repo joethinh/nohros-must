ab32391e1786bd9dedc314386e69605d6eec711e
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-09-03 20:03:57 +0000

    Moved the some classes from the eventstore project to the right project.

--- src/platform/toolkit/cqrs/eventstore/Repository.cs
+++ src/platform/toolkit/cqrs/eventstore/Repository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using EventStore.ClientAPI.Exceptions;
 using Nohros.CQRS.Domain;
 using Nohros.CQRS.Messaging;
 
@@ -8,29 +9,56 @@ namespace Nohros.CQRS.EventStore
 {
   public class Repository<T> : IRepository<T> where T : AggregateRoot
   {
-    readonly Func<Guid, T> aggregate_factory_;
+    readonly IAggregateFactory<T> aggregate_factory_;
+    readonly IConflictEvaluator conflict_evaluator_;
     readonly IEventSerializer serialzer_;
-    readonly EventStorage storage_;
+    readonly IEventStorage storage_;
 
     #region .ctor
-    public Repository(EventStorage storage, IEventSerializer serializer,
-      Func<Guid, T> aggregate_factory) {
+    public Repository(IEventStorage storage, IEventSerializer serializer,
+      IAggregateFactory<T> aggregate_factory,
+      IConflictEvaluator conflict_evaluator) {
       storage_ = storage;
       serialzer_ = serializer;
       aggregate_factory_ = aggregate_factory;
+      conflict_evaluator_ = conflict_evaluator;
     }
     #endregion
 
     public void Save(AggregateRoot aggregate, int expected_version) {
       var events = aggregate.GetUncommittedChanges().ToArray();
-      storage_.SaveEvents(aggregate.ID, events, expected_version, serialzer_);
+
+      int current_expected_version = expected_version;
+      while (true) {
+        try {
+          Save(aggregate.ID, events, current_expected_version);
+          break;
+        } catch (WrongExpectedVersionException e) {
+          IList<Event> events_since =
+            storage_.GetEventsForAggregate(aggregate.ID, serialzer_,
+              expected_version);
+
+          foreach (Event commited_event in events_since) {
+            foreach (Event @event in events) {
+              if (conflict_evaluator_.ConflictWith(commited_event, @event)) {
+                throw new ConcurrencyException();
+              }
+            }
+          }
+          current_expected_version = events_since.Last().Version;
+        }
+      }
     }
 
     public T GetByID(Guid id) {
-      T aggregate = aggregate_factory_(id);
+      T aggregate = aggregate_factory_.CreateAggregate(id);
       ICollection<Event> events = storage_.GetEventsForAggregate(id, serialzer_);
       aggregate.LoadFromHistory(events);
       return aggregate;
+    }
+
+    void Save(Guid id, ICollection<Event> events, int expected_version) {
+      storage_.SaveEvents(id, events, expected_version, serialzer_);
     }
   }
 }
---
+++ src/tests/platform/toolkit/cqrs/eventstore/RepositoryTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using EventStore.ClientAPI;
+using EventStore.ClientAPI.Exceptions;
+using NUnit.Framework;
+using Nohros.CQRS.Domain;
+using Nohros.CQRS.Messaging;
+using Telerik.JustMock;
+
+namespace Nohros.CQRS.EventStore
+{
+  public class RepositoryTests
+  {
+    class Created : Event
+    {
+      #region .ctor
+      public Created() {
+        Version = 0;
+      }
+      #endregion
+    }
+
+    class DummyAggregate : AggregateRoot
+    {
+      Guid id_;
+
+      #region .ctor
+      public DummyAggregate(Guid id) {
+        ApplyChange(new Created());
+      }
+      #endregion
+
+      public override Guid ID {
+        get { return id_; }
+      }
+    }
+
+    class Modified : Event
+    {
+      #region .ctor
+      public Modified() {
+        Version = 2;
+      }
+      #endregion
+    }
+
+    IEventSerializer serializer_;
+
+    [SetUp]
+    public void SetUp() {
+      serializer_ = EventSerializers.FuncToEventSerializer(
+        @event =>
+          new EventData(Guid.Empty, typeof (Created).FullName, false,
+            new byte[0], new byte[0]),
+        data => (Event) Activator.CreateInstance(Type.GetType(data.Type)));
+    }
+
+    [Test]
+    public void should_store_events_if_they_not_conclict() {
+      var storage = Mock.Create<IEventStorage>();
+
+      var id = Guid.Empty;
+      Mock
+        .Arrange(() => storage.GetEventsForAggregate(id, serializer_, 1))
+        .Returns(new List<Event> {
+          new Modified()
+        });
+
+      Mock
+        .Arrange(() => storage
+          .SaveEvents(Arg.IsAny<Guid>(), Arg.IsAny<ICollection<Event>>(), 1,
+            serializer_))
+        .Throws(new WrongExpectedVersionException(string.Empty));
+
+      var repository = new Repository<DummyAggregate>(storage, serializer_,
+        AggregateFactories.FromFunc(guid => new DummyAggregate(guid)),
+        ConflictEvaluators.FromFunc((@event, event1) => false));
+      var aggregate = new DummyAggregate(Guid.Empty);
+
+      Assert.That(() => repository.Save(aggregate, 1), Throws.Nothing);
+    }
+
+    [Test]
+    public void should_throw_concurrency_exception_if_events_conflict() {
+      var storage = Mock.Create<IEventStorage>();
+
+      var id = Guid.Empty;
+      Mock
+        .Arrange(() => storage.GetEventsForAggregate(id, serializer_, 1))
+        .Returns(new List<Event> {
+          new Modified()
+        });
+
+      Mock
+        .Arrange(() => storage
+          .SaveEvents(Arg.IsAny<Guid>(), Arg.IsAny<ICollection<Event>>(), 1,
+            serializer_))
+        .Throws(new WrongExpectedVersionException(string.Empty));
+
+      var repository = new Repository<DummyAggregate>(storage, serializer_,
+        AggregateFactories.FromFunc(guid => new DummyAggregate(guid)),
+        ConflictEvaluators.FromFunc((@event, event1) => true));
+      var aggregate = new DummyAggregate(Guid.Empty);
+
+      Assert.That(() => repository.Save(aggregate, 1),
+        Throws.TypeOf<ConcurrencyException>());
+    }
+  }
+}
