532f5cfccce3e86f2e5451a9d28d98f679f3a741
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-09-18 21:14:01 +0000

    * Fix a bug that causes the events from the aggregate root to not be published.

--- src/tests/platform/toolkit/cqrs/domain/AggregateRootTests.cs
+++ src/tests/platform/toolkit/cqrs/domain/AggregateRootTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using NUnit.Framework;
 using Nohros.CQRS.Messaging;
 
@@ -8,21 +9,39 @@ namespace Nohros.CQRS.Domain
   {
     class Aggregate : AggregateRoot
     {
+      AggregateEntity entity_;
       Guid id_;
 
       #region .ctor
-      public Aggregate(Guid id) {
-        id_ = id;
+      public Aggregate() {
         Subscribe<Created>(Apply);
+      }
+
+      public Aggregate(Guid id):this() {
+        ApplyChange(new Created(id));
       }
       #endregion
 
+      public void GenerateEntityEvent(Event e) {
+        entity_.GenerateEntityEvent(e);
+      }
+
       void Apply(Created e) {
+        entity_ = new AggregateEntity();
+
+        AddEntity(entity_);
         id_ = e.ID;
       }
 
       public override Guid ID {
         get { return id_; }
+      }
+    }
+
+    class AggregateEntity : Entity
+    {
+      public void GenerateEntityEvent(Event e) {
+        ApplyChange(e);
       }
     }
 
@@ -37,12 +56,45 @@ namespace Nohros.CQRS.Domain
       #endregion
     }
 
+    class EntityCreated : Event
+    {
+      public readonly Guid ID;
+
+      #region .ctor
+      public EntityCreated(Guid id) {
+        ID = id;
+      }
+      #endregion
+    }
+
     [Test]
     public void should_replay_events() {
-      var aggregate = new Aggregate(Guid.Empty);
+      var aggregate = new Aggregate();
       var guid = Guid.NewGuid();
       aggregate.LoadFromHistory(new Event[] {Deserialize(guid)});
       Assert.That(aggregate.ID, Is.EqualTo(guid));
+    }
+
+    [Test]
+    public void should_publish_events_from_internal_entities() {
+      var aggregate = new Aggregate(Guid.Empty);
+      var guid = Guid.NewGuid();
+      aggregate.GenerateEntityEvent(new EntityCreated(guid));
+      var events = aggregate.GetUncommittedChanges();
+      Assert.That(
+        events.Any(@event => @event.GetType() == typeof (EntityCreated)));
+    }
+
+    [Test]
+    public void should_publish_events_from_internal_entities_and_root() {
+      var guid = Guid.NewGuid();
+      var aggregate = new Aggregate(guid);
+      aggregate.GenerateEntityEvent(new EntityCreated(guid));
+      var events = aggregate.GetUncommittedChanges();
+      Assert.That(
+        events.Any(@event => @event.GetType() == typeof (EntityCreated)));
+      Assert.That(
+        events.Any(@event => @event.GetType() == typeof (Created)));
     }
 
     Event Deserialize(Guid guid) {
