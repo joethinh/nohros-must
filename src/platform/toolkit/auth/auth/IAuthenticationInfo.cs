6fd66c590d674ed983a7ab7edb981fc799070840
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-10-17 22:44:22 +0000

    Changed the implementation of the login module interface to make it more easy to use.

---
+++ src/platform/toolkit/auth/auth/IAuthenticationInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Nohros.Security.Auth
+{
+  /// <summary>
+  /// Provides a way for <see cref="ILoginModule"/> to store transient
+  /// authentication status.
+  /// </summary>
+  public interface IAuthenticationInfo
+  {
+    /// <summary>
+    /// Gets a value indicating if the associated authentication operation
+    /// has succeeded.
+    /// </summary>
+    /// <value>
+    /// <c>true</c> if the authentication operation has succeeded; otherwise,
+    /// <c>false</c>.
+    /// </value>
+    bool Authenticated { get; }
+  }
+}
