3ef7a57ea9816cc54edf2e8e4ca1360437c8e7a7
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-10-21 17:07:09 +0000

    * Created a Set that raise events when an item is added. * Added two events to the AbstractSubject class to allow callers to know when a permission and principal is added to the subject.

---
+++ src/platform/toolkit/auth/auth/events/PermissionEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Nohros.Security.Auth
+{
+  /// <summary>
+  /// Represents the method that handles events associated with a
+  /// <see cref="IPermission"/> object.
+  /// </summary>
+  /// <param name="permission">
+  /// The <see cref="IPermission"/> associated with the event.
+  /// </param>
+  public delegate void PermissionEventHandler(IPermission permission);
+}
