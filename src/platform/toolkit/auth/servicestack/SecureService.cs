e20fd19ca61f72eb60d3147196c4b58a83617461
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-10-23 18:30:54 +0000

    * Created a binding to the ServiceStack framework.

---
+++ src/platform/toolkit/auth/servicestack/SecureService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web;
+using Nohros.Security.Auth;
+using ServiceStack.Common.Web;
+using ServiceStack.ServiceInterface;
+
+namespace Nohros.Security.ServiceStack
+{
+  [Secure]
+  public class SecureService : Service
+  {
+    readonly HttpAuthenticationManager authenticator_manager_;
+
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SecureService"/> class
+    /// by using the given <see cref="HttpAuthenticationManager"/> object.
+    /// </summary>
+    /// <param name="authenticator_manager">
+    /// A <see cref="HttpAuthenticationManager"/> object that can be used to
+    /// get a instance of the currently logged in <see cref="ISubject"/>.
+    /// </param>
+    public SecureService(HttpAuthenticationManager authenticator_manager) {
+      authenticator_manager_ = authenticator_manager;
+    }
+    #endregion
+
+    /// <summary>
+    /// Gets the <see cref="ISubject"/> object that is associated with the
+    /// current HTTP request.
+    /// </summary>
+    /// <exception cref="HttpError.Unauthorized">
+    /// There is not subject associated with the current request.
+    /// </exception>
+    public ISubject Subject {
+      get {
+        ISubject subject = null;
+        var context = HttpContext.Current;
+        // Sanity check the context and subject.
+        if (authenticator_manager_.GetSubject(context, out subject)) {
+          throw HttpError.Unauthorized(Resources.Request_Unauthorized);
+        }
+        return subject;
+      }
+    }
+  }
+}
