2f193f8a22f0e030d2d22b30c7c7aab2d2f81fef
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-10-26 00:22:54 +0000

    Fix a bug on the HttpAuthenticationManager that causes the authentication ticket to not be generated when authentication succeed.

---
+++ src/platform/toolkit/auth/servicestack/Strings.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Nohros.Security.Auth.ServiceStack
+{
+  internal class Strings
+  {
+    public const string kWwwAuthenticateHeader = "MustAuth realm=com.nohros";
+  }
+}
