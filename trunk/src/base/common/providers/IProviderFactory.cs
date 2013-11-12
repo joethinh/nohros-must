a51c9fc2842b158efe3ac230983f56f01cdeae7f
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2013-11-12 21:01:40 +0000

    * Created a new extension method for the IConfiguration class that allows the automatically creation of all registered IProviderFactory.

---
+++ trunk/src/base/common/providers/IProviderFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Providers
+{
+  /// <summary>
+  /// A factory for providers.
+  /// </summary>
+  public interface IProviderFactory
+  {
+    /// <summary>
+    /// Creates an instance of the <see cref="IProviderFactory{T}"/>
+    /// </summary>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    object CreateProvider(IDictionary<string, string> options);
+  }
+}
