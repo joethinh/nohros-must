d9530f19c020c03b87df7bf2d5706bdfb82c76a7
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2014-01-14 12:45:43 +0000

    Added a factory for the SqlAppState class.

---
+++ src/impl/providers/sqlce/SqlAppStateFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nohros.Providers;
+
+namespace Nohros.Data.SqlCe
+{
+  /// <summary>
+  /// A factory for the <see cref="SqlCeAppState"/> class.
+  /// </summary>
+  public class SqlAppStateFactory : IProviderFactory<SqlCeAppState>
+  {
+    object IProviderFactory.CreateProvider(IDictionary<string, string> options) {
+      return CreateProvider(options);
+    }
+
+    public SqlCeAppState CreateProvider(IDictionary<string, string> options) {
+      return CreateAppState(options) as SqlCeAppState;
+    }
+
+    public IAppState CreateAppState(IDictionary<string, string> options) {
+      var factory = new SqlCeConnectionProviderFactory();
+      var sql_connection_provider = factory
+        .CreateProvider(options) as SqlCeConnectionProvider;
+      var states = new SqlCeAppState(sql_connection_provider);
+      states.Initialize();
+      return states;
+    }
+  }
+}
