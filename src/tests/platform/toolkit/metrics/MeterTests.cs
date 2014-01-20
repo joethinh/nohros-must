80fb317148955c640d1250854b509765c4c62642
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2014-01-20 22:19:36 +0000

    Added unit tests for the Meter class.

--- src/tests/platform/toolkit/metrics/MeterTests.cs
+++ src/tests/platform/toolkit/metrics/MeterTests.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using NUnit.Framework;
+using Telerik.JustMock;
+using Nohros.Extensions;
 
-namespace Nohros.Metrics
+namespace Nohros.Metrics.Tests
 {
   public class MeterTests
   {
+    [SetUp]
+    public void SetUp() {
+    }
+
     [Test]
     public void ShouldCreateBlankMeter() {
       var meter = new Meter("thing", TimeUnit.Seconds);
@@ -16,6 +22,18 @@ namespace Nohros.Metrics
       var meter = new Meter("thing", TimeUnit.Seconds);
       meter.Mark(3);
       Assert.That(meter.Count, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void should_mark_evets_and_update_rates_count() {
+      var meter =
+        new Meter(
+          new ListClock(new[] {0L, 0L, 10L.ToNanos(TimeUnit.Seconds)}));
+      meter.Mark();
+      meter.Mark(2);
+      Assert.That(meter.OneMinuteRate, Is.InRange(0.1830, 0.1850));
+      Assert.That(meter.FiveMinuteRate, Is.InRange(0.1966, 0.1976));
+      Assert.That(meter.FifteenMinuteRate, Is.InRange(0.1988, 0.1998));
     }
   }
 }
