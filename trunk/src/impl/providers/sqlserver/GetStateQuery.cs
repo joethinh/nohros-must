913ee56d3271f159326c75b89d3b88e924786b47
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2014-02-28 15:21:09 +0000

    Implemented the IAppState for the SQL Server engine.

---
+++ trunk/src/impl/providers/sqlserver/GetStateQuery.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Transactions;
+using Nohros.Logging;
+using Nohros.Resources;
+using Nohros.Extensions;
+
+namespace Nohros.Data.SqlServer
+{
+  public class GetStateQuery
+  {
+    const string kClassName = "Nohros.Data.SqlServer.GetStateQuery";
+
+    readonly MustLogger logger_ = MustLogger.ForCurrentProcess;
+    readonly SqlConnectionProvider sql_connection_provider_;
+
+    public GetStateQuery(SqlConnectionProvider sql_connection_provider) {
+      sql_connection_provider_ = sql_connection_provider;
+      logger_ = MustLogger.ForCurrentProcess;
+      SupressTransactions = true;
+    }
+
+    public bool Execute<T>(string state_name, string table_name, out T state) {
+      using (
+        new TransactionScope(SupressTransactions
+          ? TransactionScopeOption.Suppress
+          : TransactionScopeOption.Required)) {
+        using (SqlConnection conn = sql_connection_provider_.CreateConnection())
+        using (var builder = new CommandBuilder(conn)) {
+          IDbCommand cmd = builder
+            .SetText("select state from " + table_name + " where name=@name")
+            .SetType(CommandType.Text)
+            .AddParameter("@name", state_name)
+            .Build();
+          try {
+            conn.Open();
+            object obj = cmd.ExecuteScalar();
+            if (obj == null) {
+              state = default(T);
+              return false;
+            }
+            state = (T) obj;
+            return true;
+          } catch (SqlException e) {
+            logger_.Error(
+              StringResources.Log_MethodThrowsException.Fmt("Execute",
+                kClassName),
+              e);
+            throw new ProviderException(e);
+          }
+        }
+      }
+    }
+
+    public bool SupressTransactions { get; set; }
+  }
+}
