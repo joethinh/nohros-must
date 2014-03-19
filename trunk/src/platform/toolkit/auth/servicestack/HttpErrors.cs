30ad7e68b3e2d774e40afa4e4de3b373e2642a03
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2014-03-19 18:47:53 +0000

    Added a class that contains shourcuts for the HttpError constructors.

---
+++ trunk/src/platform/toolkit/auth/servicestack/HttpErrors.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using ServiceStack.Common.Web;
+
+namespace Nohros.Security.Auth.ServiceStack
+{
+  /// <summary>
+  /// A useful class that contains shortcuts for the construction of HttpError
+  /// objects.
+  /// </summary>
+  public static class HttpErrors
+  {
+    /// <summary>
+    /// Constructs a new <see cref="HttpError"/> that is related with the
+    /// <see cref="HttpStatusCode.InternalServerError"/> error code and
+    /// contains the default error message.
+    /// </summary>
+    public static Exception ServerError() {
+      return ServerError(Resources.Request_InternalServerError);
+    }
+
+    /// <summary>
+    /// Constructs a new <see cref="HttpError"/> that is related with the
+    /// <see cref="HttpStatusCode.InternalServerError"/> error code and
+    /// contains the given <paramref name="message"/>.
+    /// </summary>
+    public static Exception ServerError(string message) {
+      return new HttpError(HttpStatusCode.InternalServerError, message);
+    }
+  }
+}
