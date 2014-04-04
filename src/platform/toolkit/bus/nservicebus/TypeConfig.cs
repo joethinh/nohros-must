077217f35c3566e76f364e68ae5ebd33df52152e
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2014-04-04 00:24:37 +0000

    Created a binding of the in memory bus for the nservicebus.

---
+++ src/platform/toolkit/bus/nservicebus/TypeConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq.Expressions;
+using NServiceBus.ObjectBuilder;
+
+namespace Nohros.Bus.NServiceBus
+{
+  internal class TypeConfig : ITypeConfig
+  {
+    readonly IComponentConfig config_;
+
+    public TypeConfig(IComponentConfig config) {
+      config_ = config;
+    }
+
+    ITypeConfig ITypeConfig.ConfigureProperty(string name,
+      object value) {
+      config_.ConfigureProperty(name, value);
+
+      return this;
+    }
+  }
+
+  internal class TypeConfig<T> : ITypeConfig<T>
+  {
+    readonly IComponentConfig<T> config_;
+
+    public TypeConfig(IComponentConfig<T> config) {
+      config_ = config;
+    }
+
+    ITypeConfig<T> ITypeConfig<T>.ConfigureProperty(
+      Expression<Func<T, object>> property, object value) {
+      config_.ConfigureProperty(property, value);
+
+      return this;
+    }
+  }
+}
