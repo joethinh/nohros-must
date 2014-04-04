d4e7358aea9c139cd8a3778ca93b161c39dc4b3f
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2014-04-04 00:05:28 +0000

    Defined a container interface for the InMemoryBus

---
+++ trunk/src/platform/toolkit/bus/Lifecycle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Nohros.Bus
+{
+  /// <summary>
+  /// Represent the various lifecycles available for components configured in
+  /// the container.
+  /// </summary>
+  public enum Lifecycle
+  {
+    /// <summary>
+    /// The same instance will be returned each time.
+    /// </summary>
+    SingleInstance,
+
+    /// <summary>
+    /// The instance will be singleton for the duration of the unit of work.
+    /// In practice this means  the processing of a single transport message.
+    /// </summary>
+    InstancePerUnitOfWork,
+
+    /// <summary>
+    /// A new instance will be returned fro each call.
+    /// </summary>
+    InstancePerCall
+  }
+}
