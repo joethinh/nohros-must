6392a02105b1e77b07cbc1c1fe45bd134f020218
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2014-04-14 17:39:07 +0000

    Created an project to abstract common postsharp aspects.

--- trunk/src/platform/toolkit/bus/bus/Lifecycle.cs
+++ trunk/src/platform/toolkit/bus/bus/Lifecycle.cs
@@ -14,8 +14,21 @@ namespace Nohros.Bus
     SingleInstance,
 
     /// <summary>
-    /// A new instance will be returned fro each call.
+    /// A new instance will be returned for each call.
     /// </summary>
-    InstancePerCall
+    InstancePerCall,
+
+    /// <summary>
+    /// The instance will be singleton for the duration of the unit of work.
+    /// In practice this means the processing of a single command and the
+    /// events generated during its processing.
+    /// </summary>
+    /// <remarks>
+    /// This option is used only by application that performs transactional
+    /// processing. If the container does not support this type of lifecycle
+    /// a <see cref="NotSupportedException"/> should be throw when a attempt
+    /// to register a component using this lifecycle if performed.
+    /// </remarks>
+    InstancePerTransaction,
   }
 }
