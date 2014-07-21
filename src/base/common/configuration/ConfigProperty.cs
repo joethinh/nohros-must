f85f9927ddca984c20cdfd1596c540fac95a1516
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2014-07-21 21:22:44 +0000

    Created configuration sections for data servers config

---
+++ src/base/common/configuration/ConfigProperty.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Nohros.Configuration
+{
+  /// <summary>
+  /// An extension class for parsing configuration values to .NET types.
+  /// </summary>
+  public sealed class ConfigProperty
+  {
+    /// <summary>
+    /// Cast the <paramref name="value"/> to the type <typeparamref name="T"/>
+    /// without throwing exceptions.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type to cast to.
+    /// </typeparam>
+    /// <param name="value">
+    /// The value to cast.
+    /// </param>
+    /// <returns>
+    /// The <paramref name="value"/> casted to the type <typeparamref name="T"/>
+    /// or the default value for <typeparamref name="T"/> if the cast fails.
+    /// </returns>
+    public static T CastTo<T>(object value) {
+      try {
+        return (T) value;
+      } catch (Exception) {
+        return default(T);
+      }
+    }
+  }
+}
---
+++ src/base/common/configuration/DataServersConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+
+namespace Nohros.Configuration
+{
+  /// <summary>
+  /// Represents the main section of the <see cref="DataServerConfig"/>
+  /// configurations.
+  /// </summary>
+  public class DataServersConfig : ConfigurationSection
+  {
+    /// <summary>
+    /// Gets or sets the collection of configured <see cref="DataServersConfig"/>
+    /// </summary>
+    [ConfigurationProperty("", IsRequired = false, IsKey = false,
+      IsDefaultCollection = true)]
+    public virtual DataServerConfigCollection Servers {
+      get { return this[""] as DataServerConfigCollection; }
+      set { this[""] = value; }
+    }
+  }
+}
