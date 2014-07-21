224f793693b8d79c3e8da0ccc0216343c69999e3
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2014-07-21 21:22:44 +0000

    Created configuration sections for data servers config

---
+++ trunk/src/base/common/configuration/DataServerConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Configuration;
+
+namespace Nohros.Configuration
+{
+  /// <summary>
+  /// Defines the configuration for a data server.
+  /// </summary>
+  public class DataServerConfig : ConfigurationElement
+  {
+    /// <summary>
+    /// Gets or set the name of the data sever.
+    /// </summary>
+    [ConfigurationProperty("Name", IsRequired = true)]
+    public string Name {
+      get { return this["Name"] as string; }
+      set { this["Name"] = value; }
+    }
+
+    /// <summary>
+    /// Gets or set the name of the connection string that is associated with
+    /// the data server.
+    /// </summary>
+    [ConfigurationProperty("ConnectionStringName", IsRequired = true)]
+    public string ConnectionStringName {
+      get { return this["ConnectionStringName"] as string; }
+      set { this["ConnectionStringName"] = value; }
+    }
+
+    /// <summary>
+    /// Gets or set a <see cref="Guid"/> that uniquely identifies the server.
+    /// </summary>
+    [ConfigurationProperty("ServerId", IsRequired = true)]
+    public Guid ServerId {
+      get { return (Guid) this["ServerId"]; }
+      set { this["ServerId"] = value; }
+    }
+
+    /// <summary>
+    /// Gets or sets a value that indicates if the connections to the data
+    /// server should supress the ambient transaction context.
+    /// </summary>
+    [ConfigurationProperty("SupressDTC", IsRequired = false,
+      DefaultValue = false)]
+    public bool SupressDTC {
+      get { return ConfigProperty.CastTo<bool>(this["SupressDTC"]); }
+      set { this["SupressDTC"] = value; }
+    }
+  }
+}
---
+++ trunk/src/base/common/configuration/DataServerConfigCollection.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using Nohros.Extensions;
+
+namespace Nohros.Configuration
+{
+  /// <summary>
+  /// Represents a collection of <see cref="DataServerConfig"/> elements.
+  /// </summary>
+  public class DataServerConfigCollection : ConfigurationElementCollection
+  {
+    protected override ConfigurationElement CreateNewElement() {
+      return new DataServerConfig();
+    }
+
+    protected override object GetElementKey(ConfigurationElement element) {
+      var config = (DataServerConfig) element;
+      return "{0}::{1}".Fmt(config.ServerId, config.ConnectionStringName);
+    }
+  }
+}
