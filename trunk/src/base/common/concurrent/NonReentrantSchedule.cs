bd92138254ce885da2c62221034ee1abf81fca6b
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2014-08-20 14:21:30 +0000

    * Add a way to accept state while running the a task through a NonReentrantSchedule instance.

--- trunk/src/base/common/concurrent/NonReentrantSchedule.cs
+++ trunk/src/base/common/concurrent/NonReentrantSchedule.cs
@@ -4,24 +4,39 @@ using Nohros.Logging;
 
 namespace Nohros.Concurrent
 {
+  /// <summary>
+  /// Defines a way to schedule a task in a non-reentrant manner.
+  /// </summary>
+  /// <remarks>
+  /// The <see cref="NonReentrantSchedule"/> is non-reentrant. When a task
+  /// is running the <see cref="NonReentrantSchedule"/> wait it to finish
+  /// before start a new one. After a task is finished the
+  /// <see cref="NonReentrantSchedule"/> waits a predefined 
+  /// interval and start the task again.
+  /// <para>
+  /// The tasks runs in a background thread.
+  /// </para>
+  /// <para>
+  /// Any unhandled exception is logged through the configured
+  /// <see cref="MustLogger"/> and swallowed. Exceptions raised by one task
+  /// does not stops the scheduling.
+  /// </para>
+  /// </remarks>
   public class NonReentrantSchedule
   {
     readonly TimeSpan interval_;
     readonly ManualResetEvent signaler_;
-    RunnableDelegate task_;
-    Thread thread_;
+    bool already_started_;
+    Action<object> task_;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="NonReentrantSchedule"/>
     /// class by using the given interval.
     /// </summary>
-    /// <param name="interval">
-    /// The time to wait before run the associated action.
-    /// </param>
     NonReentrantSchedule(TimeSpan interval) {
       interval_ = interval;
       signaler_ = new ManualResetEvent(false);
-      thread_ = null;
+      already_started_ = false;
       task_ = null;
     }
 
@@ -37,19 +52,8 @@ namespace Nohros.Concurrent
     /// <see cref="interval"/>.
     /// </returns>
     /// <remarks>
-    /// The <see cref="NonReentrantSchedule"/> is non-reentrant. When a task
-    /// is running the <see cref="NonReentrantSchedule"/> wait it to finish
-    /// before start a new one. After a task is finished the
-    /// <see cref="NonReentrantSchedule"/> waits the given
-    /// <paramref name="interval"/> and start the task again.
-    /// <para>
-    /// The tasks runs in a background thread.
-    /// </para>
-    /// <para>
-    /// Any unhandled exception is logged through the configured
-    /// <see cref="MustLogger"/> and swallowed. Exceptions raised by one task
-    /// does not stops the scheduling.
-    /// </para>
+    /// The returned <see cref="NonReentrantSchedule"/> will use a dedicated
+    /// thread to run the task.
     /// </remarks>
     public static NonReentrantSchedule Every(TimeSpan interval) {
       return new NonReentrantSchedule(interval);
@@ -61,18 +65,47 @@ namespace Nohros.Concurrent
     /// <param name="task">
     /// The action that should run at the associated interval.
     /// </param>
+    [Obsolete(
+      "For .NET 4.0 this method is obsolete, use the Runnable(Action) method instead"
+      , false)]
     public void Runnable(RunnableDelegate task) {
-      if (thread_ != null) {
+      Runnable(obj => task(), null);
+    }
+
+    /// <summary>
+    /// Defines the action that should run at the associated interval.
+    /// </summary>
+    /// <param name="task">
+    /// The action that should run at the associated interval.
+    /// </param>
+    public void Runnable(Action task) {
+      Runnable(obj => task(), null);
+    }
+
+    /// <summary>
+    /// Defines the action that should run at the associated interval.
+    /// </summary>
+    /// <param name="task">
+    /// The action that should run at the associated interval.
+    /// </param>
+    /// <param name="state">
+    /// </param>
+    public void Runnable(Action<object> task, object state) {
+      if (already_started_) {
         throw new InvalidOperationException("The task is already defined.");
       }
+      already_started_ = true;
       task_ = task;
-      thread_ = new BackgroundThreadFactory().CreateThread(ThreadMain);
-      thread_.Start();
+
+      new BackgroundThreadFactory()
+        .CreateThread(ThreadMain)
+        .Start(state);
     }
+
 
     void ThreadMain(object obj) {
       while (!signaler_.WaitOne(interval_)) {
-        task_();
+        task_(obj);
       }
     }
 
