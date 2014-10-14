067743e7ce00cd79c09ea33d8a47aaea333ddbe6
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2014-10-14 15:58:21 +0000

    * Created a QueryExecutor for the MySql provider.

---
+++ trunk/src/impl/providers/mysql/MySqlExceptionExceptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace Nohros.Data.MySql.Extensions
+{
+  public static class MySqlExceptionExtensions
+  {
+    public static ProviderException AsProviderException(
+      this MySqlException exception) {
+      switch (exception.Number) {
+        case 1169: // ER_DUP_UNIQUE
+          return new UniqueConstraintViolationException(exception);
+      }
+      return new ProviderException(exception);
+    }
+  }
+}
