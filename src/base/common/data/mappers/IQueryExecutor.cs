debd99f0cf232fc2cc1773c1ece963ff88308d11
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2014-10-14 15:51:11 +0000

    * Introduced the concept of query executor and single query mapper.

---
+++ src/base/common/data/mappers/IQueryExecutor.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Data;
+
+namespace Nohros.Data
+{
+  /// <summary>
+  /// Execute SQL commands againts a database.
+  /// </summary>
+  public interface IQueryExecutor
+  {
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returns a <see cref="IDataReaderMapper{T}"/> that can be used to
+    /// map the result set to a object of the type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the object that should be returned.
+    /// </typeparam>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="mapper">
+    /// A <see cref="Func{TResult}"/> that builds a
+    /// <see cref="IDataReaderMapper{T}"/> that can be used to map
+    /// result of the given <paramref name="query"/> to a
+    /// object of the type <typeparamref name="T"/>.
+    /// </param>
+    /// <returns>
+    /// The object produced by the execution of the method
+    /// <paramref name="mapper"/>.
+    /// </returns>
+    /// <remarks>
+    /// The mapper returned from the <see cref="Func{TResult}"/> is cached
+    /// internally and associated with the given <paramref name="query"/>. The
+    /// cache is never flushed. If you are generating SQL strings on the fly
+    /// without using parameters it is possible you hit memory issues.
+    /// </remarks>
+    IQueryMapper<T> ExecuteQuery<T>(string query,
+      Func<IDataReaderMapper<T>> mapper);
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returns a <see cref="IDataReaderMapper{T}"/> that can be used to
+    /// map the result set to a object of the type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the object that should be returned.
+    /// </typeparam>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="mapper">
+    /// A <see cref="Func{TResult}"/> that builds a
+    /// <see cref="IDataReaderMapper{T}"/> that can be used to map
+    /// result of the given <paramref name="query"/> to a
+    /// object of the type <typeparamref name="T"/>.
+    /// </param>
+    /// <param name="command_type">
+    /// The type of the command that is described by the
+    /// <paramref name="query"/> parameter.
+    /// </param>
+    /// <returns>
+    /// The object produced by the execution of the method
+    /// <paramref name="mapper"/>.
+    /// </returns>
+    /// <remarks>
+    /// The mapper returned from the <see cref="Func{TResult}"/> is cached
+    /// internally and associated with the given <paramref name="query"/>. The
+    /// cache is never flushed. If you are generating SQL strings on the fly
+    /// without using parameters it is possible you hit memory issues.
+    /// </remarks>
+    IQueryMapper<T> ExecuteQuery<T>(string query,
+      Func<IDataReaderMapper<T>> mapper, CommandType command_type);
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returns a <see cref="IDataReaderMapper{T}"/> that can be used to
+    /// map the result set to a object of the type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the object that should be returned.
+    /// </typeparam>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="mapper">
+    /// A <see cref="Func{TResult}"/> that builds a
+    /// <see cref="IDataReaderMapper{T}"/> that can be used to map
+    /// result of the given <paramref name="query"/> to a
+    /// object of the type <typeparamref name="T"/>.
+    /// </param>
+    /// <param name="set_parameters">
+    /// A <see cref="Action{T}"/> that allows the caller to set the values
+    /// of the parameters defined on the given query.
+    /// </param>
+    /// <returns>
+    /// The object produced by the execution of the method
+    /// <paramref name="mapper"/>.
+    /// </returns>
+    /// <remarks>
+    /// The mapper returned from the <see cref="Func{TResult}"/> is cached
+    /// internally and associated with the given <paramref name="query"/>. The
+    /// cache is never flushed. If you are generating SQL strings on the fly
+    /// without using parameters it is possible you hit memory issues.
+    /// </remarks>
+    IQueryMapper<T> ExecuteQuery<T>(string query,
+      Func<IDataReaderMapper<T>> mapper,
+      Action<CommandBuilder> set_parameters);
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// runs the method <paramref name="mapper"/> to map the result set to
+    /// the type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the object that should be returned.
+    /// </typeparam>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="command_type">
+    /// The type of the command that is described by the
+    /// <paramref name="query"/> parameter.
+    /// </param>
+    /// <param name="mapper">
+    /// A <see cref="Func{TResult}"/> that builds a
+    /// <see cref="IDataReaderMapper{T}"/> that can be used to map
+    /// result of the given <paramref name="query"/> to a
+    /// object of the type <typeparamref name="T"/>.
+    /// </param>
+    /// <param name="set_parameters">
+    /// A <see cref="Action{T}"/> that allows the caller to set the values
+    /// of the parameters defined on the given query.
+    /// </param>
+    /// <returns>
+    /// The object produced by the execution of the method
+    /// <paramref name="mapper"/>.
+    /// </returns>
+    /// <remarks>
+    /// The mapper returned from the <see cref="Func{TResult}"/> is cached
+    /// internally and associated with the given <paramref name="query"/>. The
+    /// cache is never flushed. If you are generating SQL strings on the fly
+    /// without using parameters it is possible you hit memory issues.
+    /// </remarks>
+    IQueryMapper<T> ExecuteQuery<T>(string query,
+      Func<IDataReaderMapper<T>> mapper,
+      Action<CommandBuilder> set_parameters,
+      CommandType command_type);
+
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returns the number of rows affected.
+    /// </summary>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <returns>
+    /// The number of rows affected.
+    /// </returns>
+    int ExecuteNonQuery(string query);
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returns the number of rows affected.
+    /// </summary>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="command_type">
+    /// The type of the command that is described by the
+    /// <paramref name="query"/> parameter.
+    /// </param>
+    /// <returns>
+    /// The number of rows affected.
+    /// </returns>
+    int ExecuteNonQuery(string query, CommandType command_type);
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returns the number of rows affected.
+    /// </summary>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="set_parameters">
+    /// A <see cref="Action{T}"/> that allows the caller to set the values
+    /// of the parameters defined on the given query.
+    /// </param>
+    /// <returns>
+    /// The number of rows affected.
+    /// </returns>
+    int ExecuteNonQuery(string query, Action<CommandBuilder> set_parameters);
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returns the number of rows affected.
+    /// </summary>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="command_type">
+    /// The type of the command that is described by the
+    /// <paramref name="query"/> parameter.
+    /// </param>
+    /// <param name="set_parameters">
+    /// A <see cref="Action{T}"/> that allows the caller to set the values
+    /// of the parameters defined on the given query.
+    /// </param>
+    /// <returns>
+    /// The number of rows affected.
+    /// </returns>
+    int ExecuteNonQuery(string query, Action<CommandBuilder> set_parameters,
+      CommandType command_type);
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returned the value of the first column of the first row of the
+    /// resulting recordset.
+    /// </summary>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <returns>
+    /// The value of the first column of the first row of the recordset
+    /// resulted from the execution of the <paramref name="query"/>.
+    /// </returns>
+    T ExecuteScalar<T>(string query);
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returned the value of the first column of the first row of the
+    /// resulting recordset.
+    /// </summary>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="t">
+    /// When this method returns contains the value of the first column of
+    /// the first row of the recordset resulted from the execution of the
+    /// <paramref name="query"/> if the recordset is not empty; otherwise,
+    /// the default value for the type <typeparamref name="T"/>.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> if the recordset resulted from the execution of the
+    /// <paramref name="query"/> is not empty; otherwise, <c>false</c>.
+    /// </returns>
+    bool ExecuteScalar<T>(string query, out T t);
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returned the value of the first column of the first row of the
+    /// resulting recordset.
+    /// </summary>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="command_type">
+    /// The type of the command that is described by the
+    /// <paramref name="query"/> parameter.
+    /// </param>
+    /// <returns>
+    /// The value of the first column of the first row of the recordset
+    /// resulted from the execution of the <paramref name="query"/>.
+    /// </returns>
+    T ExecuteScalar<T>(string query, CommandType command_type);
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returned the value of the first column of the first row of the
+    /// resulting recordset.
+    /// </summary>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="command_type">
+    /// The type of the command that is described by the
+    /// <paramref name="query"/> parameter.
+    /// </param>
+    /// <param name="t">
+    /// When this method returns contains the value of the first column of
+    /// the first row of the recordset resulted from the execution of the
+    /// <paramref name="query"/> if the recordset is not empty; otherwise,
+    /// the default value for the type <typeparamref name="T"/>.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> if the recordset resulted from the execution of the
+    /// <paramref name="query"/> is not empty; otherwise, <c>false</c>.
+    /// </returns>
+    bool ExecuteScalar<T>(string query, CommandType command_type, out T t);
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returned the value of the first column of the first row of the
+    /// resulting recordset.
+    /// </summary>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="set_parameters">
+    /// A <see cref="Action{T}"/> that allows the caller to set the values
+    /// of the parameters defined on the given query.
+    /// </param>
+    /// <returns>
+    /// The value of the first column of the first row of the recordset
+    /// resulted from the execution of the <paramref name="query"/>.
+    /// </returns>
+    T ExecuteScalar<T>(string query, Action<CommandBuilder> set_parameters);
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returned the value of the first column of the first row of the
+    /// resulting recordset.
+    /// </summary>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="set_parameters">
+    /// A <see cref="Action{T}"/> that allows the caller to set the values
+    /// of the parameters defined on the given query.
+    /// </param>
+    /// <param name="t">
+    /// When this method returns contains the value of the first column of
+    /// the first row of the recordset resulted from the execution of the
+    /// <paramref name="query"/> if the recordset is not empty; otherwise,
+    /// the default value for the type <typeparamref name="T"/>.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> if the recordset resulted from the execution of the
+    /// <paramref name="query"/> is not empty; otherwise, <c>false</c>.
+    /// </returns>
+    bool ExecuteScalar<T>(string query, Action<CommandBuilder> set_parameters,
+      out T t);
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returned the value of the first column of the first row of the
+    /// resulting recordset.
+    /// </summary>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="command_type">
+    /// The type of the command that is described by the
+    /// <paramref name="query"/> parameter.
+    /// </param>
+    /// <param name="set_parameters">
+    /// A <see cref="Action{T}"/> that allows the caller to set the values
+    /// of the parameters defined on the given query.
+    /// </param>
+    /// <returns>
+    /// The value of the first column of the first row of the recordset
+    /// resulted from the execution of the <paramref name="query"/>.
+    /// </returns>
+    T ExecuteScalar<T>(string query, Action<CommandBuilder> set_parameters,
+      CommandType command_type);
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returned the value of the first column of the first row of the
+    /// resulting recordset.
+    /// </summary>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="command_type">
+    /// The type of the command that is described by the
+    /// <paramref name="query"/> parameter.
+    /// </param>
+    /// <param name="set_parameters">
+    /// A <see cref="Action{T}"/> that allows the caller to set the values
+    /// of the parameters defined on the given query.
+    /// </param>
+    /// <param name="t">
+    /// When this method returns contains the value of the first column of
+    /// the first row of the recordset resulted from the execution of the
+    /// <paramref name="query"/> if the recordset is not empty; otherwise,
+    /// the default value for the type <typeparamref name="T"/>.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> if the recordset resulted from the execution of the
+    /// <paramref name="query"/> is not empty; otherwise, <c>false</c>.
+    /// </returns>
+    bool ExecuteScalar<T>(string query, Action<CommandBuilder> set_parameters,
+      CommandType command_type, out T t);
+  }
+}
---
+++ src/base/common/data/mappers/QueryMapper.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace Nohros.Data
+{
+  /// <summary>
+  /// The default implementation of the <see cref="IQueryMapper{T}"/> interface.
+  /// </summary>
+  /// <typeparam name="T">
+  /// The type of the class to be mapped.
+  /// </typeparam>
+  /// <remarks>
+  /// The <see cref="QueryMapper{T}"/> maps the result of a
+  /// <see cref="IDataReader"/> using a supplied
+  /// <see cref="IDataReaderMapper{T}"/> and dispose the objects associated
+  /// with the <see cref="IDataReader"/> when the <see cref="QueryMapper{T}"/>
+  /// is disposed.
+  /// </remarks>
+  public class QueryMapper<T> : IQueryMapper<T>
+  {
+    readonly IDataReaderMapper<T> mapper_;
+    readonly IDataReader reader_;
+    readonly IEnumerable<IDisposable> disposables_;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DataReaderReader"/>
+    /// </summary>
+    /// <param name="mapper">
+    /// A <see cref="IDataReaderMapper{T}"/> that should be used to map
+    /// the data containined into the <paramref name="reader"/> to a
+    /// object or a collection of objects of the type <typeparamref name="T"/>.
+    /// </param>
+    /// <param name="reader">
+    /// A <see cref="IDataReader"/> to fetch the data.
+    /// </param>
+    public QueryMapper(IDataReaderMapper<T> mapper, IDataReader reader)
+      : this(mapper, reader, Enumerable.Empty<IDisposable>()) {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DataReaderReader"/>
+    /// </summary>
+    /// <param name="mapper">
+    /// A <see cref="IDataReaderMapper{T}"/> that should be used to map
+    /// the data containined into the <paramref name="reader"/> to a
+    /// object or a collection of objects of the type <typeparamref name="T"/>.
+    /// </param>
+    /// <param name="reader">
+    /// A <see cref="IDataReader"/> to fetch the data.
+    /// </param>
+    /// <param name="disposables">
+    /// A collection of <see cref="IDisposable"/> objects related with the
+    /// given <paramref name="reader"/> that needs to be disposed as the same
+    /// time as the <see cref="QueryMapper{T}"/> object.
+    /// </param>
+    public QueryMapper(IDataReaderMapper<T> mapper, IDataReader reader,
+      IEnumerable<IDisposable> disposables) {
+      mapper_ = mapper;
+      reader_ = reader;
+      disposables_ = disposables;
+    }
+
+    /// <summary>
+    /// Maps a element of a query result to a object of type
+    /// <typeparamref name="T"/>.
+    /// </summary>
+    /// <returns>
+    /// A object of type <typeparamref name="T"/> containing the data readed
+    /// from the current row of the associated <see cref="IDataReader"/>.
+    /// </returns>
+    public T Map() {
+      return mapper_.Map(reader_);
+    }
+
+    /// <summary>
+    /// maps a element of a query result to a object of type
+    /// <typeparamref name="T"/>.
+    /// </summary>
+    /// <param name="t">
+    /// When this method returns contains a object of type
+    /// <typeparamref name="T"/> associated the data readed from the current
+    /// row of the associated <see cref="IDataReader"/>.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> when the map operation the data returned from the database
+    /// was successfully mapped to a object of type <typeparamref name="T"/>;
+    /// otherwise, <c>false</c>.
+    /// </returns>
+    /// <remarks>
+    /// When the associated <see cref="IDataReader"/> does not contain any
+    /// data, <c>false</c> will be returned to indicate that the expected
+    /// result does not exists.
+    /// </remarks>
+    public bool Map(out T t) {
+      return mapper_.Map(reader_, out t);
+    }
+
+    /// <summary>
+    /// Maps a element of a query result to a object of type
+    /// <typeparamref name="T"/>.
+    /// </summary>
+    /// <returns>
+    /// A object of type <typeparamref name="T"/> containing the data readed
+    /// from the next row of the associated <see cref="IDataReader"/>.
+    /// </returns>
+    /// <remarks>
+    /// The <see cref="IDataReader.Read"/> method will be called to fetch the
+    /// next record.
+    /// </remarks>
+    public T Map(Action<T> post_map) {
+      return mapper_.Map(reader_, post_map);
+    }
+
+    /// <summary>
+    /// Maps a element of a query result to a object of type using the current
+    /// record of the <see cref="IDataReader"/>.
+    /// <typeparamref name="T"/>.
+    /// </summary>
+    /// <returns>
+    /// A object of type <typeparamref name="T"/> containing the data readed
+    /// from the current row of the associated <see cref="IDataReader"/>.
+    /// </returns>
+    /// <remarks>
+    /// The associated <see cref="IDataReader"/> should be positioned at a
+    /// valid record.
+    /// </remarks>
+    public T MapCurrent() {
+      return mapper_.MapCurrent(reader_);
+    }
+
+    /// <summary>
+    /// Maps a element of a query result to a collection of object of type
+    /// <typeparamref name="T"/>.
+    /// </summary>
+    /// <returns>
+    /// A collection of objects of type <typeparamref name="T"/> containing
+    /// the data readed from the associated <see cref="IDataReader"/>.
+    /// </returns>
+    public IEnumerable<T> Map(bool defer) {
+      return mapper_.Map(reader_, defer);
+    }
+
+    /// <summary>
+    /// Maps a element of a query result to a collection of object of type
+    /// <typeparamref name="T"/>.
+    /// </summary>
+    /// <returns>
+    /// A collection of objects of type <typeparamref name="T"/> containing
+    /// the data readed from the associated <see cref="IDataReader"/>.
+    /// </returns>
+    public IEnumerable<T> Map(bool defer, Action<T> post_map) {
+      return mapper_.Map(reader_, defer, post_map);
+    }
+
+    /// <inheritdoc/>
+    public void Dispose() {
+      reader_.Dispose();
+      foreach (var disposable in disposables_) {
+        if (disposable != null) {
+          disposable.Dispose();
+        }
+      }
+    }
+  }
+}
