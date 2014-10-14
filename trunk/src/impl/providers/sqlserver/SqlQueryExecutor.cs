9d7a12ad588f042f38aff95f96b85c4dd2de23fe
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2014-10-14 15:56:40 +0000

    * Adapted the SqlQueryExecutor to the new IQueryExecutor interface

--- trunk/src/impl/providers/sqlserver/SqlQueryExecutor.cs
+++ trunk/src/impl/providers/sqlserver/SqlQueryExecutor.cs
@@ -1,23 +1,27 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Data;
 using System.Data.SqlClient;
 using Nohros.Data.SqlServer.Extensions;
 using Nohros.Extensions;
 using Nohros.Logging;
 using Nohros.Resources;
+using System.Linq;
 
 namespace Nohros.Data.SqlServer
 {
   /// <summary>
   /// Execute SQL commands againts a SQL Server database.
   /// </summary>
-  public class SqlQueryExecutor
+  public class SqlQueryExecutor : IQueryExecutor
   {
     const string kClassName = "Nohros.Data.SqlServer.SqlQueryExecutor";
 
     readonly SqlConnectionProvider sql_connection_provider_;
     readonly CommandType default_command_type_;
     readonly MustLogger logger_;
+
+    readonly ConcurrentDictionary<string, object> cache_;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="SqlQueryExecutor"/> class
@@ -36,6 +40,7 @@ namespace Nohros.Data.SqlServer
       sql_connection_provider_ = sql_connection_provider;
       default_command_type_ = default_command_type;
       logger_ = MustLogger.ForCurrentProcess;
+      cache_ = new ConcurrentDictionary<string, object>();
     }
 
     /// <summary>
@@ -58,6 +63,7 @@ namespace Nohros.Data.SqlServer
     /// The object produced by the execution of the method
     /// <paramref name="mapper"/>.
     /// </returns>
+    [Obsolete("This method is obsolete. Check the ExecuteQuery that returns a IQueryMapper")]
     public T ExecuteQuery<T>(string query, Func<IDataReader, T> mapper) {
       return ExecuteQuery(query, mapper, builder => { },
         default_command_type_);
@@ -87,6 +93,7 @@ namespace Nohros.Data.SqlServer
     /// The object produced by the execution of the method
     /// <paramref name="mapper"/>.
     /// </returns>
+    [Obsolete("This method is obsolete. Check the ExecuteQuery that returns a IQueryMapper")]
     public T ExecuteQuery<T>(string query,
       Func<IDataReader, T> mapper, CommandType command_type) {
       return ExecuteQuery(query, mapper, builder => { }, command_type);
@@ -116,6 +123,7 @@ namespace Nohros.Data.SqlServer
     /// The object produced by the execution of the method
     /// <paramref name="mapper"/>.
     /// </returns>
+    [Obsolete("This method is obsolete. Check the ExecuteQuery that returns a IQueryMapper")]
     public T ExecuteQuery<T>(string query, Func<IDataReader, T> mapper,
       Action<CommandBuilder> set_parameters) {
       return ExecuteQuery(query, mapper, set_parameters, default_command_type_);
@@ -149,6 +157,7 @@ namespace Nohros.Data.SqlServer
     /// The object produced by the execution of the method
     /// <paramref name="mapper"/>.
     /// </returns>
+    [Obsolete("This method is obsolete. Check the ExecuteQuery that returns a IQueryMapper")]
     public T ExecuteQuery<T>(string query, Func<IDataReader, T> mapper,
       Action<CommandBuilder> set_parameters, CommandType command_type) {
       using (SqlConnection conn = sql_connection_provider_.CreateConnection())
@@ -172,6 +181,177 @@ namespace Nohros.Data.SqlServer
         }
       }
     }
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returns a <see cref="IDataReaderMapper{T}"/> that can be used to
+    /// map the result set to a object of the type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the object that should be returned.
+    /// </typeparam>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="mapper">
+    /// A <see cref="Func{TResult}"/> that builds a
+    /// <see cref="IDataReaderMapper{T}"/> that can be used to map
+    /// result of the given <paramref name="query"/> to a
+    /// object of the type <typeparamref name="T"/>.
+    /// </param>
+    /// <returns>
+    /// The object produced by the execution of the method
+    /// <paramref name="mapper"/>.
+    /// </returns>
+    /// <remarks>
+    /// The mapper returned from the <see cref="Func{TResult}"/> is cached
+    /// internally and associated with the given <paramref name="query"/>. The
+    /// cache is never flushed. If you are generating SQL strings on the fly
+    /// without using parameters it is possible you hit memory issues.
+    /// </remarks>
+    public IQueryMapper<T> ExecuteQuery<T>(string query,
+      Func<IDataReaderMapper<T>> mapper) {
+      return ExecuteQuery(query, mapper, builder => { },
+        default_command_type_);
+    }
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returns a <see cref="IDataReaderMapper{T}"/> that can be used to
+    /// map the result set to a object of the type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the object that should be returned.
+    /// </typeparam>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="mapper">
+    /// A <see cref="Func{TResult}"/> that builds a
+    /// <see cref="IDataReaderMapper{T}"/> that can be used to map
+    /// result of the given <paramref name="query"/> to a
+    /// object of the type <typeparamref name="T"/>.
+    /// </param>
+    /// <param name="command_type">
+    /// The type of the command that is described by the
+    /// <paramref name="query"/> parameter.
+    /// </param>
+    /// <returns>
+    /// The object produced by the execution of the method
+    /// <paramref name="mapper"/>.
+    /// </returns>
+    /// <remarks>
+    /// The mapper returned from the <see cref="Func{TResult}"/> is cached
+    /// internally and associated with the given <paramref name="query"/>. The
+    /// cache is never flushed. If you are generating SQL strings on the fly
+    /// without using parameters it is possible you hit memory issues.
+    /// </remarks>
+    public IQueryMapper<T> ExecuteQuery<T>(string query,
+      Func<IDataReaderMapper<T>> mapper, CommandType command_type) {
+      return ExecuteQuery(query, mapper, builder => { }, command_type);
+    }
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// returns a <see cref="IDataReaderMapper{T}"/> that can be used to
+    /// map the result set to a object of the type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the object that should be returned.
+    /// </typeparam>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="mapper">
+    /// A <see cref="Func{TResult}"/> that builds a
+    /// <see cref="IDataReaderMapper{T}"/> that can be used to map
+    /// result of the given <paramref name="query"/> to a
+    /// object of the type <typeparamref name="T"/>.
+    /// </param>
+    /// <param name="set_parameters">
+    /// A <see cref="Action{T}"/> that allows the caller to set the values
+    /// of the parameters defined on the given query.
+    /// </param>
+    /// <returns>
+    /// The object produced by the execution of the method
+    /// <paramref name="mapper"/>.
+    /// </returns>
+    /// <remarks>
+    /// The mapper returned from the <see cref="Func{TResult}"/> is cached
+    /// internally and associated with the given <paramref name="query"/>. The
+    /// cache is never flushed. If you are generating SQL strings on the fly
+    /// without using parameters it is possible you hit memory issues.
+    /// </remarks>
+    public IQueryMapper<T> ExecuteQuery<T>(string query,
+      Func<IDataReaderMapper<T>> mapper,
+      Action<CommandBuilder> set_parameters) {
+      return ExecuteQuery(query, mapper, set_parameters, default_command_type_);
+    }
+
+    /// <summary>
+    /// Executes the command described by <see cref="query"/> on the server and
+    /// runs the method <paramref name="mapper"/> to map the result set to
+    /// the type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the object that should be returned.
+    /// </typeparam>
+    /// <param name="query">
+    /// The query to be executed on the server.
+    /// </param>
+    /// <param name="command_type">
+    /// The type of the command that is described by the
+    /// <paramref name="query"/> parameter.
+    /// </param>
+    /// <param name="mapper">
+    /// A <see cref="Func{TResult}"/> that builds a
+    /// <see cref="IDataReaderMapper{T}"/> that can be used to map
+    /// result of the given <paramref name="query"/> to a
+    /// object of the type <typeparamref name="T"/>.
+    /// </param>
+    /// <param name="set_parameters">
+    /// A <see cref="Action{T}"/> that allows the caller to set the values
+    /// of the parameters defined on the given query.
+    /// </param>
+    /// <returns>
+    /// The object produced by the execution of the method
+    /// <paramref name="mapper"/>.
+    /// </returns>
+    /// <remarks>
+    /// The mapper returned from the <see cref="Func{TResult}"/> is cached
+    /// internally and associated with the given <paramref name="query"/>. The
+    /// cache is never flushed. If you are generating SQL strings on the fly
+    /// without using parameters it is possible you hit memory issues.
+    /// </remarks>
+    public IQueryMapper<T> ExecuteQuery<T>(string query,
+      Func<IDataReaderMapper<T>> mapper,
+      Action<CommandBuilder> set_parameters,
+      CommandType command_type) {
+      SqlConnection conn = sql_connection_provider_.CreateConnection();
+      var builder = new CommandBuilder(conn);
+      IDbCommand cmd = builder
+        .SetText(query)
+        .SetType(command_type)
+        .Set(set_parameters)
+        .Build();
+      try {
+        conn.Open();
+        IDataReader reader = cmd.ExecuteReader();
+        var m = (IDataReaderMapper<T>) cache_.GetOrAdd(query, s => mapper());
+        return new QueryMapper<T>(m, reader);
+      } catch (SqlException e) {
+        logger_.Error(
+          StringResources
+            .Log_MethodThrowsException
+            .Fmt("ExecuteQuery", kClassName), e);
+        throw e.AsProviderException();
+      } catch {
+        builder.Dispose();
+        conn.Dispose();
+        throw;
+      }
+    }
+
 
     /// <summary>
     /// Executes the command described by <see cref="query"/> on the server and
