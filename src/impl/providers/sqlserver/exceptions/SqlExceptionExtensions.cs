b75a15820bf4d90601c34fdaca18e8a456bb2579
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2015-01-20 18:41:57 +0000

    * Added a extension method to detect a NOT NULL constraint violation exception for Sql Server.

--- src/impl/providers/sqlserver/exceptions/SqlExceptionExtensions.cs
+++ src/impl/providers/sqlserver/exceptions/SqlExceptionExtensions.cs
@@ -12,6 +12,8 @@ namespace Nohros.Data.SqlServer.Extensions
           case 2601: // duplicate index constraint
           case 2627: // duplicate check constraint
             return new UniqueConstraintViolationException(exception);
+          case 515: // insert on non nulable column
+            return new NotNullConstraintViolationException(exception);
         }
 
         if (exception.Number >= 50000) {
