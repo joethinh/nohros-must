575da9ba52fcc8ed5cb7834883ff13e488ae396c
nohros.ceo <nohros.ceo@aaea48d5-2f21-36e0-6168-e527f18fc9e2>
2015-03-11 21:34:13 +0000

    * Added a way to specify a list of tags that should be added to all metrics registered through the AppMEtrics class.

--- trunk/src/platform/toolkit/metrics/library/AppMetrics.cs
+++ trunk/src/platform/toolkit/metrics/library/AppMetrics.cs
@@ -11,12 +11,16 @@ using Nohros.Metrics.Annotations;
 namespace Nohros.Metrics
 {
   /// <summary>
-  /// A default registry that delegates all actions to a class specified by
-  /// the key "DefaultMetricsRegistry" in the
-  /// <see cref="ConfigurationManager.AppSettings"/> configuration section. The
-  /// specified registry class must have a constructor with no arguments. If
-  /// the property is not specified or the class cannot be loaded an instance
-  /// of the <see cref="MetricsRegistry"/> will be used.
+  /// A default registry that delegates all actions to a either class
+  /// configured  through the <see cref="Configure"/> method, the class
+  /// defined by the the key "DefaultMetricsRegistry" in the
+  /// <see cref="ConfigurationManager.AppSettings"/> configuration section,
+  /// or the default registry(<see cref="MetricsRegistry"/>).
+  /// <para>
+  /// The class defined by the key "DefaultMetricsRegistry" must have a
+  /// constructor with no arguments. If the class cannot be loaded
+  /// de defualt metrics registry approach will be used.
+  /// </para>
   /// </summary>
   public class AppMetrics
   {
@@ -37,11 +41,6 @@ namespace Nohros.Metrics
       }
 
       /// <inheritdoc/>
-      protected internal override Measure Compute(long tick) {
-        return CreateMeasure(Metrics.Count);
-      }
-
-      /// <inheritdoc/>
       public IEnumerator<IMetric> GetEnumerator() {
         return Metrics.GetEnumerator();
       }
@@ -53,6 +52,106 @@ namespace Nohros.Metrics
 
       /// <inheritdoc/>
       public ICollection<IMetric> Metrics { get; private set; }
+
+      /// <inheritdoc/>
+      protected internal override Measure Compute(long tick) {
+        return CreateMeasure(Metrics.Count);
+      }
+    }
+
+    /// <summary>
+    /// A class that is used to configure the behavior of the
+    /// global <see cref="AppMetrics"/>.
+    /// </summary>
+    public class Configurer
+    {
+      internal Configurer() {
+        process_tags_
+          .WithTag("ns.node", Environment.MachineName);
+      }
+
+      /// <summary>
+      /// Define the collection of tags that should be associated with
+      /// all metrics registered through the <see cref="AppMetrics"/>.
+      /// </summary>
+      /// <param name="tags">
+      /// The list of tags to be associated with all metrics registered
+      /// through the <see cref="AppMetrics"/>.
+      /// </param>
+      /// <remarks>
+      /// This method should be used only if you want to remove all the
+      /// tags that is automatically created by the metrics library and
+      /// provide your own list of tags. If the intend is to increment
+      /// the default list of tags, use the <see cref="WithAdditionalTags(Tags)"/>
+      /// method instead.
+      /// </remarks>
+      public Configurer WithTags(Tags tags) {
+        process_tags_ = new Tags.Builder(tags);
+        return this;
+      }
+
+      /// <summary>
+      /// Adds the given collection of tags to the current list of
+      /// tags that is associated with all the metrics registered through
+      /// the <see cref="AppMetrics"/>.
+      /// </summary>
+      /// <param name="tags">
+      /// The list of tags to be associated with all metrics registered
+      /// through the <see cref="AppMetrics"/>.
+      /// </param>
+      public Configurer WithAdditionalTags(Tags tags) {
+        process_tags_.WithTags(tags);
+        return this;
+      }
+
+      /// <summary>
+      /// Adds the given collection of tags to the current list of
+      /// tags that will be associated with all the metrics registered
+      /// through the <see cref="AppMetrics"/>.
+      /// </summary>
+      /// <param name="tags">
+      /// The list of tags to be merged with the list of all tags that
+      /// will be associated with all metrics registered through the
+      /// <see cref="AppMetrics"/>.
+      /// </param>
+      public Configurer WithAdditionalTags(IEnumerable<Tag> tags) {
+        process_tags_.WithTags(tags);
+        return this;
+      }
+
+      /// <summary>
+      /// Adds the given tag to the current list of tags that will be
+      /// associated with all the metrics registered through
+      /// the <see cref="AppMetrics"/>.
+      /// </summary>
+      /// <param name="tag">
+      /// The tag to be added to the list of tags that will be associated
+      /// with all metrics registered through the <see cref="AppMetrics"/>.
+      /// </param>
+      public Configurer WithAdditionalTag(Tag tag) {
+        process_tags_.WithTag(tag);
+        return this;
+      }
+
+      /// <summary>
+      /// Adds the given tag to the current list of tags that will be
+      /// associated with all the metrics registered through
+      /// the <see cref="AppMetrics"/>.
+      /// </summary>
+      /// <param name="name">
+      /// The name of the tag to be added to the list of tags that
+      /// will be associated with all metrics registered through the
+      /// <see cref="AppMetrics"/>.
+      /// </param>
+      /// <param name="value">
+      /// The value of the tag to be added to the list of tags that
+      /// will be associated with all metrics registered through the
+      /// <see cref="AppMetrics"/>.
+      /// </param>
+      public Configurer WithAdditionalTag(string name, string value) {
+        process_tags_.WithTag(name, value);
+        return this;
+      }
     }
 
     /// <summary>
@@ -89,15 +188,18 @@ namespace Nohros.Metrics
         metric_.GetMeasure(m => callback(WrapMeasure(m), (state)));
       }
 
+      /// <inheritdoc/>
+      public MetricConfig Config { get; private set; }
+
       Measure WrapMeasure(Measure measure) {
         return new Measure(Config, measure.Value, measure.IsObservable);
       }
-
-      /// <inheritdoc/>
-      public MetricConfig Config { get; private set; }
     }
 
     const string kDefaultMetricsRegistryKey = "DefaultMetricsRegistry";
+
+    static bool configured_;
+    static Tags.Builder process_tags_;
 
     static AppMetrics() {
       string default_metrics_registry_class =
@@ -118,6 +220,41 @@ namespace Nohros.Metrics
       if (ForCurrentProcess == null) {
         ForCurrentProcess = new MetricsRegistry();
       }
+
+      process_tags_ = new Tags.Builder();
+
+      Configure();
+
+      // Mark the class as not configured to allow users to
+      // overrite the default behavior.
+      configured_ = false;
+    }
+
+    /// <summary>
+    /// Configure the behavior of the <see cref="AppMetrics"/>.
+    /// </summary>
+    /// <returns>
+    /// A <see cref="Configurer"/> that can be used to manage the
+    /// behavior of the <see cref="AppMetrics"/>.
+    /// </returns>
+    /// <exception cref="InvalidOperationException">
+    /// The <see cref="Configure"/> method was already called.
+    /// </exception>
+    /// <remarks>
+    /// This method should be called before the usage of any
+    /// method of the <see cref="AppMetrics"/>, usually at the application
+    /// startup.
+    ///<para>
+    /// This method should be called once per application and calling it
+    /// a second time will throw an exception.
+    /// </para>
+    /// </remarks>
+    public static Configurer Configure() {
+      if (configured_) {
+        throw new ArgumentException("The AppMetrics was already configured.");
+      }
+      configured_ = true;
+      return new Configurer();
     }
 
     /// <summary>
@@ -156,7 +293,7 @@ namespace Nohros.Metrics
     /// Note that the <see cref="RegisterObject(object)"/>  will use
     /// reflection to add all instances of <see cref="IMetric"/> that have
     /// been declared, and also add a tag with the value set to class simple
-    /// name (<see cref="Type.Name"/>).
+    /// name (<see cref="Type.Name"/>) and namespace (<see cref="Type.Namespace"/>).
     /// </para>
     /// </remarks>
     /// <returns>
@@ -169,6 +306,7 @@ namespace Nohros.Metrics
       Type klass = obj.GetType();
       Tags tags =
         new Tags.Builder()
+          .WithTags(process_tags_.Build())
           .WithTags(GetTags(klass, true)) // static class tags
           .WithTags(GetTagsList(obj)) // dynamic class tags
           .WithTag("class", klass.Name)
@@ -237,6 +375,11 @@ namespace Nohros.Metrics
       return new MetricWrapper(tags, metric);
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
     static IEnumerable<Tag> GetTagsList(object obj) {
       FieldInfo field =
         GetFields(obj.GetType())
@@ -258,18 +401,70 @@ namespace Nohros.Metrics
       return Enumerable.Empty<Tag>();
     }
 
+    /// <summary>
+    /// Checks if the given <paramref name="field"/> represents a
+    /// type that can be is assiged to an <see cref="IMetric"/>.
+    /// </summary>
+    /// <param name="field">
+    /// A <see cref="FieldInfo"/> contianing the type information
+    /// about the field to check.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> if the type associated with the given
+    /// <paramref name="field"/> can be assigned to a
+    /// <see cref="IMetric"/>.
+    /// </returns>
     static bool IsMetricType(FieldInfo field) {
       return typeof (IMetric).IsAssignableFrom(field.FieldType);
     }
 
+    /// <summary>
+    /// Checks if the given <paramref name="field"/> represents a
+    /// type that can be is assiged to an
+    /// <see cref="IEnumerable{T}"/> of <see cref="Tag"/>.
+    /// </summary>
+    /// <param name="field">
+    /// A <see cref="FieldInfo"/> contianing the type information
+    /// about the field to check.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> if the type associated with the given
+    /// <paramref name="field"/> can be assigned to a
+    /// <see cref="IEnumerable{T}"/> of <see cref="Tag"/>.
+    /// </returns>
     static bool IsTagList(FieldInfo field) {
       return IsTagList(field.FieldType);
     }
 
+    /// <summary>
+    /// Checks if the given <paramref name="method"/> returns am
+    /// object that can be is assiged to an
+    /// <see cref="IEnumerable{T}"/> of <see cref="Tag"/>.
+    /// </summary>
+    /// <param name="method">
+    /// A <see cref="MethodInfo"/> contianing the type information
+    /// about the method to check.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> if the return type of the given
+    /// <paramref name="method"/> can be assigned to a
+    /// <see cref="IEnumerable{T}"/> of <see cref="Tag"/>.
+    /// </returns>
     static bool IsTagList(MethodInfo method) {
       return IsTagList(method.ReturnType);
     }
 
+    /// <summary>
+    /// Checks if the given type is assignable to an
+    /// <see cref="IEnumerable{T}"/> of <see cref="Tag"/>
+    /// </summary>
+    /// <param name="type">
+    /// The type to check for assignability.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> if <paramref name="type"/> can be assigned
+    /// to a <see cref="IEnumerable{T}"/> of <see cref="Tag"/>.
+    /// </returns>
     static bool IsTagList(Type type) {
       return typeof (IEnumerable<Tag>).IsAssignableFrom(type);
     }
